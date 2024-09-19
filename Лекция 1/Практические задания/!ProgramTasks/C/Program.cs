241c35ac61045ba693d9354aa872d62025ba6716
darkarat <dar.karat3011@gmail.com>
2024-09-19 22:56:07 +0300

    Update Program.cs
    Сделано

--- Лекция 1/Практические задания/!ProgramTasks/C/Program.cs
+++ Лекция 1/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,14 +1,14 @@
-﻿namespace C
+namespace C
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             double pi = Math.PI;
-            int tenThousand = 10000L;
-            float tenThousandPi = pi * tenThousand;
-            int roundedTenThousandPi = tenThousandPi;
-            int integerPartOfTenThousandPi = tenThousandPi;
+            int tenThousand = 10000;
+            double tenThousandPi = pi * tenThousand;
+            int roundedTenThousandPi = (int)Math.Round(tenThousandPi);
+            int integerPartOfTenThousandPi = (int)(tenThousand * pi);
             Console.WriteLine(integerPartOfTenThousandPi);
             Console.WriteLine(roundedTenThousandPi);
         }
