0430b2ef2fd9784540cb70f19676092350d30adc
darkarat <dar.karat3011@gmail.com>
2024-09-20 00:23:12 +0300

    Update Program.cs
    Сделано

--- Лекция 1/Практические задания/!ProgramTasks/E/Program.cs
+++ Лекция 1/Практические задания/!ProgramTasks/E/Program.cs
@@ -1,11 +1,14 @@
-﻿namespace E
+using System;
+using System.Globalization;
+
+namespace E
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string doubleNumber = "894376.243643";
-            int number = doubleNumber; // Вася уверен, что ошибка где-то тут
+            double number = double.Parse(doubleNumber, CultureInfo.InvariantCulture); // Сделано, но при изменении строки добавились первые две строчки, чтобы возможно было применить CultureInfo
             Console.WriteLine(number + 1);
         }
     }
