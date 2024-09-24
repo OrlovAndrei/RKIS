8b326b73debaaf348a7f6292218326df3c6906c5
sonyxwx <sofikurbanova@icloud.com>
2024-09-24 13:16:53 +0300

    AngryBirdsTask.cs

--- Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
+++ Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
@@ -6,6 +6,9 @@ public static class AngryBirdsTask
 {
     public static double FindSightAngle(double v, double distance)
     {
-        return Math.PI / 4;
+         double g = 9.8;
+ 
+	     return 0.5 * Math.Asin(distance * g / (v*v));
+ 
     }
 }
