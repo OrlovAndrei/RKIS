803e25dd006185c13beaea2b24ef9c6f159899d6
Дарья Каратеева 3834 <dar.karat3011@gmail.com>
2024-09-27 22:43:02 +0300

    Update Program.cs
    Сделано, но повторный коммит

--- Занятие 1/Практические задания/!ProgramTasks/G/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/G/Program.cs
@@ -1,11 +1,11 @@
-﻿namespace G
+namespace G
 {
     internal class Program
     {
         private static string GetGreetingMessage(string name, double salary)
         {
             // возвращает "Hello, <name>, your salary is <salary>"
-            ...
+            return $"Hello, {name} your salary is {Math.Ceiling(salary)}"; //Сделано, Ceiling окргуляет число вверх
         }
 
         static void Main(string[] args)
