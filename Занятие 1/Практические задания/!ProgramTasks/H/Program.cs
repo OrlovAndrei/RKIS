08b372e03c21839172645289def5231467f55c0e
Дарья Каратеева 3834 <dar.karat3011@gmail.com>
2024-09-27 22:44:03 +0300

    Update Program.cs
    Сделано, но повторный коммит

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace H
+namespace H
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Print(GetSquare(42));
+        }
+        public static void Print(int x)
+        {
+            Console.WriteLine(x);
+        }
+        public static int GetSquare(int x)
+        {
+            return (int)Math.Pow(x, 2);
         }
     }
 }
