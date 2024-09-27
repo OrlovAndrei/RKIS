d7f292023d935ea848f1614f9dc1c7e7b51f4aff
kartoxaArtem <darwincompanyfilms@gmail.com>
2024-09-27 12:34:14 +0300

    wew

--- Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
@@ -1,17 +1,22 @@
-﻿namespace I
+﻿using System.Text;
+
+namespace I
 {
     internal class Program
     {
         static string GetLastHalf(string text)
         {
-            ...
+
+            text = text.Substring(text.Length / 2);
+            string c = text.Replace(" ", String.Empty);
+            return c;
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine(GetLastHalf("I love CSharp!"));
             Console.WriteLine(GetLastHalf("1234567890"));
-            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+            Console.WriteLine(GetLastHalf("�� �� �� �� ���� �� ��"));
         }
     }
 }
