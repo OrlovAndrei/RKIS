9432cfd1960150b3233184f8dd0ec391389362ee
darkarat <dar.karat3011@gmail.com>
2024-09-28 20:27:01 +0300

    add BilliardsTask ex.1

--- Занятие 1/Практические задания/Billiards/BilliardsTask.cs
+++ Занятие 1/Практические задания/Billiards/BilliardsTask.cs
@@ -1,16 +1,11 @@
+using System.Xml.Linq;
+
 namespace Billiards;
 
 public static class BilliardsTask
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="directionRadians">Угол направления движения шара</param>
-    /// <param name="wallInclinationRadians">Угол</param>
-    /// <returns></returns>
     public static double BounceWall(double directionRadians, double wallInclinationRadians)
     {
-        //TODO
-        return 0.0;
+        return 2 * (wallInclinationRadians) - directionRadians; //2a-B
     }
 }
