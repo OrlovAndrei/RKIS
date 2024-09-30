deea3c4889c1866171d8cba4ad1ab0d71b1ae5bc
Nameless-gg <igor01.12.2004@mail.ru>
2024-10-01 01:08:21 +0300

    Исправил задание D

--- Занятие 2/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/D/Program.cs
@@ -39,7 +39,7 @@ namespace D
 
         public static bool ShouldFire2(bool enemyInFront, string enemyName, int robotHealth)
         {
-            return enemyInFront && enemyName != "boss" || enemyInFront && (enemyName == "boss" && robotHealth >= 50);
+            return enemyInFront && (enemyName != "boss" || robotHealth >= 50);
         }
     }
 }
