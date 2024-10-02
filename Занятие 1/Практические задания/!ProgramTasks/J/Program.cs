fd764c13e49986ac4618e73cf4de5057bb537f82
Daria Oleinik <darya.oleynik.06@bk.ru>
2024-10-02 17:13:40 +0300

    J

--- Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            double number = 5.5+7;
-            Console.Writeline(number):
+            {
+                Console.WriteLine("Hello, World!");
+                var number = 5.5;
+                number += 7;
+                Console.WriteLine(number);
+            }
         }
     }
-}
