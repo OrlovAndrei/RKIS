a771e4c0c32e86fe92c670164259e83d9ed357cc
Roman <168776357+deniezky@users.noreply.github.com>
2024-10-02 21:51:18 +0300

    Update Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,14 +1,14 @@
-﻿namespace C
+namespace C
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             double pi = Math.PI;
-            int tenThousand = 10000L;
-            float tenThousandPi = pi * tenThousand;
-            int roundedTenThousandPi = tenThousandPi;
-            int integerPartOfTenThousandPi = tenThousandPi;
+            long tenThousand = 10000L;
+            float tenThousandPi = (float)pi * tenThousand;
+            int roundedTenThousandPi = Math.Round(tenThousandPi);
+            int integerPartOfTenThousandPi = (int)Math.Floor(tenThousandPi);
             Console.WriteLine(integerPartOfTenThousandPi);
             Console.WriteLine(roundedTenThousandPi);
         }
