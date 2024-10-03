a3a215526a241ac564a735ff814e406f79afb074
artem-boost <155313634+artem-boost@users.noreply.github.com>
2024-10-03 07:58:25 +0000

    D

--- Занятие 1/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/D/Program.cs
@@ -6,8 +6,9 @@
         {
             double amount = 1.11; //количество биткоинов от одного человека
             int peopleCount = 60; // количество человек
-            int totalMoney = (int)amount * peopleCount; // ← исправьте ошибку в этой строке
+            int totalMoney = (int)Math.Round(amount*peopleCount);
             Console.WriteLine(totalMoney);
+
         }
     }
 }
