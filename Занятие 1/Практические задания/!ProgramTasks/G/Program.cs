eb166a4a81e70eb7ec3c7e853a5ca9eb10cb4801
NikB159 <bartgames73@gmail.com>
2024-10-03 13:55:50 +0300

    Выполнил G

--- Занятие 1/Практические задания/!ProgramTasks/G/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/G/Program.cs
@@ -5,7 +5,14 @@
         private static string GetGreetingMessage(string name, double salary)
         {
             // возвращает "Hello, <name>, your salary is <salary>"
-            ...
+            string namee = (string)name;
+            int salaryy = (int)Math.Ceiling(salary);
+            string s1 = "Hello, ";
+            string s2 = namee;
+            string s3 = ", your salary is ";
+            string s4 = salaryy.ToString();
+            string total = s1 + s2 + s3 + s4;
+            return total;
         }
 
         static void Main(string[] args)
