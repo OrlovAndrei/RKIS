5dfdc13ddcd8f2a24da05e186a1f759e61e13f10
Алексей Железков <Alexeychik-uss@mail.ru>
2024-10-06 20:42:59 +0300

    Алексей Железков 3834

--- Занятие 1/Практические задания/Percentages/Percentages.cs
+++ Занятие 1/Практические задания/Percentages/Percentages.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 
-public class Percentages
+class Percentages
 {
-	static void Main(string[] args)
-	{
-		int InitialAmount = Console.Read();
-
+    public static double Calculate(string userInput)
+    {
+        double InitialAmount = double.Parse(userInput.Split(' ')[0]);
+        double Bet = double.Parse(userInput.Split(' ')[1]) / 100;
+        int Months = int.Parse(userInput.Split(' ')[2]);
+        
+        for (int i = 1; i <= Months; i++)
+        {
+            InitialAmount += InitialAmount * Bet / 12;
+        }
+        return InitialAmount;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите исходную сумму, процентную ставку (в процентах) и срок вклада в месяцах (через пробел): ");
+        string UserInput = Console.ReadLine();
+        Console.WriteLine(Calculate(UserInput));
     }
 }
