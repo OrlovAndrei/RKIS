081ddfb46e86531a2e84043890e37c2834ea4ce5
habibylin <postnikovaleks0@gmail.com>
2024-10-08 13:25:46 +0300

    Update BilliardsTask.cs

--- Занятие 1/Практические задания/Billiards/BilliardsTask.cs
+++ Занятие 1/Практические задания/Billiards/BilliardsTask.cs
@@ -1,16 +1,22 @@
 namespace Billiards;
 
 public static class BilliardsTask
+double BounceWall(double directionRadians, double wallInclinationRadians)
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="directionRadians">Угол направления движения шара</param>
-    /// <param name="wallInclinationRadians">Угол</param>
-    /// <returns></returns>
-    public static double BounceWall(double directionRadians, double wallInclinationRadians)
-    {
-        //TODO
-        return 0.0;
-    }
+            double direction = (directionRadians * 180) / Math.PI;
+            double wallInclination = (wallInclinationRadians * 180) / Math.PI;
+ 
+            double wallAngle = 90+wallInclination;
+            double ballAngle = 180+direction;
+ 
+            double diff = wallAngle - ballAngle;
+            double angle = ballAngle + 2 * diff + 360 * 5;
+ 
+            angle %= 360;
+            if (angle > 180)
+            {
+                angle -= 360;
+            }
+ 
+            return (angle*Math.PI)/180;
 }
