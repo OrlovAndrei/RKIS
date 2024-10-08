7bd46736879e57b4166471d7e6e7e8f8c7fbb31d
Курбанова София <sofikurbanova@icloud.com>
2024-10-08 13:22:47 +0300

    5Update PluralizeTask.cs

--- Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
+++ Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
@@ -1,10 +1,9 @@
-﻿namespace Pluralize;
-
-public static class PluralizeTask
+public static void Main(string[] args)
 {
-	public static string PluralizeRubles(int count)
-	{
-		// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-		return "руб.";
-	}
+    int n = int.Parse(Console.ReadLine());
+    string s = "рублей";
+    if (n % 10 == 1) s = "рубль";
+    if (n % 10 >= 2 && n % 10 <= 4) s = "рубля";
+    Console.WriteLine("{0} {1}", n, s);
+    Console.ReadKey();
 }
