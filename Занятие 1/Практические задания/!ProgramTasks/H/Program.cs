5009021a4bd4f163ab64c1ecd459a61d88e9ab7c
Мария <mirajkaano@gmail.com>
2024-10-11 12:45:44 +0300

    lesson 1 made D, E, F, G, H, I, J

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -6,5 +6,13 @@
         {
             Print(GetSquare(42));
         }
+        public static void Print(int x)
+        { 
+            Console.WriteLine(x);
+        }
+        public static int GetSquare(int x) 
+        {
+            return (int)Math.Pow(x, 2);
+        }
     }
 }
