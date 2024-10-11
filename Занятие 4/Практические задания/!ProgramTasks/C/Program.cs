1ef5b8709fc177dfc1355556576740b518488016
habibylin <postnikovaleks0@gmail.com>
2024-10-11 10:21:37 +0300

    C

--- Занятие 4/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,15 +1,18 @@
 ﻿namespace C
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
-            Console.WriteLine(GetElementCount(new[] {1, 1, 2, 1}, 1));
-        }
-
-        public static int GetElementCount(int[] items, int itemToCount)
-        {
-            ...
+            double pi = Math.PI;
+            int tenThousand = 10000L;
+            float tenThousandPi = pi * tenThousand;
+            int roundedTenThousandPi = tenThousandPi;
+            int integerPartOfTenThousandPi = tenThousandPi;
+            int tenThousand =(int)10000L;
+            float tenThousandPi =(float)(pi * tenThousand);
+            int roundedTenThousandPi =(int)Math.Round(tenThousandPi);
+            int integerPartOfTenThousandPi =(int)tenThousandPi;
+            Console.WriteLine(integerPartOfTenThousandPi);
+            Console.WriteLine(roundedTenThousandPi);
         }
     }
 }
