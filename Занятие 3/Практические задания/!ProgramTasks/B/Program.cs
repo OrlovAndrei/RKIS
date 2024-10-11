11f7166e37da58b48af26706d51b294288abbfba
habibylin <postnikovaleks0@gmail.com>
2024-10-11 09:59:02 +0300

    Update Program.cs

--- Занятие 3/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/B/Program.cs
@@ -1,27 +1,12 @@
 ﻿namespace B
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+        static void Main(строка[] аргументы)
         {
-            Console.WriteLine(RemoveStartSpaces("a"));
-            Console.WriteLine(RemoveStartSpaces(" b"));
-            Console.WriteLine(RemoveStartSpaces(" cd"));
-            Console.WriteLine(RemoveStartSpaces(" efg"));
-            Console.WriteLine(RemoveStartSpaces(" text"));
-            Console.WriteLine(RemoveStartSpaces(" two words"));
-            Console.WriteLine(RemoveStartSpaces("  two spaces"));
-            Console.WriteLine(RemoveStartSpaces("	tabs"));
-            Console.WriteLine(RemoveStartSpaces("		two	tabs"));
-            Console.WriteLine(RemoveStartSpaces("                             many spaces"));
-            Console.WriteLine(RemoveStartSpaces("               "));
-            Console.WriteLine(RemoveStartSpaces("\n\r line breaks are spaces too"));
-        }
-
-        public static string RemoveStartSpaces(string text)
-        {
-            if (char.IsWhiteSpace(text[0])) return text.Substring(1);
-            else return text;
-        }
-    }
-}
+            ... число1 = +5.5e-2;
+            ... число2 = 7.8f;
+            ... число3 = 0;
+            ... число4 = 2000000000000L;
+            двойное число1 = +5.5e-2;
+            число с плавающей точкой num2 = 7.8f;
+             int  num3  =  0 ;
+            длинное число4 = 2000000000000L;
