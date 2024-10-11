fd849a75e3c82ebce886774e2596c7c9f325412d
habibylin <postnikovaleks0@gmail.com>
2024-10-11 10:02:36 +0300

    Update Program.cs

--- Занятие 3/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/D/Program.cs
@@ -1,28 +1,6 @@
 ﻿namespace D
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            WriteBoard(8);
-            WriteBoard(1);
-            WriteBoard(2);
-            WriteBoard(3);
-            WriteBoard(10);
-        }
-
-        private static void WriteBoard(int size)
-        {
-
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine();
-        }
-    }
-}
+ {
+double amount = 1.11; //количество биткоинов от одного человека
+int peopleCount = 60; // количество человек
+int totalMoney = (int)amount * peopleCount; // ← исправьте ошибку в этой строке
+int totalMoney = (int)Math.Round(amount*peopleCount); // ← исправьте ошибку в этой строке
