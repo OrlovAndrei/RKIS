d532727c509a4c8a446ad0cf45472a1cbc3774cf
Артем Рогов <arogov279@gmail.com>
2024-10-12 16:52:36 +0300

    задание B

--- Занятие 1/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/B/Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            ... num1 = +5.5e-2;
+            ... num2 = 7.8f;
+            ... num3 = 0;
+            ... num4 = 2000000000000L;
             double num1 = +5.5e-2;
             float num2 = 7.8f;
             int num3 = 0;
@@ -11,7 +15,6 @@
             Console.WriteLine(num1);
             Console.WriteLine(num2);
             Console.WriteLine(num3);
-            Console.WriteLine(num4);
         }
     }
 }
