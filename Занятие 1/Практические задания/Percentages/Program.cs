427ab4181acd24ab99c34bfb527b498825dccdbd
artem-boost <155313634+artem-boost@users.noreply.github.com>
2024-10-12 11:06:30 +0000

    leson 1 make precenteges

---
+++ Занятие 1/Практические задания/Percentages/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+string? input = Console.ReadLine();
+string[]? result  = input.Split(separator: ' ');
+double P = Double.Parse(result[0]);
+var N = Double.Parse(result[1]);
+N = N/100;
+var T = Double.Parse(result[2]);
+var result1 = P*Math.Pow(1+(N/12),T);
+Console.WriteLine(result1);
+
