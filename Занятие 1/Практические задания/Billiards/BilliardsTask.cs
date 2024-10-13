68ca593df0706175f68d4cf049f0ae21fa7eeab2
Алексей Железков <Alexeychik-uss@mail.ru>
2024-10-13 11:15:01 +0300

    Lesson 1. Billiards

--- Занятие 1/Практические задания/Billiards/BilliardsTask.cs
+++ Занятие 1/Практические задания/Billiards/BilliardsTask.cs
@@ -4,20 +4,9 @@ namespace Billiards
 {
     public static class BilliardsTask
     {
-        static double radToDeg(double rad)// радианы в градусы
-        {
-            return (rad * 180) / Math.PI;
-        }
-
-        static double degToRad(double deg)// градусы в радианы
-        {
-            return (Math.PI * deg) / 180;
-        }
-
         public static double BounceWall(double directionRadians, double wallInclinationRadians)
         {
-
-            return degToRad(360.0 - radToDeg(directionRadians) + 2.0 * radToDeg(wallInclinationRadians));
+            return 2 * wallInclinationRadians - directionRadians;
         }
     }
 }
