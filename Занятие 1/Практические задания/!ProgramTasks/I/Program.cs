bf0f16fe4455f4e021be613a35df46600aae7d9c
GOL <164672521+accum104@users.noreply.github.com>
2024-10-13 22:41:09 +0300

    lesson 1 make I

--- Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
@@ -1,17 +1,17 @@
-﻿namespace I
+namespace I
 {
     internal class Program
     {
-        static string GetLastHalf(string text)
-        {
-            ...
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine(GetLastHalf("I love CSharp!"));
             Console.WriteLine(GetLastHalf("1234567890"));
             Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+        }
+
+        static string GetLastHalf(string text)
+        {
+            return text.Substring(text.Length / 2).Replace(" ", "");
         }
     }
 }
