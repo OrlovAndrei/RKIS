03be80257080f8166cfe363d80c095f4b9214843
Daria Oleinik <darya.oleynik.06@bk.ru>
2024-10-14 11:38:50 +0300

    EMPTYMAZETASK

--- Занятие 3/Практические задания/Mazes/EmptyMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/EmptyMazeTask.cs
@@ -2,8 +2,25 @@
 
 public static class EmptyMazeTask
 {
-	public static void MoveOut(Robot robot, int width, int height)
-	{
-		
-	}
+    public static void MoveOut(Robot robot, int width, int height)
+    {
+        MoveRight(robot, width - 2);
+        MoveDown(robot, height - 2);
+    }
+
+    private static void MoveRight(Robot robot, int steps)
+    {
+        for (int i = 0; i < steps; i++)
+        {
+            robot.MoveTo(Direction.Right);
+        }
+    }
+
+    private static void MoveDown(Robot robot, int steps)
+    {
+        for (int i = 0; i < steps; i++)
+        {
+            robot.MoveTo(Direction.Down);
+        }
+    }
 }
--- Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
@@ -4,6 +4,44 @@ public static class SnakeMazeTask
 {
     public static void MoveOut(Robot robot, int width, int height)
     {
+        MoveRight(robot, width - 2);
+        MoveDown(robot);
+        for (int i = 1; i < height - 2; i++)
+        {
+            MoveHorizontally(robot, i % 2 == 0);
+            MoveDown(robot);
+        }
+        MoveToExit(robot);
+    }
 
+    private static void MoveRight(Robot robot, int steps)
+    {
+        for (int i = 0; i < steps; i++)
+        {
+            robot.MoveTo(Direction.Right);
+        }
+    }
+
+    private static void MoveDown(Robot robot)
+    {
+        robot.MoveTo(Direction.Down);
+    }
+
+    private static void MoveHorizontally(Robot robot, bool toRight)
+    {
+        int steps = robot.GetWidth() - 2; // Убедитесь, что Вы получаете ширину корректно
+        for (int i = 0; i < steps; i++)
+        {
+            if (toRight)
+                robot.MoveTo(Direction.Right);
+            else
+                robot.MoveTo(Direction.Left);
+        }
+    }
+
+    private static void MoveToExit(Robot robot)
+    {
+        for (int i = 0; i < robot.GetHeight() - 2; i++)
+            robot.MoveTo(Direction.Down);
     }
 }
