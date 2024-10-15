ba73ad01052063f6f7a9f399de00dd8cfe41f6bb
RUDxNIK <wwwnikrud1205@mail.ru>
2024-10-15 18:38:00 +0300

    lession 1: Main A

--- Занятие 1/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/A/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The first step!!!!");
+            Console.WriteLine("The first step!!");
         }
     }
 }
