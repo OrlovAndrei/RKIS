b66f470b01dfc48c775d39e6b931c216ed028748
RUDxNIK <wwwnikrud1205@mail.ru>
2024-10-15 17:15:23 +0300

    lession 1: Procent

---
+++ Занятие 1/Практические задания/Percentages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Percentages
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            var input = Console.ReadLine().Split();
+            var money = double.Parse(input[0]);
+            var percent = double.Parse(input[1]);
+            var months = double.Parse(input[2]);
+            Console.WriteLine(Calculate(money, percent, months));
+
+
+        }
+
+        private static double Calculate(double money, double percent, double months)
+        {
+            return Math.Pow(1 + (percent / 10) / 90, months);
+        }
+    }
+}
