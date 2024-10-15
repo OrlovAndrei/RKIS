bd4000362cba1b1ceb2f07c72823af376d597bf0
maxvlzzn <msms60837@gmail.com>
2024-10-15 16:34:35 +0300

    lesson 1: make AngryBirds

--- Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
+++ Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
@@ -7,7 +7,6 @@ public static class AngryBirdsTask
     public static double FindSightAngle(double v, double distance)
     {
         double g = 9.8;
-        return 0.5 * Math.Asin(distance * g / Math.Pow(v, 2));
         return 0.5 * Math.Asin(distance * g / (v * v));
     }
 }
