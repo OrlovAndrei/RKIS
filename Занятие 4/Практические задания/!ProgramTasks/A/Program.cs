36e4464eae97353d22a2aff670e781b213b9cf32
kartoxaArtem <darwincompanyfilms@gmail.com>
2024-10-16 13:38:07 +0300

    lesson 4: make A

--- Занятие 4/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/A/Program.cs
@@ -10,7 +10,11 @@
 
         public static int[] GetFirstEvenNumbers(int count)
         {
-            ...
+           int[] evenNumbers = new int[count]
+            for (int i = 0; i < count; i++) {
+                evenNumbers[i] = (i + 1) * 2
+            }
+            return evenNumbers;
         }
 
     }
