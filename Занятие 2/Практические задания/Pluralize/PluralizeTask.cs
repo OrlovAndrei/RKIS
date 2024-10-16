942df777cb4040ce7fbac647d4b15e7318d49a00
Agni <63151335+crash36663@users.noreply.github.com>
2024-10-16 18:48:12 +0300

    Update PluralizeTask.cs

--- Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
+++ Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
@@ -1,10 +1,11 @@
 ﻿namespace Pluralize;
 
-public static class PluralizeTask
-{
-	public static string PluralizeRubles(int count)
-	{
-		// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-		return "руб.";
-	}
-}
+public static string PluralizeRubles(int count)
+    {     
+        if (count % 100 > 10 && count % 100 < 20)
+            return "рублей";
+        if(count % 10 > 4 && count % 10 < 10)
+            return "рублей";
+        if(count == 0 || count % 10 == 0)
+            return "рублей";
+        return count % 10 == 1 ? "рубль" : "рубля";
