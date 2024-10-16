4355a01db110b88e1808a8b76fb1bd10a83a9fcb
savitar696 <karosen1448@gmail.com>
2024-10-16 05:21:48 +0300

    lesson 5: make B

--- Занятие 5/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 5/Практические задания/!ProgramTasks/B/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace B
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmailDictionary
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string[] entries = {
+                "Sasha:sasha1995@sasha.ru",
+                "Alex:alex99@mail.ru",
+                "Shurik:shurik2020@google.com",
+                "Sergey:sergey1985@mail.ru",
+                "Anna:anna_123@gmail.com",
+                "Anastasia:anastasia2022@gmail.com"
+            };
+
+            Dictionary<string, HashSet<string>> emailDictionary = new Dictionary<string, HashSet<string>>(); // java
+
+            foreach (var entry in entries)
+            {
+                var parts = entry.Split(':');
+                if (parts.Length == 2)
+                {
+                    string name = parts[0].Trim();
+                    string email = parts[1].Trim();
+
+                    string key = name.Length >= 2 ? name.Substring(0, 2).ToUpper() : name.ToUpper();
+
+                    if (!emailDictionary.ContainsKey(key))
+                    {
+                        emailDictionary[key] = new HashSet<string>();
+                    }
+                    emailDictionary[key].Add(email);
+                }
+            }
+
+            foreach (var kvp in emailDictionary)
+            {
+                Console.WriteLine($"Key: {kvp.Key} -> Value: {{{string.Join(", ", kvp.Value)}}}");
+            }
         }
     }
 }
