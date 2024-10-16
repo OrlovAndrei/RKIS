2f2b55407eb6e859ecb0d82968ad0fcd7f142840
artem-boost <155313634+artem-boost@users.noreply.github.com>
2024-10-16 08:15:55 +0000

    lesn 1 fix AbgriBirds

--- Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
+++ Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
@@ -5,7 +5,7 @@ public static class AngryBirdsTask
 {
     public static double FindSightAngle(double v, double distance)
     {
-        int g = 9.8
+         double g = 9.8;
        return 0.5 * Math.Asin(distance * g / (v * v));
     }
 }
