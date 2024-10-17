cc6d4fd4c3c5fa55ca30d113757a52be5941643e
RUDxNIK <wwwnikrud1205@mail.ru>
2024-10-17 16:20:05 +0300

    lession 3: C

--- Занятие 3/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/C/Program.cs
@@ -13,6 +13,12 @@
 
         private static void WriteTextWithBorder(string text)
         {
+            int textLength = text.Length;
+            string border = "+" + new string('-', textLength + 2) + "+";
+
+            Console.WriteLine(border);
+            Console.WriteLine($"| {text} |");
+            Console.WriteLine(border);
         }
     }
 }
