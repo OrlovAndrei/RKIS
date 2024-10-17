aeda7e74d32b2bf76864cee85938ed4ea3d273bb
AkobJan <akobtonoyan78@gmail.com>
2024-10-17 17:14:27 +0300

    first

--- Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
+++ Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
@@ -1,6 +1,22 @@
 using System;
 
 namespace Rectangles;
+{
+     public class Rectangle
+{
+    public int Left { get; }
+    public int Top { get; }
+    public int Right { get; }
+    public int Bottom { get; }
+
+    public Rectangle(int left, int top, int right, int bottom)
+    {
+        Left = left;
+        Top = top;
+        Right = right;
+        Bottom = bottom;
+    }
+}
 
 public static class RectanglesTask
 {
