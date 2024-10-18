2eaeb57328e07b93932383446dfb3304b9ac9ab6
42408 <42408@SRV-12>
2024-10-18 12:46:57 +0300

    Lesson 1: Make H(1)

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace H
+﻿using System;
+
+namespace H
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Print(GetSquare(42));
+            var a = 42;
+            var b = 2;
+            int GetSquare = (int)Math.Pow(a, b);
+            Console.WriteLine(GetSquare);
         }
     }
 }
