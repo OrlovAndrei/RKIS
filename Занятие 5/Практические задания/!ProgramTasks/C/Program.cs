d8dbd2ae6eaa0dcc12eade62c2d5543624568d6c
Daria Oleinik <darya.oleynik.06@bk.ru>
2024-10-18 08:32:50 +0300

    LEsson5: make С

--- Занятие 5/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 5/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace C
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace C
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "Россия: 145872256, США - 331883986, Индия; 1380004385, Бразилия 213993437";
+
+            Console.WriteLine("Оригинальная строка:");
+            Console.WriteLine(input);
+
+            string pattern = @"[ :;,-]+";
+            string output = Regex.Replace(input, pattern, "\t");
+
+            Console.WriteLine("\nПреобразованная строка:");
+            Console.WriteLine(output);
         }
     }
 }
