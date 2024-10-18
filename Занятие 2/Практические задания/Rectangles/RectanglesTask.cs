4df8b5eead7a3bf51015b1b5d58f428e5ff1320a
Programmuer <dashazagorulko06@gmail.com>
2024-10-18 20:56:19 +0300

    Update RectanglesTask.cs

--- Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
+++ Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
@@ -1,27 +1,28 @@
 using System;
-
-namespace Rectangles;
-
-public static class RectanglesTask
+ 
+namespace Rectangles
 {
-    // Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
-    public static bool AreIntersected(Rectangle r1, Rectangle r2)
+    public static class RectanglesTask
     {
-        // так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
-        return true;
-    }
-
-    // Площадь пересечения прямоугольников
-    public static int IntersectionSquare(Rectangle r1, Rectangle r2)
-    {
-        return 0;
-    }
-
-    // Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
-    // Иначе вернуть -1
-    // Если прямоугольники совпадают, можно вернуть номер любого из них.
-    public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
-    {
-        return -1;
+        // Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
+        public static bool AreIntersected(Rectangle r1, Rectangle r2)
+        {
+            // так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
+            return true;
+        }
+ 
+        // Площадь пересечения прямоугольников
+        public static int IntersectionSquare(Rectangle r1, Rectangle r2)
+        {
+            return 0;
+        }
+ 
+        // Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
+        // Иначе вернуть -1
+        // Если прямоугольники совпадают, можно вернуть номер любого из них.
+        public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
+        {
+            return -1;
+        }
     }
 }
