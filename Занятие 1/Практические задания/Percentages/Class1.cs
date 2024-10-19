6934cb9ae135ec2103a19eddd981ea954d0cb8a8
Ardashev Sasha <ardashev004@gmail.com>
2024-10-19 16:58:23 +0300

    lesson 1: Percentages

--- Занятие 1/Практические задания/Percentages/Class1.cs
+++ Занятие 1/Практические задания/Percentages/Class1.cs
@@ -2,8 +2,9 @@
 
 public class Percentages
 {
-		static void Main(string[] args)
-		{
-			int InitialAmount = Console.ReadLine();
-	}
+    static void Main(string[] args)
+    {
+        int InitialAmount = Console.Read();
+    }
+
 }
