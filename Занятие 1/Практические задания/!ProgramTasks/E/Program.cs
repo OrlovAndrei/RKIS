301f33deb4ac46b9145a8343576964798971bf2d
qYs1er <daniilkokorin49@gmail.com>
2024-10-20 21:24:03 +0300

    Lesson 1: E измененно

--- Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
@@ -6,7 +6,7 @@
         {
             string doubleNumber = "894376.243643";
             double number = double.Parse(doubleNumber); // Вася уверен, что ошибка где-то тут
-            Console.WriteLine(number + 1);
+            Console.WriteLine(number + 1); 
         }
     }
 }
