f395f815de67ed8473050368698d14b5926c7e0d
maxvlzzn <msms60837@gmail.com>
2024-10-21 18:04:51 +0300

    lesson 2: изменил Rectangles

--- Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
+++ Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
@@ -6,34 +6,43 @@ public static class RectanglesTask
 {
     public static bool AreIntersected(Rectangle r1, Rectangle r2)
     {
-        var left = Math.Max(Math.Min(r1.Left, r1.Right), Math.Min(r2.Right, r2.Left));
-        var right = Math.Min(Math.Max(r1.Left, r1.Right), Math.Max(r2.Right, r2.Left));
+        var left = Math.Max(Math.Min(r1!.Left, r1!.Right), Math.Min(r2.Left, r2.Right));
+        var right = Math.Min(Math.Max(r1!.Left, r1!.Right), Math.Max(r2.Left, r2.Right));
+        var bottom = Math.Min(Math.Max(r1!.Top, r1!.Bottom), Math.Max(r2.Top, r2.Bottom));
         var top = Math.Max(Math.Min(r1.Top, r1.Bottom), Math.Min(r2.Top, r2.Bottom));
-        var bottom = Math.Min(Math.Max(r1.Top, r1.Bottom), Math.Max(r2.Top, r2.Bottom));
-        var width = Math.Max(right, left) - Math.Min(left, right);
-        var height = Math.Max(top, bottom) - Math.Min(top, bottom);
-        if (right < left || bottom < top) return false;
-        return true;
+        if (right < left || bottom < top)
+        {
+            return false;
+        }
     }
 
     public static int IntersectionSquare(Rectangle r1, Rectangle r2)
     {
         if (AreIntersected(r1, r2))
         {
-            var left = Math.Max(Math.Min(r1.Left, r1.Right), Math.Min(r2.Right, r2.Left));
-            var right = Math.Min(Math.Max(r1.Left, r1.Right), Math.Max(r2.Right, r2.Left));
-            var top = Math.Max(Math.Min(r1.Top, r1.Bottom), Math.Min(r2.Top, r2.Bottom));
+            var left = Math.Max(Math.Min(r1.Left, r1.Right), Math.Min(r2.Left, r2.Right));
+            var right = Math.Min(Math.Max(r1.Left, r1.Right), Math.Max(r2.Left, r2.Right));
             var bottom = Math.Min(Math.Max(r1.Top, r1.Bottom), Math.Max(r2.Top, r2.Bottom));
-            var width = Math.Max(right, left) - Math.Min(left, right);
-            var height = Math.Max(top, bottom) - Math.Min(top, bottom);
+            var top = Math.Max(Math.Min(r1.Top, r1.Bottom), Math.Min(r2.Top, r2.Bottom));
+            var height = Math.Max(right, left) - Math.Min(left, right);
+            var width = Math.Max(bottom, top) - Math.Min(top, bottom);
+            return height * width;
         }
-        return 0;
     }
 
     public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
     {
-        if (r2.Top >= r1.Top && r2.Left >= r1.Left && r2.Right <= r1.Right && r2.Bottom <= r1.Bottom) return 1;
-        else if (r1.Top >= r2.Top && r1.Left >= r2.Left && r1.Right <= r2.Right && r1.Bottom <= r2.Bottom) return 0;
-        else return -1;
+        if (r1.Top >= r2.Top && r1.Left >= r2.Left && r1.Right <= r2.Right && r1.Bottom <= r2.Bottom)
+        {
+            return 0;
+        }
+        else if (r2.Top >= r1.Top && r2.Left >= r1.Left && r2.Right <= r1.Right && r2.Bottom <= r1.Bottom)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1; 
+        }
     }
 }
