d324ca07185e9959593a2a9675a6e90f98adc986
Рудакова Алёна <arudakova436@gmail.com>
2024-10-21 13:19:39 +0300

    Update HeatmapTask.cs

--- Занятие 4/Практические задания/Names/HeatmapTask.cs
+++ Занятие 4/Практические задания/Names/HeatmapTask.cs
@@ -1,13 +1,34 @@
-﻿namespace Names;
-
-internal static class HeatmapTask
+using System;
+ 
+namespace Names
 {
-    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+    internal static class HeatmapTask
     {
-        return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+        public static string[] NomerDayGet(NameData[] names)
+        {
+            var nomerDayArr = new string[30];
+            for (int i = 0; i<30; i++)
+                nomerDayArr[i] = (i+2).ToString();
+            return nomerDayArr;
+        }
+        
+        public static string[] NomerMounthGet(NameData[] names)
+        {
+            var nomerMounthArr = new string[12];
+            for (int i = 0; i<12; i++)
+                {nomerMounthArr[i] = (i+1).ToString();}        
+            return nomerMounthArr;
+        }
+        
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            string[] mounth = NomerMounthGet(names);
+            string[] day = NomerDayGet(names);
+            double[,] mounthDay  = new double[30, 12];
+            foreach (var mname in names)
+                if (mname.BirthDate.Day !=1)
+                    mounthDay[mname.BirthDate.Day-2, mname.BirthDate.Month-1]++;
+            return new HeatmapData("Пример карты интенсивностей", mounthDay, day, mounth);
+        }
     }
 }
