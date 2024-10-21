aa4edab08d1f1ce3f557c122ed558e48273e7b5e
Basileios-afk <grom154825@gmail.com>
2024-10-21 22:13:27 +0300

    lesson 5: make B

--- Занятие 6/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 6/Практические задания/!ProgramTasks/B/Program.cs
@@ -17,9 +17,18 @@
         public static MenuItem[] GenerateMenu()
         {
             return new[] {
-            ...
+                new MenuItem {
+                Caption = "File", HotKey = "F", Items = new MenuItem [] {
+                new MenuItem {Caption = "New", HotKey = "N"},
+                new MenuItem {Caption = "Save", HotKey = "S"}
+                }},
+                new MenuItem {
+                Caption = "Edit", HotKey = "E", Items = new MenuItem [] {
+                new MenuItem {Caption = "Copy", HotKey = "C"},
+                new MenuItem {Caption = "Paste", HotKey = "V"}
+                }}
             };
         }
-}
     }
+
 }
