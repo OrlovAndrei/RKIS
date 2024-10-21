9d0b1393b00fc52fb614ac08aa6111d570f42f4b
Lussuru <Lussuru17@mail.ru>
2024-10-21 21:18:32 +0300

    lesson 1: make H

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -6,5 +6,13 @@
         {
             Print(GetSquare(42));
         }
+        static int GetSquare(int number)
+        {
+            return number * number;
+        }
+        static void Print(int result)
+        {
+            Console.WriteLine(result);
+        }
     }
 }
