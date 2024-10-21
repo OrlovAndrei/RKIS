37096b9dd91754a2952996c3c78880b595cde71a
Рудакова Алёна <arudakova436@gmail.com>
2024-10-21 13:09:51 +0300

    Update SnakeMazeTask.cs

--- Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
@@ -1,9 +1,33 @@
-﻿namespace Mazes;
-
-public static class SnakeMazeTask
+namespace Mazes
 {
-    public static void MoveOut(Robot robot, int width, int height)
+    public static class DiagonalMazeTask
     {
-
+        public static void Move(Robot robot, int attitudeParties, Direction direction, Direction direction1)
+        {
+            if (!robot.Finished)
+                for (var robotStep = 0; robotStep < attitudeParties; robotStep++)
+                {
+                    robot.MoveTo(direction);
+                }
+            if (!robot.Finished)
+                robot.MoveTo(direction1);
+        }
+ 
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            while (!robot.Finished) //Этот цикл стоит вынести во вспомогательный метод и там делать нужные шаги. А метод движения в любом направлении на любое число шагов взять из первой задачи
+            {
+                if (width > height) //Зачем данное условие многократно проверять в цикле, если значения не меняются
+                {
+                    var attitudeParties = (width - 2) / (height - 2);
+                    Move(robot, attitudeParties, Direction.Right, Direction.Down);
+                }
+                else
+                {
+                    var attitudeParties = (height - 2) / (width - 2);
+                    Move(robot, attitudeParties, Direction.Down, Direction.Right);
+                }
+            }
+        }
     }
 }
