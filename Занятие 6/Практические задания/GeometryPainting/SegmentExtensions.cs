c345cf2f0e59ea13cd82d091889f38470bb61b76
savitar696 <karosen1448@gmail.com>
2024-10-21 12:12:36 +0300

    lesson 6: make GeometryPainting

--- Занятие 6/Практические задания/GeometryPainting/SegmentExtensions.cs
+++ Занятие 6/Практические задания/GeometryPainting/SegmentExtensions.cs
@@ -2,6 +2,21 @@
 using Avalonia.Media;
 using GeometryTasks;
 
-namespace GeometryPainting;
+namespace GeometryPainting {
+    public static class SegmentExtensions {
+        private static readonly Dictionary<Segment, Color> SegmentColors = new Dictionary<Segment, Color>();
 
-//Напишите здесь код, который заставит работать методы segment.GetColor и segment.SetColor
+        public static void SetColor(this Segment segment, Color color)
+        {
+            SegmentColors[segment] = color;
+        }
+        
+        public static Color GetColor(this Segment segment)
+        {
+            if (SegmentColors.TryGetValue(segment, out var color))
+            {
+                return color;
+            }
+            return Color.Black;
+        }
+}
