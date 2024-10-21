5787216fb6f2cb85f07654112ba0347b8a7ec4c6
Dasha2005-zxc <diddasha05@bk.ru>
2024-10-21 20:01:12 +0300

    make 3: lesson D

--- Занятие 3/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/D/Program.cs
@@ -14,15 +14,21 @@
         private static void WriteBoard(int size)
         {
 
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                  
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
-}
