ea50db00d8af65ef4553a0c08fd60afcac4b490a
savitar696 <karosen1448@gmail.com>
2024-10-21 11:49:22 +0300

    lesson 6: make B

--- Занятие 6/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 6/Практические задания/!ProgramTasks/B/Program.cs
@@ -12,12 +12,44 @@
         static void Main(string[] args)
         {
             GenerateMenu();
+            foreach (var menuItem in menu)
+            {
+                System.Console.WriteLine($"{menuItem.Caption} ({menuItem.HotKey})");
+                if (menuItem.Items != null)
+                {
+                    foreach (var subItem in menuItem.Items)
+                    {
+                        System.Console.WriteLine($"    {subItem.Caption} ({subItem.HotKey})");
+                    }
+                }
+            }
+            
         }
 
         public static MenuItem[] GenerateMenu()
         {
-            return new[] {
-            ...
+            return new[]
+            {
+                new MenuItem
+                {
+                    Caption = "File",
+                    HotKey = "F",
+                    Items = new[]
+                    {
+                        new MenuItem { Caption = "New", HotKey = "N", Items = null },
+                        new MenuItem { Caption = "Save", HotKey = "S", Items = null }
+                    }
+                },
+                new MenuItem
+                {
+                    Caption = "Edit",
+                    HotKey = "E",
+                    Items = new[]
+                    {
+                        new MenuItem { Caption = "Copy", HotKey = "C", Items = null },
+                        new MenuItem { Caption = "Paste", HotKey = "V", Items = null }
+                    }
+                }
             };
         }
 }
