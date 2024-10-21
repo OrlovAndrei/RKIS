ef8307d1adb9b2f6943efa0559ccbd519a4b779a
Basileios-afk <grom154825@gmail.com>
2024-10-21 21:30:37 +0300

    Lesson 3: make A

--- Занятие 3/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/B/Program.cs
@@ -1,27 +1,24 @@
-﻿namespace B
+﻿namespace A
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(RemoveStartSpaces("a"));
-            Console.WriteLine(RemoveStartSpaces(" b"));
-            Console.WriteLine(RemoveStartSpaces(" cd"));
-            Console.WriteLine(RemoveStartSpaces(" efg"));
-            Console.WriteLine(RemoveStartSpaces(" text"));
-            Console.WriteLine(RemoveStartSpaces(" two words"));
-            Console.WriteLine(RemoveStartSpaces("  two spaces"));
-            Console.WriteLine(RemoveStartSpaces("	tabs"));
-            Console.WriteLine(RemoveStartSpaces("		two	tabs"));
-            Console.WriteLine(RemoveStartSpaces("                             many spaces"));
-            Console.WriteLine(RemoveStartSpaces("               "));
-            Console.WriteLine(RemoveStartSpaces("\n\r line breaks are spaces too"));
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(2) == 4);
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(15) == 16);
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(-2) == 1);
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(-100) == 1);
+            Console.WriteLine(GetMinPowerOfTwoLargerThan(100) == 128);
         }
 
-        public static string RemoveStartSpaces(string text)
+        private static int GetMinPowerOfTwoLargerThan(int number)
         {
-            if (char.IsWhiteSpace(text[0])) return text.Substring(1);
-            else return text;
+            int result = 1;
+            while (result <= number)
+            {
+                result *= 2;
+            }
+            return result;
         }
     }
 }
