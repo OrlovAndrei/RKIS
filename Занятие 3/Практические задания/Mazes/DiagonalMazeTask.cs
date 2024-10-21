45735145307c9c4034627b0998d11929effa4c9e
Рудакова Алёна <arudakova436@gmail.com>
2024-10-21 13:05:41 +0300

    Update DiagonalMazeTask.cs

--- Занятие 3/Практические задания/Mazes/DiagonalMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/DiagonalMazeTask.cs
@@ -4,6 +4,11 @@ public static class DiagonalMazeTask
 {
     public static void MoveOut(Robot robot, int width, int height)
     {
-        
+         
+        for(int i = 0; i < width-3; i++) 
+            { robot.MoveTo(Direction.Right);}
+            for (int i = 0; i < height - 3; i++)
+            { robot.MoveTo(Direction.Down); }
+        }
     }
-}
+}  
