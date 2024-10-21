9131d42b4e00207d9a0fd138f511176880c3d029
Lussuru <Lussuru17@mail.ru>
2024-10-21 20:21:32 +0300

    lesson 1: make E

--- Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            string doubleNumber = "894376.243643";
-            int number = doubleNumber; // Вася уверен, что ошибка где-то тут
+            string doubleNumber = "894376,243643";
+            double number = double.Parse(doubleNumber);
             Console.WriteLine(number + 1);
         }
     }
