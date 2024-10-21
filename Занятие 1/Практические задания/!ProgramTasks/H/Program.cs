49d28f4f9644e2cc7972a3ca61b02ef5992a792b
Basileios-afk <grom154825@gmail.com>
2024-10-21 20:53:41 +0300

    Lesson 1: make H

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
+        static int GetSquare(int num)
+        {
+            return (int)Math.Sqrt(num);
+        }
+        static void Print(int number)
+        {
+            Console.WriteLine(number);
+        }
         static void Main(string[] args)
         {
             Print(GetSquare(42));
         }
+
     }
 }
