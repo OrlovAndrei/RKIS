59b036d725106ed46764667aad091158f8c77140
Nameless-gg <igor01.12.2004@mail.ru>
2024-10-22 01:40:54 +0300

    lesson 3: make mazes

--- Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
@@ -1,9 +1,35 @@
-﻿namespace Mazes;
+﻿using System.Drawing.Drawing2D;
+
+namespace Mazes;
 
 public static class SnakeMazeTask
 {
     public static void MoveOut(Robot robot, int width, int height)
     {
-
+        var s = width - 3;
+        while (true)
+        {
+            MoveRight(robot, s);
+            MoveDown(robot);
+            MoveLeft(robot, s);   
+            if (robot.Finished)
+                break;
+            MoveDown(robot);
+        }
+    }
+    public static void MoveDown(Robot robot)
+    {
+        for (var i = 0; i < 2; i++)
+            robot.MoveTo(Direction.Down);
+    }
+    public static void MoveLeft(Robot robot, int steps)
+    {
+        for (var i = 0; i < steps; i++)
+            robot.MoveTo(Direction.Left);
+    }
+    public static void MoveRight(Robot robot, int steps)
+    {
+        for (var i = 0; i < steps; i++)
+            robot.MoveTo(Direction.Right);
     }
 }
