f76557510a4c02619b4643d7137825beec1704ea
maxvlzzn <msms60837@gmail.com>
2024-10-22 14:22:33 +0300

    lesson 5: make A

--- Занятие 5/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 5/Практические задания/!ProgramTasks/A/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace A
+﻿using System;
+using System.Collections.Generic;
+
+namespace A
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string message = "решИла нЕ Упрощать и зашифРОВАтЬ Все послаНИЕдАже не Старайся нИЧЕГО у тЕбя нЕ получится с расшифРОВкойСдавайся НЕ твоего ума Ты не споСОбЕн Но может бытьесли особенно упорно подойдешь к делубудет Трудно конечнОКод ведЬ не из простыхочень ХОРОШИЙ кодто у тебя все получитсяи я буДу Писать тЕбЕ ещечао";
+            List<string> capitalWords = new List<string>();
+            string[] words = message.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                if (char.IsUpper(words[i][0]))
+                {
+                    capitalWords.Add(words[i]);
+                }
+            }
+            Console.WriteLine("Результат:");
+            foreach (string word in capitalWords)
+            {
+                Console.Write(word + " ");
+            }
         }
     }
 }
