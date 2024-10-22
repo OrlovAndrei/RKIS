cfa361bb745ba7a28a5b3ff4e6465b9d052bc866
Dhhahahs <nikitapodolniy13h@gmail.com>
2024-10-22 21:02:59 +0300

    lesson 3: MazeTask

--- Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
@@ -4,6 +4,33 @@ public static class SnakeMazeTask
 {
     public static void MoveOut(Robot robot, int width, int height)
     {
-
+        while (!robot.Finished)
+        {
+            MoveRight(robot, width, height);
+            MoveDown(robot, width, height);
+            MoveLeft(robot, width, height);
     }
+    if (!robot.Finished) MoveDown(robot, width, height);
+        }
+    }
+    public static void MoveRight(Robot robot, int width, int height)
+    {
+        while (robot.X != width - 2)
+        {
+            robot.MoveTo(Direction.Right);
+        }
+    }
+       public static void MoveDown(Robot robot, int width, int height)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            robot.MoveTo(Direction.Down);
+        }
+    }
+    public static void MoveLeft(Robot robot, int width, int height)
+    {
+        while (robot.X != 1)
+        {
+            robot.MoveTo(Direction.Left);
+        }
 }
