7c5e5c13853e1cdb4a9ee49fe78ca6a60ae4bcb4
savitar696 <karosen1448@gmail.com>
2024-10-22 14:34:53 +0300

    lesson 7: make C

--- Занятие 7/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 7/Практические задания/!ProgramTasks/C/Program.cs
@@ -11,9 +11,14 @@
             Console.WriteLine(MiddleOfThree(3.45, 2.67, 3.12));
         }
 
-        static ... MiddleOfThree(... a, ... b, ... c)
+        static T MiddleOfThree<T>(T a, T b, T c) where T : IComparable<T>
         {
-            ...
+            if ((a.CompareTo(b) > 0 && a.CompareTo(c) < 0) || (a.CompareTo(b) < 0 && a.CompareTo(c) > 0))
+                return a;
+            else if ((b.CompareTo(a) > 0 && b.CompareTo(c) < 0) || (b.CompareTo(a) < 0 && b.CompareTo(c) > 0))
+                return b;
+            else
+                return c;
         }
     }
 }
