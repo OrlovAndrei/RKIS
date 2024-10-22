e3b3c612c4cb6fbd49fd0ae1cc423afee62fe3af
GOL <164672521+accum104@users.noreply.github.com>
2024-10-22 13:57:34 +0300

    lesson2 make Pluralize

--- Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
+++ Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
@@ -8,3 +8,20 @@ public static class PluralizeTask
 		return "руб.";
 	}
 }
+ public static class PluralizeTask
+    {
+        public static string PluralizeRubles(int count)
+        {
+            if (count % 100 >= 11 && count % 100 <= 14)
+                return "рублей";
+
+            int lastDigit = count % 10;
+            if (lastDigit == 1)
+                return "рубль";
+            else if (lastDigit >= 2 && lastDigit <= 4)
+                return "рубля";
+            else
+                return "рублей";
+        }
+    }
+}
