d6f5263ee71530500784e7577653e203d78dcbb3
GOL <164672521+accum104@users.noreply.github.com>
2024-10-22 14:02:19 +0300

    lesson3 make A

--- Занятие 3/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/A/Program.cs
@@ -16,6 +16,8 @@
             int result = 1;
             while (...)
                 result = ...;
+            while (result <= number)
+                result *= 2;
             return result;
         }
     }
