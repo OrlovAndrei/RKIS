1d4b3d2a994e582f3a1513a6d69eb578f001941c
savitar696 <karosen1448@gmail.com>
2024-10-22 15:03:27 +0300

    lesson 7: make F

--- Занятие 7/Практические задания/!ProgramTasks/F/ClockwiseComparer.cs
+++ Занятие 7/Практические задания/!ProgramTasks/F/ClockwiseComparer.cs
@@ -4,7 +4,18 @@
     {
         public int Compare(Point x, Point y)
         {
+            double angleX = GetAngle(x);
+            double angleY = GetAngle(y);
 
+            return angleX.CompareTo(angleY);
+        }
+
+        public double GetAngle(Point point)
+        {
+            double angle = Math.Atan2(point.X, point.Y);
+            if (angle < 0) angle += 2 * Math.PI;
+
+            return angle;
         }
     }
 }
