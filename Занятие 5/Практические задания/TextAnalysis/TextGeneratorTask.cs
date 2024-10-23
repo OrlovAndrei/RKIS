26a1a1e67202441c29fc086208603b20bc67df2a
Basileios-afk <grom154825@gmail.com>
2024-10-23 17:51:55 +0300

    Lesson 5: make TextGeneratorTask

--- Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
@@ -1,12 +1,29 @@
-﻿namespace TextAnalysis;
-
-static class TextGeneratorTask
+﻿namespace TextAnalysis
 {
-    public static string ContinuePhrase(
+    static class TextGeneratorTask
+    {
+        public static string ContinuePhrase(
         Dictionary<string, string> nextWords,
         string phraseBeginning,
         int wordsCount)
-    {
-        return phraseBeginning;
+        {
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string[] splitWords = phraseBeginning.Split(' ');
+                if (splitWords.Length > 0)
+                {
+                    if (splitWords.Length >= 2 && nextWords.ContainsKey(splitWords[splitWords.Length - 2] + " " + splitWords[splitWords.Length - 1]))
+                        phraseBeginning += " " + nextWords[splitWords[splitWords.Length - 2] + " " + splitWords[splitWords.Length - 1]];
+
+                    else if (splitWords.Length >= 1 && nextWords.ContainsKey(splitWords[splitWords.Length - 1]))
+                        phraseBeginning += " " + nextWords[splitWords[splitWords.Length - 1]];
+
+                    else
+                        return phraseBeginning;
+                }
+            }
+
+            return phraseBeginning;
+        }
     }
 }
