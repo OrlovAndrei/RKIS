27b0759d809b2364c21d8c12200f3ae5a93151ea
42808 <42808@SRV-13>
2024-10-23 14:51:58 +0300

    Lesson 2: Rectangles

--- Занятие 2/Практические задания/Rectangles/Rectangle.cs
+++ Занятие 2/Практические задания/Rectangles/Rectangle.cs
@@ -1,22 +1,24 @@
-﻿namespace Rectangles;
-
-public class Rectangle
+﻿namespace Rectangles
 {
-    public readonly int Left, Top, Width, Height;
 
-    public Rectangle(int left, int top, int width, int height)
+    public class Rectangle
     {
-        Left = left;
-        Top = top;
-        Width = width;
-        Height = height;
-    }
+        public readonly int Left, Top, Width, Height;
 
-    public int Bottom => Top + Height;
-    public int Right => Left + Width;
+        public Rectangle(int left, int top, int width, int height)
+        {
+            Left = left;
+            Top = top;
+            Width = width;
+            Height = height;
+        }
 
-    public override string ToString()
-    {
-        return $"Left: {Left}, Top: {Top}, Width: {Width}, Height: {Height}";
+        public int Bottom => Top + Height;
+        public int Right => Left + Width;
+
+        public override string ToString()
+        {
+            return $"Left: {Left}, Top: {Top}, Width: {Width}, Height: {Height}";
+        }
     }
 }
