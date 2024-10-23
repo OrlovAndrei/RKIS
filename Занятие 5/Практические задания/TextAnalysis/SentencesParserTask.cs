44767fadfddacd74d785072adc7de7c730a39060
Basileios-afk <grom154825@gmail.com>
2024-10-23 16:22:04 +0300

    Lesson 5: make SentencesParserTask

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,11 +1,45 @@
-﻿namespace TextAnalysis;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-static class SentencesParserTask
+namespace TextAnalysis
 {
-    public static List<List<string>> ParseSentences(string text)
+    static class SentencesParserTask
     {
-        var sentencesList = new List<List<string>>();
-        //...
-        return sentencesList;
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var listSentences = new List<List<string>>();
+
+
+            if (text == null) return null;
+            text = text.ToLower();
+            var sentences = text.Split(".:;?!()".ToCharArray(),
+                StringSplitOptions.RemoveEmptyEntries);
+            foreach (var sent in sentences)
+            {
+                var listWords = new List<string>();
+                var builder = new StringBuilder();
+                foreach (var ch in sent)
+                {
+                    if (char.IsLetter(ch) || ch == '\'')
+                        builder.Append(ch);
+                    else
+                        AddNotEmptyWord(builder, listWords);
+                }
+                AddNotEmptyWord(builder, listWords);
+                if (listWords.Count > 0)
+                    listSentences.Add(listWords);
+            }
+            return listSentences;
+        }
+
+        public static void AddNotEmptyWord(StringBuilder builder, List<string> listWords)
+        {
+            if (builder.Length > 0)
+            {
+                listWords.Add(builder.ToString());
+                builder.Clear();
+            }
+        }
     }
-}
