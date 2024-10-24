a8a3fbb25d2b3b4a09837e9b7c5fe13609c054f4
Артем Рогов <arogov279@gmail.com>
2024-10-24 13:50:17 +0300

    занятие 4 С

--- Занятие 4/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/C/Program.cs
@@ -10,6 +10,12 @@
         public static int GetElementCount(int[] items, int itemToCount)
         {
             ...
+            int count = 0;
+            foreach (var item in items)
+            {
+                if (item == itemToCount) count++;
+            }
+            return count;
         }
     }
 }
