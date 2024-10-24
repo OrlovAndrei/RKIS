53ee9a4799732dc32bd770177dd8894625b7ddf1
AdamJensen197367 <konstantinpavluk1@gmail.com>
2024-10-24 11:29:33 +0300

    lesson 1: make A

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first step!");
+        }
+    }
+}
