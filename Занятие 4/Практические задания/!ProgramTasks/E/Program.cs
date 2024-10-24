cad455e76b6d0f608c202caa9091610f3e82ed6e
Артем Рогов <arogov279@gmail.com>
2024-10-24 13:52:48 +0300

    занятие 4 E

--- Занятие 4/Практические задания/!ProgramTasks/E/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/E/Program.cs
@@ -24,6 +24,8 @@
             else if (suit == Suits.Coins) return "монет";
             else if (suit == Suits.Cups) return "кубков";
             else return "мечей";
+            return new string[] { "жезлов", "монет", "кубков", "мечей" }[Convert.ToInt32(suit)];
+
         }
     }
 }
