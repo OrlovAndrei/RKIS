598a6ff93f6779f92764f529378391aa743698b6
Bogdan Metelkin <simpl.top48786@gmai.com>
2024-10-25 00:24:37 +0300

    lesson 6: make C

--- Занятие 6/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 6/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,5 +1,15 @@
-﻿namespace C
+﻿using System;
+
+namespace C
 {
+    public static class StringExtensions
+    {
+        public static int ToInt(this string str)
+        {
+            return int.TryParse(str, out var result) ? result : 0;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
