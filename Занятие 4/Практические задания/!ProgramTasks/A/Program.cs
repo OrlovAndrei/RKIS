d7d6f81af29a780678300df98b16c6f5923287d4
Артем Рогов <arogov279@gmail.com>
2024-10-24 13:49:15 +0300

    Занятие 4 А

--- Занятие 4/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/A/Program.cs
@@ -11,6 +11,10 @@
         public static int[] GetFirstEvenNumbers(int count)
         {
             ...
+            var array = new int[count];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = 2 * i + 2;
+            return array;
         }
 
     }
