d494183a6125c365f40de1992170dcd4ada8e34d
GOL <164672521+accum104@users.noreply.github.com>
2024-10-24 22:36:02 +0300

    lesson1 update D

--- Занятие 1/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/D/Program.cs
@@ -4,9 +4,10 @@
     {
         static void Main(string[] args)
         {
-            double amount = 1.11; 
-            int peopleCount = 60; 
-            int totalMoney = Math.Round(amount * peopleCount);
+            double amount = 1.11; //количество биткоинов от одного человека
+            int peopleCount = 60; // количество человек
+            int totalMoney = (int)amount * peopleCount; // ← исправьте ошибку в этой строке
+            int totalMoney = (int)Math.Round(amount * peopleCount); // ← ошибка исправлена в этой строке
             Console.WriteLine(totalMoney);
         }
     }
