f2cd41eb3d8dc63d5af511d18accaf3be1ec24c8
Bogdan Metelkin <simpl.top48786@gmai.com>
2024-10-25 00:22:54 +0300

    lesson 6: fix B

--- Занятие 6/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 6/Практические задания/!ProgramTasks/B/Program.cs
@@ -18,15 +18,25 @@
         public static MenuItem[] GenerateMenu()
         {
             return new[]
-           {
+            {
                 new MenuItem
                 {
                     Caption = "File",
                     HotKey = "F",
                     Items = new[]
-                    {
-                        new MenuItem { Caption = "New", HotKey = "N", Items = null },
-                        new MenuItem { Caption = "Save", HotKey = "S", Items = null }
+                     {
+                        new MenuItem 
+                        { 
+                            Caption = "New", 
+                            HotKey = "N", 
+                            Items = null 
+                        },
+                        new MenuItem 
+                        { 
+                            Caption = "Save", 
+                            HotKey = "S", 
+                            Items = null 
+                        }
                     }
                 },
                 new MenuItem
@@ -35,8 +45,18 @@
                     HotKey = "E",
                     Items = new[]
                     {
-                        new MenuItem { Caption = "Copy", HotKey = "C", Items = null },
-                        new MenuItem { Caption = "Paste", HotKey = "V", Items = null }
+                        new MenuItem 
+                        { 
+                            Caption = "Copy", 
+                            HotKey = "C", 
+                            Items = null 
+                        },
+                        new MenuItem 
+                        { 
+                            Caption = "Paste", 
+                            HotKey = "V", 
+                            Items = null 
+                        }
                     }
                 }
             };
