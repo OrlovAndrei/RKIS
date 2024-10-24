bc7a929b590da51bb329b60dcc88032ba637fdcc
GOL <164672521+accum104@users.noreply.github.com>
2024-10-24 22:52:46 +0300

    lesson1 update Risivatel

--- Занятие 1/Практические задания/Risovatel/DrawingProgram.cs
+++ Занятие 1/Практические задания/Risovatel/DrawingProgram.cs
@@ -2,62 +2,71 @@ using System;
 using Avalonia.Media;
 using RefactorMe.Common;
 
-namespace RefactorMe {
-    class Risovatel {
+namespace RefactorMe 
+{
+    class Risovatel 
+    {
         private float x, y;
         private IGraphics graphics;
 
-        public void Initialization(IGraphics graphics) {
-            graphics.Clear(Colors.Black);
+        public static void Initialization(IGraphics newGraphic) 
+        {
+            graphic = newGraphic;
+            //grafic.SmoothingMode = SmoothingMode.None;
+            graphic.Clear(Colors.Black);
         }
 
-        public void SetPosition(float x0, float y0) {
-            x = x0;
-            y = y0;
-        }
+        public static void SetPosition(float x0, float y0)
+        { x = x0; y = y0; }
 
-        public void MakeStep(Pen pen, double length, double corner) {
-            var x1 = (float)(x + length * Math.Cos(corner));
-            var y1 = (float)(y + length * Math.Sin(corner));
-            graphics.DrawLine(pen, x, y, x1, y1);
+        public static void MakeStep(Pen pen, double length, double angle)
+        {
+            //Делает шаг длиной length в направлении и рисует пройденную траекторию
+            var x1 = (float)(x + length * Math.Cos(angle));
+            var y1 = (float)(y + length * Math.Sin(angle));
+            graphic.DrawLine(pen, x, y, x1, y1);
             x = x1;
             y = y1;
         }
 
-        public void ChangePosition(double length, double corner) {
-            x = (float)(x + length * Math.Cos(corner));
-            y = (float)(y + length * Math.Sin(corner));
+        public static void ChangePositione(double length, double angle) 
+        {
+            x = (float)(x + length * Math.Cos(angle));
+            y = (float)(y + length * Math.Sin(angle));
         }
     }
 
-    public class ImpossibleSquare {
-        public static void Draw(int canvasWidth, int canvasHeight, double angle, IGraphics graphics) {
-        var drawer = new Risovatel();
-        drawer.Initialization(graphics);
+    public class ImpossibleSquare
+    {
+        public static void Draw(int width, int height, double RotationAngle, IGraphics graphic)
+        {
+            // RotationAngle пока не используется, но будет использоваться в будущем
+            Risovatel.Initialization(graphic);  
 
-        var squareSize = Math.Min(canvasWidth, canvasHeight);
-        var sideLength = Math.Sqrt(2) * (squareSize * 0.375f + squareSize * 0.04f) / 2;
+            var size = Math.Min(width, height);
 
-        var centerX = (float)(sideLength * Math.Cos(Math.PI / 4 + Math.PI)) + canvasWidth / 2f;
-        var centerY = (float)(sideLength * Math.Sin(Math.PI / 4 + Math.PI)) + canvasHeight / 2f;
-        drawer.SetPosition(centerX, centerY);
+            var diagonal_length = Math.Sqrt(2) * (size * 0.375f + size * 0.04f) / 2;
+            var x0 = (float)(diagonal_length * Math.Cos(Math.PI / 4 + Math.PI)) + width / 2f;
+            var y0 = (float)(diagonal_length * Math.Sin(Math.PI / 4 + Math.PI)) + height / 2f;
 
-        DrawSide(drawer, squareSize, Math.PI);
-        DrawSide(drawer, squareSize, Math.PI + Math.PI / 2);
-        DrawSide(drawer, squareSize, Math.PI + Math.PI);
-        DrawSide(drawer, squareSize, Math.PI / 2);
+            Risovatel.SetPosition(x0, y0);
+
+            DrawSide(size, 0);
+            DrawSide(size, -Math, PI / 2);
+            DrawSide(size, Math.PI);
+            DrawSide(size, Math.PI / 2)
+        }
+
+        private static void DrawSide(int size, double angle)
+        {
+            Risovatel.MakeStep(new Pen(Brushes.Yellow), size * 0.375f, 0 + angle);
+            Risovatel.MakeStep(new Pen(Brushes.Yellow), size * 0.04f * Math.Sqrt(2), Math.PI / 4 + angle);
+            Risovatel.MakeStep(new Pen(Brushes.Yellow), size * 0.375f, Math.PI + angle);
+            Risovatel.MakeStep(new Pen(Brushes.Yellow), size * 0.375f - size * 0.04f, Math.PI / 2 + angle);
+
+            Risovatel.ChangePosition(size * 0.04f, angle - Math.PI);
+            Risovatel.ChangePosition(size * 0.04f * Math.Sqrt(2), angle + 3 * Math.PI / 4);
+        }
+
+        }
     }
-
-    private static void DrawSide(Risovatel drawer, float size, double startingAngle) {
-        var pen = new Pen(Brushes.Yellow);
-
-        drawer.MakeStep(pen, size * 0.375f, startingAngle);
-        drawer.MakeStep(pen, size * 0.04f * Math.Sqrt(2), startingAngle + Math.PI / 4);
-        drawer.MakeStep(pen, size * 0.375f, startingAngle + Math.PI);
-        drawer.MakeStep(pen, size * 0.375f - size * 0.04f, startingAngle + Math.PI / 2);
-
-        drawer.ChangePosition(size * 0.04f, startingAngle - Math.PI);
-        drawer.ChangePosition(size * 0.04f * Math.Sqrt(2), startingAngle + 3 * Math.PI / 4);
-    }
-}
-}
