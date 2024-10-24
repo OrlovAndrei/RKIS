a20c0daacdb0dc929c1a0c37bcf4caf9dca2d1c0
Артем Рогов <arogov279@gmail.com>
2024-10-24 13:53:46 +0300

    занятие 4  G

--- Занятие 4/Практические задания/!ProgramTasks/G/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/G/Program.cs
@@ -26,6 +26,11 @@
         public static int[] GetPoweredArray(int[] arr, int power)
         {
             ...
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (int)Math.Pow(i + 1, power);
+            }
+            return arr;
         }
     }
 }
