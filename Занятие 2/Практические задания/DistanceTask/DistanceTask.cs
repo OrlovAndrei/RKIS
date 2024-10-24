2cde78ecc37e3b7c0fba93d4b24687d947f4a83b
GOL <164672521+accum104@users.noreply.github.com>
2024-10-24 23:05:20 +0300

    lesson2 update DistanceTask

--- Занятие 2/Практические задания/DistanceTask/DistanceTask.cs
+++ Занятие 2/Практические задания/DistanceTask/DistanceTask.cs
@@ -7,7 +7,6 @@ public static class DistanceTask
     // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
     public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
     {
-        return 0.0;
         double ABx = bx - ax;
         double ABy = by - ay;
         double APx = x - ax;
