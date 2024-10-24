a9a361a37c66388378397012b1f99b807250ae7b
Артем Рогов <arogov279@gmail.com>
2024-10-24 13:51:37 +0300

    занятие 4 D

--- Занятие 4/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/D/Program.cs
@@ -16,5 +16,17 @@ namespace D
                     return i;
             return -1;
         }
+        static bool ContainsAtIndex(int[] array, int[] subArray, int index)
+        {
+            for (int j = 0; j < subArray.Length; j++)
+            {
+                if (array[index + j] != subArray[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
     }
 }
