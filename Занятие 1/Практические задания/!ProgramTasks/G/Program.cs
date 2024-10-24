10570631c989a551355a36f7b3f35f3b50292452
Kurt337 <tema.sitnikov05@gmail.com>
2024-10-24 22:05:10 +0300

    lesson 1 make G

--- Занятие 1/Практические задания/!ProgramTasks/G/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/G/Program.cs
@@ -5,7 +5,7 @@
         private static string GetGreetingMessage(string name, double salary)
         {
             // возвращает "Hello, <name>, your salary is <salary>"
-            ...
+            return $"Hello, {name}, your salary is {Math.Ceiling(salary)}"
         }
 
         static void Main(string[] args)
