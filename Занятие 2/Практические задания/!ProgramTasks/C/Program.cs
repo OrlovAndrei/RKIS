4aafcc15a389aa0aad58bbc65a3a1cfebc4c2fdc
Bogdan Metelkin <simpl.top48786@gmai.com>
2024-10-24 20:37:50 +0300

    lesson 2: make C

--- Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
@@ -7,18 +7,25 @@
             Console.WriteLine(MiddleOf(5, 0, 100)); // => 5
             Console.WriteLine(MiddleOf(12, 12, 11)); // => 12
             Console.WriteLine(MiddleOf(1, 1, 1)); // => 1
-            Console.WriteLine(MiddleOf(2, 3, 2));
-            Console.WriteLine(MiddleOf(8, 8, 8));
-            Console.WriteLine(MiddleOf(5, 0, 1));
+            Console.WriteLine(MiddleOf(2, 3, 2)); // => 2
+            Console.WriteLine(MiddleOf(8, 8, 8)); // => 8
+            Console.WriteLine(MiddleOf(5, 0, 1)); // => 1
+            Console.WriteLine(MiddleOf(1, 2, 3));
+            Console.WriteLine(MiddleOf(4, 5, 6));
+            Console.WriteLine(MiddleOf(7, 9, 2));
+            Console.WriteLine(MiddleOf(999, 9999, 99999));
+            Console.WriteLine(MiddleOf(6, 3, 2));
+            Console.WriteLine(MiddleOf(4, 0, 4));
         }
 
         public static int MiddleOf(int a, int b, int c)
         {
-            if (a > b)
-                if (b > c) return b;
-                else if (a > c) return ...
-            else
-                ...
+            if ((a >= b) && (b >= c) || (c >= b) && (b >= a)) 
+                return b;
+            else if ((a >= c) && (c >= b) || (b >= c) && (c >= a)) 
+                return c;
+            else 
+                return a;
         }
     }
 }
