9e0a3c9e33615bf008ccf456aa01cdcc91cb233f
Bogdan Metelkin <simpl.top48786@gmai.com>
2024-10-24 20:21:20 +0300

    lesson 1: make G

--- Занятие 1/Практические задания/!ProgramTasks/G/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/G/Program.cs
@@ -4,8 +4,14 @@
     {
         private static string GetGreetingMessage(string name, double salary)
         {
-            // возвращает "Hello, <name>, your salary is <salary>"
-            ...
+            string namee = (string)name;
+            int salaryy = (intintMath.Ceiling(salary);
+            string s1 = "Hello, ";
+            string s2 = namee;
+            string s3 = ", your salary is";
+            string s4 = salaryy.ToSpring();
+            string total = s1 + s2 + s3 + s4;
+            return total;
         }
 
         static void Main(string[] args)
