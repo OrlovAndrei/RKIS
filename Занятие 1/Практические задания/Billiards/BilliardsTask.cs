b191ba9f404cb5a1e6da5e3d78a903350928d3f5
Agni <63151335+crash36663@users.noreply.github.com>
2024-10-25 15:09:42 +0300

    Update BilliardsTask.cs

--- Занятие 1/Практические задания/Billiards/BilliardsTask.cs
+++ Занятие 1/Практические задания/Billiards/BilliardsTask.cs
@@ -1,20 +1,25 @@
-namespace Billiards;
-
-public static class BilliardsTask
+using System;
+namespace Billiards
 {
-    static double radToDeg(double rad)
+    public static class BilliardsTask
     {
-        return (rad * 180 )/ Math.PI;
-    }
- 
-    static double degToRad(double deg)
-    {
-        return (Math.PI * deg) / 180;
-    }
- 
-    public static double BounceWall(double directionRadians, double wallInclinationRadians)
-    {
- 
-        return degToRad(360.0 - radToDeg(directionRadians) + 2.0 * radToDeg(wallInclinationRadians));
+        static double radToDeg (double rad)
+        {
+            return (rad * 180) / Math.PI;
+        }
+        static double degToRad(double deg)
+        {
+            return (Math.PI * deg) / 180;
+        }
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            return degToRad(180.0 - radToDeg(directionRadians) + 2.0 * radToDeg(wallInclinationRadians));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(radToDeg(BounceWall(degToRad(45.0), degToRad(0.0))));
+            Console.WriteLine(radToDeg(BounceWall(degToRad(45.0), degToRad(90.0))));
+            Console.ReadLine();
+        }
     }
 }
