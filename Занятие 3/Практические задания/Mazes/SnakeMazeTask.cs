1e07167330c5e40fce970b5b03f7a54818b245e2
darkarat <dar.karat3011@gmail.com>
2024-10-25 21:31:42 +0300

    Lesson 3: made SnakeMazeTask

--- Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
@@ -4,6 +4,25 @@ public static class SnakeMazeTask
 {
     public static void MoveOut(Robot robot, int width, int height)
     {
+        int X = width - 3;
+        int Y = 2;
 
+        while (robot.Finished == false)  //пока не достиг финиша
+        {
+            if (robot.Finished == false)  //если не достиг финиша, то в циклах проверяем до каких пор движется
+            {
+                for (int i = 0; i < X; i++)  // -->
+                { robot.MoveTo(Direction.Right); }
+                for (int i = 0; i < Y; i++)         //|
+                { robot.MoveTo(Direction.Down); }  // v
+                for (int i = 0; i < X; i++)
+                { robot.MoveTo(Direction.Left); }  // -->
+            }
+            if (robot.Finished == false)
+            {
+                for (int i = 0; i < Y; i++)
+                { robot.MoveTo(Direction.Down); } // какой-то робот дурачок не выходит из лабиринта до конца
+            }
+        }
     }
 }
