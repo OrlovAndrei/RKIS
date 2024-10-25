a28a4a6f17cb51bb8037471f24251899a46f4297
Мария <mirajkaano@gmail.com>
2024-10-25 20:44:21 +0300

    Lesson 1 Percentages

---
+++ Занятие 1/Практические задания/Percentages/Percentages/Percentages/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Program
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate(Console.ReadLine()));
+            Console.ReadLine();
+        }
+
+        public static string Calculate(string userInput)
+        {
+            var initialMoney = double.Parse(userInput.Split()[0]);
+            var yearlyPercent = double.Parse(userInput.Split()[1]);
+            var monthsCount = int.Parse(userInput.Split()[2]);
+            return "\n" + Convert.ToString(initialMoney * Math.Pow((1 + yearlyPercent / 100 / 12), monthsCount));
+        }
+
+    }
+}
