f07f84d0c459f5f8c49c18936fbecf9209e8fa5a
MilkaX681XX <gritsenko.vova988@mail.ru>
2024-10-25 22:16:47 +0300

    Lesson 2 make DistanceTask

--- Занятие 2/Практические задания/DistanceTask/DistanceTask.cs
+++ Занятие 2/Практические задания/DistanceTask/DistanceTask.cs
@@ -8,5 +8,25 @@ public static class DistanceTask
     public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
     {
         return 0.0;
+         double abx = bx - ax;
+        double aby = by - ay;
+        double apx = x - ax;
+        double apy = y - ay;
+        double bpx = x - bx;
+        double bpy = y - by;
+        double ab2 = abx * abx + aby * aby;
+        double ap_ab = apx * abx + apy * aby;
+        if (ab2 == 0)
+            return Math.Sqrt(apx * apx + apy * apy);
+         double t = ap_ab / ab2;
+        if (t < 0)
+            return Math.Sqrt(apx * apx + apy * apy);
+        else if (t > 1)
+            return Math.Sqrt(bpx * bpx + bpy * bpy);
+        double projx = ax + t * abx;
+        double projy = ay + t * aby;
+        double dx = projx - x;
+        double dy = projy - y;
+            return Math.Sqrt(dx * dx + dy * dy);
     }
 }
