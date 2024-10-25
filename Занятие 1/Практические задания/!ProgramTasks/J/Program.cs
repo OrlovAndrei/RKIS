a96f6e42fe9cb9a9e496a9ea5a12c5be59efa297
Dhhahahs <maxim.sheva06@gmail.com>
2024-10-26 01:56:30 +0300

    lesson 1: make J

--- Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine{ 'Hello, World!'};
-            var numher = 5,5;
+            Console.WriteLine{ "Hello, World!"};
+            var numher = 5.5;
             number += 7;
             Console.Writeline(number):
         }
