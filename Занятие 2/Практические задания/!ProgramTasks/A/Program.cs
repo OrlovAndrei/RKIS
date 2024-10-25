019ce78cf468e8558ad123ede2b7fbdad98c1913
MilkaX681XX <gritsenko.vova988@mail.ru>
2024-10-25 20:53:31 +0300

    lesson 2: make A

--- Занятие 2/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/A/Program.cs
@@ -4,14 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(IsLeapYear(2014) == false);
-            Console.WriteLine(IsLeapYear(1999) == false);
-            Console.WriteLine(IsLeapYear(8992) == true);
-            Console.WriteLine(IsLeapYear(1) == false);
-            Console.WriteLine(IsLeapYear(14) == false);
-            Console.WriteLine(IsLeapYear(400) == true);
-            Console.WriteLine(IsLeapYear(600) == false);
-            Console.WriteLine(IsLeapYear(3200) == true);
+           Console.WriteLine(IsLeapYear(2014)); //false
+            Console.WriteLine(IsLeapYear(1999)); //false
+            Console.WriteLine(IsLeapYear(8992)); //true
+            Console.WriteLine(IsLeapYear(1)); //false
+            Console.WriteLine(IsLeapYear(14)); //false
+            Console.WriteLine(IsLeapYear(400)); //true
+            Console.WriteLine(IsLeapYear(600)); //false
+            Console.WriteLine(IsLeapYear(3200)); //true 
         }
 
         public static bool IsLeapYear(int year)
