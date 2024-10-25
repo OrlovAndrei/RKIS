f1692e09657b9391012d295c0a818084f80420ea
aroman-hash <aromanovskaia2023@mail.ru>
2024-10-25 21:25:18 +0300

    lesson 3: make A/B/C/D

--- Занятие 3/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/C/Program.cs
@@ -13,6 +13,15 @@
 
         private static void WriteTextWithBorder(string text)
         {
+            string top = null!;
+            for (int i = 0; i < text.Length + 2; i++)
+            {
+                top += "-";
+            }
+            Console.WriteLine($"+{top}+");
+            Console.WriteLine($"| {text} |");
+            Console.WriteLine($"+{top}+");
+
         }
     }
 }
