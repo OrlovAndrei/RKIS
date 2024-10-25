be0f76389e973db9cdc8d88b676a010e87b50817
aroman-hash <aromanovskaia2023@mail.ru>
2024-10-25 21:22:56 +0300

    lesson 2: make A/B/C/D

--- Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
@@ -14,11 +14,14 @@
 
         public static int MiddleOf(int a, int b, int c)
         {
-            if (a > b)
-                if (b > c) return b;
-                else if (a > c) return ...
-            else
-                ...
+            if ((a >= b) && (b >= c) || (c >= b) && (b >= a)) // проверяем b по всем возможным сценариям
+                return b;
+            else if ((a >= c) && (c >= b) || (b >= c) && (c >= a)) // проверяем c по всем возможным сценариям
+                return c;
+            else // a можно не проверять так как он крайний вариант
+                return a;
+
+
         }
     }
 }
