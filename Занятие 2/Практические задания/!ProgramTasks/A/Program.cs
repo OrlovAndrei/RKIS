9199d30fa7311608ee1137a047070b06dab234fd
kartoxaArtem <darwincompanyfilms@gmail.com>
2024-10-26 10:42:52 +0300

    lesson 2: make A

--- Занятие 2/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/A/Program.cs
@@ -1,22 +1,22 @@
-﻿namespace A
+namespace A
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(IsLeapYear(2014) == false);
-            Console.WriteLine(IsLeapYear(1999) == false);
-            Console.WriteLine(IsLeapYear(8992) == true);
-            Console.WriteLine(IsLeapYear(1) == false);
-            Console.WriteLine(IsLeapYear(14) == false);
-            Console.WriteLine(IsLeapYear(400) == true);
-            Console.WriteLine(IsLeapYear(600) == false);
-            Console.WriteLine(IsLeapYear(3200) == true);
+            Console.WriteLine(IsLeapYear(2014)); //false
+            Console.WriteLine(IsLeapYear(1999)); //false
+            Console.WriteLine(IsLeapYear(8992));  //true
+            Console.WriteLine(IsLeapYear(1)); //false
+            Console.WriteLine(IsLeapYear(14)); //false
+            Console.WriteLine(IsLeapYear(400)); //true
+            Console.WriteLine(IsLeapYear(600)); //false
+            Console.WriteLine(IsLeapYear(3200)); //true
         }
 
         public static bool IsLeapYear(int year)
         {
-            return (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0))
+            return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
         }
     }
 }
