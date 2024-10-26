df0cced02f38cc30d781ac9ac02c103785a65653
wrzla <wrzla@ya.ru>
2024-10-26 13:17:13 +0300

    Program.cs

--- Занятие 2/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/A/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace A
+
+﻿using System;
+namespace A
 {
     internal class Program
     {
@@ -16,7 +18,8 @@
 
         public static bool IsLeapYear(int year)
         {
-            return year % 4 == 0;
+            if (year % 400 == 0 && year % 4 == 0 && !(year % 100 == 0)) return false;
+            else return true;
         }
     }
 }
