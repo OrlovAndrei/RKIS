08ab3690ab2dd93801072b4f3a656e9f3e13cdfa
kartoxaArtem <darwincompanyfilms@gmail.com>
2024-10-26 12:51:29 +0300

    lesson 3: make DragonFractalTask

--- Занятие 3/Практические задания/DragonFractal/DragonFractalTask.cs
+++ Занятие 3/Практические задания/DragonFractal/DragonFractalTask.cs
@@ -6,31 +6,29 @@ internal static class DragonFractalTask
 {
     public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
     {
-        double x = 1.0;
-            double y = 0.0;
-
-            var random = new Random(seed);
-
-            for (int i = 0; i < iterationsCount; i++) {
-                int nextNumber = random.Next(2);
-
-                if (nextNumber == 0) {
-                    double cos45 = Math.Cos(Math.PI / 4);
-                    double sin45 = Math.Sin(Math.PI / 4);
-                    double newX = (x * cos45 - y * sin45) / Math.Sqrt(2);
-                    double newY = (x * sin45 + y * cos45) / Math.Sqrt(2);
-                    x = newX;
-                    y = newY;
-                } else {
-                    double cos135 = Math.Cos(3 * Math.PI / 4);
-                    double sin135 = Math.Sin(3 * Math.PI / 4);
-                    double newX = (x * cos135 - y * sin135) / Math.Sqrt(2) + 1;
-                    double newY = (x * sin135 + y * cos135) / Math.Sqrt(2);
-                    x = newX;
-                    y = newY;
-                }
-
-                pixels.SetPixel((int)Math.Round(x), (int)Math.Round(y));
+        var x = 1.0;
+        var y = 0.0;
+        var angle45 = Math.PI * 45 / 180;
+        var angle135 = Math.PI * 135 / 180;
+        var rand = new Random(seed);
+        for (int i = 0; i < iterationsCount; i++)
+        {
+            var nextNum = rand.Next(1, 3);
+            if (nextNum == 1)
+            {
+                var x1 = (x * Math.Cos(angle45) - y * Math.Sin(angle45)) / Math.Sqrt(2);
+                var y1 = (x * Math.Sin(angle45) + y * Math.Cos(angle45)) / Math.Sqrt(2);
+                x = x1;
+                y = y1;
             }
+            if (nextNum == 2)
+            {
+                var x1 = (x * Math.Cos(angle135) - y * Math.Sin(angle135)) / Math.Sqrt(2) + 1;
+                var y1 = (x * Math.Sin(angle135) + y * Math.Cos(angle135)) / Math.Sqrt(2);
+                x = x1;
+                y = y1;
+            }
+            pixels.SetPixel(x, y);
+        }
     }
 }
