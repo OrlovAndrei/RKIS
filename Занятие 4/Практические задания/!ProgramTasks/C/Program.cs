1a7b1f52fa865eec503e7733863113e3113c5bef
artem-boost <155313634+artem-boost@users.noreply.github.com>
2024-10-26 10:40:46 +0000

    lsn4 make C

--- Занятие 4/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/C/Program.cs
@@ -4,12 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetElementCount(new[] {1, 1, 2, 1}, 1));
+            Console.WriteLine(GetElementCount(new[] { 1, 1, 2, 1 }, 1));
         }
 
         public static int GetElementCount(int[] items, int itemToCount)
         {
-            ...
+            int count = 0; // Переменная для хранения количества вхождений
+            foreach (var item in items) // Перебираю элементы массива
+            {
+                if (item == itemToCount) // Проверяю, совпадает ли элемент с искомым
+                {
+                    count++; // Увеличиваю счетчик
+                }
+            }
+            return count; // Возвращаю количество вхождений
         }
     }
 }
