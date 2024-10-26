3d664829639937f01e82a830fd753fe88fbac1d1
User <User@DESKTOP-LPL27TD>
2024-10-26 21:15:21 +0300

    Lesson 3: C

--- Занятие 3/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/C/Program.cs
@@ -13,6 +13,15 @@
 
         private static void WriteTextWithBorder(string text)
         {
+            string middleFrame = "| " + text + " |";
+            string topFrame = "";
+            for (int i = 0; i < middleFrame.Length; i++)
+            {
+                if (i == 0 || i == middleFrame.Length - 1) topFrame += "+";
+                else topFrame += "-";
+            }
+            string bottomFrame = topFrame;
+            Console.WriteLine(topFrame + "\n" + middleFrame + "\n" + bottomFrame);
         }
     }
 }
