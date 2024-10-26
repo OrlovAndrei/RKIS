df1bccdcf789c7967bbe3f7e6930693788e7918e
027 <027@J27>
2024-10-26 23:19:59 +0300

    Lesson 2: PluralizeTask

--- Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
+++ Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
@@ -6,5 +6,8 @@ public static class PluralizeTask
 	{
 		// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
 		return "руб.";
-	}
+        if ((count % 100 > 10 && count % 100 < 20) || (count % 10 > 4 && count % 10 < 10) || count % 10 == 0)
+            return "рублей";
+        return count % 10 == 1 ? "рубль" : "рубля";
+    }
 }
