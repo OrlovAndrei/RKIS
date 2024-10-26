54dadf5ab2a7f568056dff70968c92e2d2282fc3
Артем Рогов <arogov279@gmail.com>
2024-10-26 18:56:10 +0300

    занятие 2 В

--- Занятие 2/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/B/Program.cs
@@ -16,10 +16,9 @@
 
         public static bool IsCorrectMove(string from, string to)
         {
-            var dx = Math.Abs(to[0] - from[0]); 
-            var dy = Math.Abs(to[1] - from[1]); 
-
-            return dx == 0 || dy == 0 || dx == dy;
+            var dx = Math.Abs(to[0] - from[0]); //смещение фигуры по горизонтали
+            var dy = Math.Abs(to[1] - from[1]); //смещение фигуры по вертикали
+            return from != to && (dx == 0 || dy == 0 || dx == dy);
         }
     }
 }
