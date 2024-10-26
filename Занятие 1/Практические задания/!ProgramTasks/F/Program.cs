ef31daed35034b62e340039c15df4b93afab4e21
wrzla <wrzla@ya.ru>
2024-10-26 12:50:42 +0300

    Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/F/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/F/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var a = 5; // ← исправьте эту строку
+            double a = 5; // ← исправьте эту строку
             a += 0.5;
             Console.WriteLine(a);
         }
