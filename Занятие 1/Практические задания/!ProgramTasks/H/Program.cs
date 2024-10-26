40775f5bba70a0f3f8d824fc1f36bdad6331371d
40614 <40614@SRV-18>
2024-10-26 15:13:01 +0300

    Lession 1: make H

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace H
+﻿﻿namespace H
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Print(GetSquare(42));
+        }
+        public static int GetSquare(int a)
+        {
+            return (int)Math.Pow(a, 2);
+        }
+        static void Print(int a)
+        {
+            Console.WriteLine(a.ToString());
         }
     }
 }
