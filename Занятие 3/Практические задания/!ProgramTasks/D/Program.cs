5193535bc2a01efbe113d26a7b627630d0823079
User <User@DESKTOP-LPL27TD>
2024-10-26 21:15:53 +0300

    Lesson 3: D

--- Занятие 3/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/D/Program.cs
@@ -13,15 +13,25 @@
 
         private static void WriteBoard(int size)
         {
+            string symbol = "#";
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(symbol);
+                for (int k = 0; k < size - 1; k++)
+                {
+                    if (symbol == "#") symbol = ".";
+                    else if (symbol == ".") symbol = "#";
+                    Console.Write(symbol);
+                }
 
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
-            Console.WriteLine("#.#.#.#.");
-            Console.WriteLine(".#.#.#.#");
+                if (size % 2 != 0)
+                {
+                    if (symbol == "#") symbol = ".";
+                    else if (symbol == ".") symbol = "#";
+                }
+
+                Console.WriteLine();
+            }
             Console.WriteLine();
         }
     }
