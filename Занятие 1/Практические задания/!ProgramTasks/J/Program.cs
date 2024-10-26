02633516dee70984db0824871b5908235049fb5b
Serafim <serafimperminov21@gmail.com>
2024-10-26 11:25:53 +0300

    leson 1: make J

--- Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
@@ -1,13 +1,15 @@
-﻿namespace J
+﻿using System;
+
+namespace J
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine{ 'Hello, World!'};
-            var numher = 5,5;
-            number += 7;
-            Console.Writeline(number):
+            Console.WriteLine("Hello, World!"); // Исправлено: использован правильный синтаксис для Console.WriteLine
+            var number = 5.5; // Исправлено: заменено на правильный синтаксис для объявления переменной с плавающей запятой
+            number += 7; // Исправлено: переменная должна называться "number"
+            Console.WriteLine(number); // Исправлено: правильный синтаксис для Console.WriteLine
         }
     }
 }
