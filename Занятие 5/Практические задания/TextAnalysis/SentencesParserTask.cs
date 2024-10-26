7296ca03f07afedf39cbb0bee709eab059238d00
kartoxaArtem <darwincompanyfilms@gmail.com>
2024-10-26 13:23:32 +0300

    lesson 5: make SentencesParserTask

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,11 +1,47 @@
-﻿namespace TextAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-static class SentencesParserTask
+namespace TextAnalysis
 {
-    public static List<List<string>> ParseSentences(string text)
+    static class SentencesParserTask
     {
-        var sentencesList = new List<List<string>>();
-        //...
-        return sentencesList;
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var sentenceDelimiters = new[] { '.', '!', '?', ';', ':', '(', ')' };
+            var sentences = text.Split(sentenceDelimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sentence in sentences)
+            {
+                // Разделяем предложение на слова
+                var words = sentence.Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(word => new string(word.Where(c => char.IsLetter(c) || c == '\'').ToArray()))
+                    .Where(word => !string.IsNullOrEmpty(word))
+                    .Select(word => word.ToLowerInvariant())
+                    .ToList();
+
+                if (words.Count > 0)
+                {
+                    sentencesList.Add(words);
+                }
+            }
+
+            return sentencesList;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Привет! Как дела? Это пример текста: Hello, world! What's up?";
+            var sentences = SentencesParserTask.ParseSentences(text);
+
+            foreach (var sentence in sentences)
+            {
+                Console.WriteLine(string.Join(", ", sentence));
+            }
+        }
     }
 }
