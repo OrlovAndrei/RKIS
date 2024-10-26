5d37c0fd12547dab7d303b4f7cac38cf51d2375c
ALi-ctrl-boop <allaoa215@bk.ru>
2024-10-26 19:10:22 +0300

    lesson 4; make G

--- Занятие 4/Практические задания/!ProgramTasks/G/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/G/Program.cs
@@ -1,23 +1,15 @@
-﻿namespace G
-{
-    internal class Program
+static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
             var arrayToPower = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
             // Метод PrintArray уже написан за вас
             PrintArray(GetPoweredArray(arrayToPower, 1));
-
             // если вы будете менять исходный массив, то следующие два теста сработают неверно:
             PrintArray(GetPoweredArray(arrayToPower, 2));
             PrintArray(GetPoweredArray(arrayToPower, 3));
-
             // не забывайте про частные случаи:
             PrintArray(GetPoweredArray(new int[0], 1));
             PrintArray(GetPoweredArray(new[] { 42 }, 0));
-        }
-
+    }
         public static void PrintArray(int[] arr) {
             foreach (var item in arr)
                 Console.WriteLine(item);
@@ -25,7 +17,11 @@
 
         public static int[] GetPoweredArray(int[] arr, int power)
         {
-            ...
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (int)Math.Pow(i + 1, power);
+            }
+            return arr;
         }
     }
 }
