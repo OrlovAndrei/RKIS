1c65523fb3e83e31f9060d2f46fbcae7a70b87d2
027 <027@J27>
2024-10-26 23:53:01 +0300

    Lesson3: DiagonalMazeTask

--- Занятие 3/Практические задания/Mazes/DiagonalMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/DiagonalMazeTask.cs
@@ -4,6 +4,7 @@ namespace Mazes
 {
     public static class DiagonalMazeTask
     {
+        public static void MoveOut(Robot robot, int width, int height)
         {
             var firstDirection = ChooseDirection(width, height);
             var secondDirection = ChangeDirection(firstDirection);
