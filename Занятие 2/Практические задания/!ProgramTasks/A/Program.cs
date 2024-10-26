3cea87fe6bb866b48d7c54647ccc43b1e251d59e
Артем Рогов <arogov279@gmail.com>
2024-10-26 18:52:27 +0300

    занятие 2 А

--- Занятие 2/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/A/Program.cs
@@ -16,8 +16,8 @@
 
         public static bool IsLeapYear(int year)
         {
-            bool isLeapYear = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
-            return isLeapYear; 
+            return year % 4 == 0;
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
         }
     }
 }
