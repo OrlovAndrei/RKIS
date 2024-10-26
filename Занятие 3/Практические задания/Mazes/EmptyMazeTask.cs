5e0a5b26fcac9d0b120f51eda0d2f95c2089f8e6
027 <027@J27>
2024-10-27 00:05:19 +0300

    lesson3: EmptyMazeTask

--- Занятие 3/Практические задания/Mazes/EmptyMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/EmptyMazeTask.cs
@@ -1,9 +1,23 @@
-﻿namespace Mazes;
-
-public static class EmptyMazeTask
+﻿namespace Mazes
 {
-	public static void MoveOut(Robot robot, int width, int height)
-	{
-		
-	}
+    public static class EmptyMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            GoX(robot, width);
+            GoY(robot, height);
+        }
+
+        private static void GoX(Robot robot, int coords)
+        {
+            while (robot.X != (coords - 2))
+                robot.MoveTo(Direction.Right);
+        }
+
+        private static void GoY(Robot robot, int coords)
+        {
+            while (robot.Y != (coords - 2))
+                robot.MoveTo(Direction.Down);
+        }
+    }
 }
