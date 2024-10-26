f3812f1a114912fdc34f1a63908186e9917421e9
027 <027@J27>
2024-10-26 23:29:31 +0300

    Lesson3: A

--- Занятие 3/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/A/Program.cs
@@ -14,8 +14,8 @@
         private static int GetMinPowerOfTwoLargerThan(int number)
         {
             int result = 1;
-            while (...)
-                result = ...;
+            while(result <= number)
+                result = result * 2;
             return result;
         }
     }
