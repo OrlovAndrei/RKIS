e25576c9cd00f3d182605b99ebc7c920a02702e5
Serafim <serafimperminov21@gmail.com>
2024-10-26 11:13:24 +0300

    leson 1: make E

--- Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
@@ -5,8 +5,11 @@
         static void Main(string[] args)
         {
             string doubleNumber = "894376.243643";
-            int number = doubleNumber; // Вася уверен, что ошибка где-то тут
-            Console.WriteLine(number + 1);
+            // Преобразуем строку в double, а затем в int
+            double parsedDouble = double.Parse(doubleNumber); // Преобразуем строку в double
+            int number = (int)parsedDouble; // Приводим double к int (это приведет к потере дробной части)
+
+            Console.WriteLine(number + 1); // Выводим результат
         }
     }
 }
