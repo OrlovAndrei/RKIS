4aa06ac97f2c6596994d953a516c3c02d2e6bb5b
wrzla <wrzla@ya.ru>
2024-10-26 13:47:29 +0300

    RectanglesTask.cs

--- Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
+++ Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
@@ -1,27 +1,42 @@
+
 using System;
-
-namespace Rectangles;
-
-public static class RectanglesTask
+ 
+namespace Rectangles
 {
-    // Пересекаются ли два прямоугольника (пересечение только по границе также считается пересечением)
-    public static bool AreIntersected(Rectangle r1, Rectangle r2)
+    public static class RectanglesTask
     {
-        // так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
-        return true;
-    }
-
-    // Площадь пересечения прямоугольников
-    public static int IntersectionSquare(Rectangle r1, Rectangle r2)
-    {
-        return 0;
-    }
-
-    // Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
-    // Иначе вернуть -1
-    // Если прямоугольники совпадают, можно вернуть номер любого из них.
-    public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
-    {
-        return -1;
+        public static bool AreIntersected(Rectangle r1, Rectangle r2)
+        {
+            return !(r1.Left > r2.Right || r1.Top > r2.Bottom || r1.Right < r2.Left || r1.Bottom < r2.Top);
+        }
+ 
+        public static int IntersectionSquare(Rectangle r1, Rectangle r2)
+        {
+            if (AreIntersected(r1, r2))
+            {
+                if (r1.Left == r2.Right || r1.Top == r2.Bottom || r1.Right == r2.Left || r1.Bottom == r2.Top)
+                    return 0;
+                else
+                {
+                    int maximumLeft = Math.Max(r1.Left, r2.Left);
+                    int minimumLeftWeight = Math.Min(r1.Right, r2.Right);
+                    int maximumTop = Math.Max(r1.Top, r2.Top);
+                    int minimumTopHeight = Math.Min(r1.Bottom, r2.Bottom);
+                    return (minimumLeftWeight - maximumLeft) * (minimumTopHeight - maximumTop);
+                }
+            }
+            return 0;
+        }
+ 
+        public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
+        {
+            if (r1.Left <= r2.Left && r1.Right >= r2.Right && r1.Bottom >= r2.Bottom && r1.Top <= r2.Top)
+                return 1;
+            else if
+                (r1.Left >= r2.Left && r1.Right <= r2.Right && r1.Bottom <= r2.Bottom && r1.Top >= r2.Top)
+                return 0;
+            else
+                return -1;              
+        }
     }
 }
