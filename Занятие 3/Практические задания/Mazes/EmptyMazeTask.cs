15898d5495f69a023a1854d7781d8032ec32caa0
ALi-ctrl-boop <allaoa215@bk.ru>
2024-10-26 18:57:58 +0300

    lesson 3; make EmptyMaze

--- Занятие 3/Практические задания/Mazes/EmptyMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/EmptyMazeTask.cs
@@ -2,8 +2,23 @@
 
 public static class EmptyMazeTask
 {
-	public static void MoveOut(Robot robot, int width, int height)
+{
+public static void MoveOut(Robot robot, int width, int height)
 	{
-		
-	}
+        for (int i = 0; robot.Finished != true; i++)
+        {
+            RobotMoveRight(robot, width, height, i);
+            RobotMoveDown(robot, width, height, i);
+        }
+    }
+    public static void RobotMoveRight(Robot robot, int width, int height, int i)
+    {
+        if (i < width - 3) robot.MoveTo(Direction.Right);
+    }
+    public static void RobotMoveDown(Robot robot, int width, int height, int i)
+    {
+        if (i < height - 3) robot.MoveTo(Direction.Down);
+    }
+}
+	
 }
