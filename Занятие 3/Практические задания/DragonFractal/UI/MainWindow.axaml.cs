12768fafae323571707e67ee23c3f7ad199058eb
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-10-27 22:24:02 +0300

    Татаров Дмитрий3833

--- Занятие 3/Практические задания/DragonFractal/UI/MainWindow.axaml.cs
+++ Занятие 3/Практические задания/DragonFractal/UI/MainWindow.axaml.cs
@@ -6,40 +6,41 @@ using Avalonia.Media;
 using Color = System.Drawing.Color;
 using Image = Avalonia.Controls.Image;
 
-namespace Fractals.UI;
+namespace Fractals.UI
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-        Opened += (_,__) =>
+   public MainWindow()
         {
-            ShowImageInWindow(CreateImage());
-        };
-    }
-
-    private static Bitmap CreateImage()
-    {
-        var pixels = new Pixels();
-        var image = new Bitmap(800, 800, PixelFormat.Format24bppRgb);
-        using (var g = Graphics.FromImage(image))
-        {
-            g.Clear(Color.Black);
+             InitializeComponent();
+            Opened += (_, __) =>
+            {
+                ShowImageInWindow(CreateImage());
+            };
         }
+
+                    private static Bitmap CreateImage()
+        {
+            var pixels = new Pixels();
+            var image = new Bitmap(800, 800, PixelFormat.Format24bppRgb);
+            using (var g = Graphics.FromImage(image))
+            {
+
+            }
 
         DragonFractalTask.DrawDragonFractal(pixels, 100000, 123456);
         pixels.DrawToBitmap(image);
-
-        return image;
     }
 
-    private void ShowImageInWindow(Bitmap image)
-    {
-        var imageControl = this.Find<Image>("Bitmap");
-        var stream = new MemoryStream();
-        image.Save(stream, ImageFormat.Bmp);
-        stream.Position = 0;
-        imageControl.Source = new Avalonia.Media.Imaging.Bitmap(stream);
-    }
+                   return image;
+        }
+
+        private void ShowImageInWindow(Bitmap image)
+        {
+            var imageControl = this.Find<Image>("Bitmap");
+            var stream = new MemoryStream();
+            image.Save(stream, ImageFormat.Bmp);
+            stream.Position = 0;
+            imageControl.Source = new Avalonia.Media.Imaging.Bitmap(stream);
+        }
 }
