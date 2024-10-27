ba44b44df15f001da8f2ea189866aa7f510567ea
Виталик Марфин <marfinvitalik70@gmail.com>
2024-10-27 11:45:01 +0300

    lesson1:H

--- Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
@@ -12,6 +12,7 @@
             Console.WriteLine(GetLastHalf("I love CSharp!"));
             Console.WriteLine(GetLastHalf("1234567890"));
             Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+
         }
     }
 }
