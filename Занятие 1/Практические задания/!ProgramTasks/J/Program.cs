c60a801c3c171f6e226c68aaabf10b4752c1421a
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-10-27 19:57:09 +0300

    Татаров Дмитрий 3833

--- Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/J/Program.cs
@@ -6,8 +6,11 @@
         {
             Console.WriteLine{ 'Hello, World!'};
             var numher = 5,5;
+            Console.WriteLine("Hello, World!");
+            var number = 5.5;
             number += 7;
             Console.Writeline(number):
+            Console.Writeline(number);
         }
     }
 }
