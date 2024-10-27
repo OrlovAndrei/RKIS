d62d956b4934519f399b9231623c08c899be7056
Serafim <serafimperminov21@gmail.com>
2024-10-27 20:25:07 +0300

    leson 1: make angrybirds

--- Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
+++ Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
@@ -1,11 +1,34 @@
 using System;
 
-namespace AngryBirds;
-
-public static class AngryBirdsTask
+namespace AngryBirds
 {
-    public static double FindSightAngle(double v, double distance)
+    public static class AngryBirdsTask
     {
-        return Math.PI / 4;
+        public static double FindSightAngle(double v, double distance)
+        {
+            const double g = 9.81; // Ускорение свободного падения
+            
+            // Проверка на допустимость
+            if (v <= 0 || distance < 0)
+            {
+                return double.NaN;
+            }
+
+            // Вычисляем дискриминант
+            double discriminant = v * v * v * v - g * (g * distance * distance);
+            
+            // Если дискриминант меньше нуля, решения не существует
+            if (discriminant < 0)
+            {
+                return double.NaN;
+            }
+
+            // Вычисляем два возможных угла
+            double angle1 = Math.Atan((v * v + Math.Sqrt(discriminant)) / (g * distance));
+            double angle2 = Math.Atan((v * v - Math.Sqrt(discriminant)) / (g * distance));
+
+            // Возвращаем наименьший угол
+            return Math.Min(angle1, angle2);
+        }
     }
 }
