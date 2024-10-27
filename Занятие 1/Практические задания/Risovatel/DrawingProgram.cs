15e484cf09750200420a59532bed183907975e91
Али <allaoa215@bk.ru>
2024-10-27 19:12:40 +0300

    lesson 1; make DrawingProgram

--- Занятие 1/Практические задания/Risovatel/DrawingProgram.cs
+++ Занятие 1/Практические задания/Risovatel/DrawingProgram.cs
@@ -1,75 +1,68 @@
-﻿using System;
+﻿﻿using System;
 using Avalonia.Media;
-
+using RefactorMe.Common;
 namespace RefactorMe
 {
-    class Painter
+    class Artist
     {
-        private static float currentX, currentY;
-        private static IGraphics graphics;
+        static float x, y;
+        static IGraphics graphics;
 
-        private const float SquareSideRatio = 0.375f;
-        private const float LineThickness = 0.04f;
-
-        public static void Initialize(IGraphics newGraphics)
+        public static void Initialization(IGraphics NewGraphics)
         {
-            graphics = newGraphics;
+            graphics = NewGraphics;
             graphics.Clear(Colors.Black);
         }
 
-        public static void SetPosition(float x, float y)
+        public static void SetPosition(float x0, float y0)
         {
-            currentX = x;
-            currentY = y;
+            x = x0;
+            y = y0;
         }
 
-        public static void DrawLine(Pen pen, double length, double angle)
+        public static void makeIt(Pen pen, double length, double angle)
         {
-            var xEnd = (float)(currentX + length * Math.Cos(angle));
-            var yEnd = (float)(currentY + length * Math.Sin(angle));
-            graphics.DrawLine(pen, currentX, currentY, xEnd, yEnd);
-            currentX = xEnd;
-            currentY = yEnd;
+            var x1 = (float)(x + length * Math.Cos(angle));
+            var y1 = (float)(y + length * Math.Sin(angle));
+            graphics.DrawLine(pen, x, y, x1, y1);
+            x = x1;
+            y = y1;
         }
 
-        public static void ChangePosition(double length, double angle)
+        public static void Change(double length, double angle)
         {
-            currentX += (float)(length * Math.Cos(angle));
-            currentY += (float)(length * Math.Sin(angle));
+            x = (float)(x + length * Math.Cos(angle));
+            y = (float)(y + length * Math.Sin(angle));
         }
     }
 
     public class ImpossibleSquare
     {
-        public static void Draw(int width, int height, double rotationAngle, IGraphics graphics)
+        public static void Draw(int width, int height, double angle, IGraphics graphics)
         {
-            Painter.Initialize(graphics);
+            Artist.Initialize(graphics);
 
-            var squareSize = Math.Min(width, height);
-            var diagonalLength = Math.Sqrt(2) * (squareSize * SquareSideRatio + squareSize * LineThickness) / 2;
-            var centerX = width / 2f;
-            var centerY = height / 2f;
+            var size = Math.Min(width, height);
 
-            var startX = (float)(diagonalLength * Math.Cos(Math.PI / 4 + Math.PI)) + centerX;
-            var startY = (float)(diagonalLength * Math.Sin(Math.PI / 4 + Math.PI)) + centerY;
+            var diagonal_length = Math.Sqrt(2) * (size * 0.375f + size * 0.04f) / 2;
+            var x0 = (float)(diagonal_length * Math.Cos(Math.PI / 4 + Math.PI)) + width / 2f;
+            var y0 = (float)(diagonal_length * Math.Sin(Math.PI / 4 + Math.PI)) + height / 2f;
 
-            Painter.SetPosition(startX, startY);
-
-            for (int i = 0; i < 4; i++)
-            {
-                DrawSquareSide(squareSize, i * Math.PI / 2);
-            }
+            Artist.Set_position(x0, y0);
+            DrawSide(size, 0);
+            DrawSide(size, -Math.PI / 2);
+            DrawSide(size, Math.PI);
+            DrawSide(size, Math.PI / 2);
         }
 
-        private static void DrawSquareSide(int size, double startAngle)
+        private static void DrawSide(int size, double angle)
         {
-            Painter.DrawLine(new Pen(Brushes.Yellow), size * SquareSideRatio, startAngle);
-            Painter.DrawLine(new Pen(Brushes.Yellow), size * LineThickness * Math.Sqrt(2), startAngle + Math.PI / 4);
-            Painter.DrawLine(new Pen(Brushes.Yellow), size * SquareSideRatio, startAngle + Math.PI);
-            Painter.DrawLine(new Pen(Brushes.Yellow), size * SquareSideRatio - size * LineThickness, startAngle + Math.PI / 2);
-
-            Painter.ChangePosition(size * LineThickness, startAngle + Math.PI);
-            Painter.ChangePosition(size * LineThickness * Math.Sqrt(2), startAngle + 3 * Math.PI / 4);
+            Artist.MakeIt(new Pen(Brushes.Yellow), size * 0.375f, angle);
+            Artist.MakeIt(new Pen(Brushes.Yellow), size * 0.04f * Math.Sqrt(2), angle + Math.PI / 4);
+            Artist.MakeIt(new Pen(Brushes.Yellow), size * 0.375f, angle + Math.PI);
+            Artist.MakeIt(new Pen(Brushes.Yellow), size * 0.375f - size * 0.04f, angle + Math.PI / 2);
+            Artist.Change(size * 0.04f, angle + Math.PI);
+            Artist.Change(size * 0.04f * Math.Sqrt(2), angle + 3 * Math.PI / 4);
         }
     }
 }
