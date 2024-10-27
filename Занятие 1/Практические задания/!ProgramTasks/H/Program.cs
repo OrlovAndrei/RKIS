4b4f5a21f1e2ea0518a42583bd88e70cfabdeb47
Али <allaoa215@bk.ru>
2024-10-27 19:11:08 +0300

    lesson 1; make H

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -1,20 +1,18 @@
-﻿using System;
-
-namespace H
+﻿namespace H
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int baseNum = 42;
-            int stepen = 2;
-            int GetSquare = 1;
-
-            for (int i = 0; i < stepen; i++)
-            {
-                GetSquare *= baseNum;
-            }
-            Console.WriteLine(GetSquare);
+            Print(GetSquare(42));
+        }
+        public static int GetSquare(int a)
+        {
+            return (int)Math.Pow(a, 2);
+        }
+        static void Print(int a)
+        {
+            Console.WriteLine(a.ToString());
         }
     }
 }
