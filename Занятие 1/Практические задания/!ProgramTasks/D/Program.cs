aaca7837c17d2d9128cd80a283485e55948c2e60
Виталик Марфин <marfinvitalik70@gmail.com>
2024-10-27 11:34:34 +0300

    lesson 1:d

--- Занятие 1/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/D/Program.cs
@@ -8,6 +8,7 @@
             int peopleCount = 60; // количество человек
             int totalMoney = (int)Math.Round(amount * peopleCount); // ← исправьте ошибку в этой строке
             Console.WriteLine(totalMoney);
+
         }
     }
 }
