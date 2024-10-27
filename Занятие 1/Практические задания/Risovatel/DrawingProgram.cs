0cea6a71941b5602e6907b255c2c9f2441f7b298
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-10-27 22:51:14 +0300

    Татаров Дмитрий3833

--- Занятие 1/Практические задания/Risovatel/DrawingProgram.cs
+++ Занятие 1/Практические задания/Risovatel/DrawingProgram.cs
@@ -4,26 +4,17 @@ using RefactorMe.Common;
 
 namespace RefactorMe
 {
-    class Risovatel
     class Draw
     {
-        static float x, y;
-        static IGraphics grafika;
         static float X, Y;
         static IGraphics graphics;
 
-        public static void Initialization ( IGraphics novayaGrafika )
         public static void Initialize(IGraphics newGraphic)
         {
-            grafika = novayaGrafika;
             graphics = newGraphic;
             //grafika.SmoothingMode = SmoothingMode.None;
-            grafika.Clear(Colors.Black);
             ClearScreen();
         }
-
-        public static void set_position(float x0, float y0)
-        {x = x0; y = y0;}
                 public static void SetPosition(float x0, float y0)
         {
             X = x0;
@@ -34,11 +25,6 @@ namespace RefactorMe
         public static void DrawLine(Pen ruchka, double dlina, double ugol)
         {
         //Делает шаг длиной dlina в направлении ugol и рисует пройденную траекторию
-        var x1 = (float)(x + dlina * Math.Cos(ugol));
-        var y1 = (float)(y + dlina * Math.Sin(ugol));
-        grafika.DrawLine(ruchka, x, y, x1, y1);
-        x = x1;
-        y = y1;
             var x1 = (float)(X + dlina * Math.Cos(ugol));
             var y1 = (float)(Y + dlina * Math.Sin(ugol));
             graphics.DrawLine(ruchka, X, Y, x1, y1);
@@ -50,7 +36,6 @@ namespace RefactorMe
             Y = (float)(Y + dlina * Math.Sin(ugol));
         }
 
-        public static void Change(double dlina, double ugol)
         private static void ClearScreen()
         {
             x = (float)(x + dlina * Math.Cos(ugol)); 
@@ -65,13 +50,6 @@ namespace RefactorMe
     public static void Draw(int shirina, int visota, double ugolPovorota, IGraphics grafika)
     {
         // ugolPovorota пока не используется, но будет использоваться в будущем
-        Risovatel.Initialization(grafika);
-
-        var sz = Math.Min(shirina, visota);
-
-        var diagonal_length = Math.Sqrt(2) * (sz * 0.375f + sz * 0.04f) / 2;
-        var x0 = (float)(diagonal_length * Math.Cos(Math.PI / 4 + Math.PI)) + shirina / 2f;
-        var y0 = (float)(diagonal_length * Math.Sin(Math.PI / 4 + Math.PI)) + visota / 2f;
                 private static void DrawSide(float sideLength, float thickness, double angle)
         {
             Draw.DrawLine(new Pen(Brushes.Yellow), sideLength, angle);
@@ -80,12 +58,6 @@ namespace RefactorMe
             Draw.DrawLine(new Pen(Brushes.Yellow), sideLength - thickness, angle + Math.PI / 2);
         }
 
-        Risovatel.set_position(x0, y0);
-        //Рисуем 1-ую сторону
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f, 0);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.04f * Math.Sqrt(2), Math.PI / 4);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f, Math.PI);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f - sz * 0.04f, Math.PI / 2);
             public static void Draw(int shirina, int visota, double ugolPovorota, IGraphics graphics)
         {
             Draw.Initialize(graphics);
@@ -93,42 +65,20 @@ namespace RefactorMe
         Risovatel.Change(sz * 0.04f, -Math.PI);
         Risovatel.Change(sz * 0.04f * Math.Sqrt(2), 3 * Math.PI / 4);
 
-        //Рисуем 2-ую сторону
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f, -Math.PI / 2);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.04f * Math.Sqrt(2), -Math.PI / 2 + Math.PI / 4);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f, -Math.PI / 2 + Math.PI);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f - sz * 0.04f, -Math.PI / 2 + Math.PI / 2);
         var diagonalLength = Math.Sqrt(2) * ((SIDE_LENGTH + THICKNESS) / 2);
             var x0 = (float)(diagonalLength * Math.Cos(Math.PI / 4 + Math.PI)) + shirina / 2f;
             var y0 = (float)(diagonalLength * Math.Sin(Math.PI / 4 + Math.PI)) + visota / 2f;
 
-        Risovatel.Change(sz * 0.04f, -Math.PI / 2 - Math.PI);
-        Risovatel.Change(sz * 0.04f * Math.Sqrt(2), -Math.PI / 2 + 3 * Math.PI / 4);
         Draw.SetPosition(x0, y0);
 
-        //Рисуем 3-ю сторону
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f, Math.PI);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.04f * Math.Sqrt(2), Math.PI + Math.PI / 4);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f, Math.PI + Math.PI);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f - sz * 0.04f, Math.PI + Math.PI / 2);
-
-        Risovatel.Change(sz * 0.04f, Math.PI - Math.PI);
-        Risovatel.Change(sz * 0.04f * Math.Sqrt(2), Math.PI + 3 * Math.PI / 4);
         DrawSide(SIDE_LENGTH, THICKNESS, 0);
         Draw.Change(THICKNESS, -Math.PI);
         Draw.Change(THICKNESS * Math.Sqrt(2), 3 * Math.PI / 4);
 
-        //Рисуем 4-ую сторону
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f, Math.PI / 2);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.04f * Math.Sqrt(2), Math.PI / 2 + Math.PI / 4);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f, Math.PI / 2 + Math.PI);
-        Risovatel.makeIt(new Pen(Brushes.Yellow), sz * 0.375f - sz * 0.04f, Math.PI / 2 + Math.PI / 2);
         DrawSide(SIDE_LENGTH, THICKNESS, -Math.PI / 2);
         Draw.Change(THICKNESS, -Math.PI / 2 - Math.PI);
         Draw.Change(THICKNESS * Math.Sqrt(2), -Math.PI / 2 + 3 * Math.PI / 4);
 
-        Risovatel.Change(sz * 0.04f, Math.PI / 2 - Math.PI);
-        Risovatel.Change(sz * 0.04f * Math.Sqrt(2), Math.PI / 2 + 3 * Math.PI / 4);
         DrawSide(SIDE_LENGTH, THICKNESS, Math.PI / 2);
         Draw.Change(THICKNESS, Math.PI / 2 - Math.PI);
         Draw.Change(THICKNESS * Math.Sqrt(2), Math.PI / 2 + 3 * Math.PI / 4);
