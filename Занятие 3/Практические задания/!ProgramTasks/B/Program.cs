50134c4e0cfbd7bbf8fe1416dd4c493f3e33b7bd
Kymis417 <susukilowing@yandex.ru>
2024-10-27 16:18:31 +0300

    lesson3: make B

--- Занятие 3/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/B/Program.cs
@@ -1,25 +1,30 @@
-﻿namespace A
+﻿namespace B
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetMinPowerOfTwoLargerThan(2) == 4);
-            Console.WriteLine(GetMinPowerOfTwoLargerThan(15) == 16);
-            Console.WriteLine(GetMinPowerOfTwoLargerThan(-2) == 1);
-            Console.WriteLine(GetMinPowerOfTwoLargerThan(-100) == 1);
-            Console.WriteLine(GetMinPowerOfTwoLargerThan(100) == 128);
+            Console.WriteLine(RemoveStartSpaces("a"));
+            Console.WriteLine(RemoveStartSpaces(" b"));
+            Console.WriteLine(RemoveStartSpaces(" cd"));
+            Console.WriteLine(RemoveStartSpaces(" efg"));
+            Console.WriteLine(RemoveStartSpaces(" text"));
+            Console.WriteLine(RemoveStartSpaces(" two words"));
+            Console.WriteLine(RemoveStartSpaces("  two spaces"));
+            Console.WriteLine(RemoveStartSpaces("	tabs"));
+            Console.WriteLine(RemoveStartSpaces("		two	tabs"));
+            Console.WriteLine(RemoveStartSpaces("                             many spaces"));
+            Console.WriteLine(RemoveStartSpaces("               "));
+            Console.WriteLine(RemoveStartSpaces("\n\r line breaks are spaces too"));
         }
 
-        private static int GetMinPowerOfTwoLargerThan(int number)
-        }
-            int result = 1;
-            while (true)
+        public static string RemoveStartSpaces(string text)
+        {
+            while (text.Length > 0 && char.IsWhiteSpace(text[0]))
             {
-                if (result > number) break;
-                if (result = number) continue;
-                result += result;
+                text = text.Substring(1);
             }
-            return result;
+            return text;
         }
     }
+}
