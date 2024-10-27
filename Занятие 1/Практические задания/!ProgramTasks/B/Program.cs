a02a5004ef0a377e4bddb342214a6edf60327db6
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-10-27 22:38:25 +0300

    Татаров Дмитрий3833

--- Занятие 1/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/B/Program.cs
@@ -4,10 +4,6 @@
     {
         static void Main(string[] args)
         {
-            ... num1 = +5.5e-2;
-            ... num2 = 7.8f;
-            ... num3 = 0;
-            ... num4 = 2000000000000L;
             double num1 = +5.5e-2;
             float num2 = 7.8f;
             int num3 = 0;
