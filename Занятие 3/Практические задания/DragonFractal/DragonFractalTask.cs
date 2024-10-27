f35076c4b59f649cd096837caff9b6d6ff1080b6
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-10-27 22:03:06 +0300

    Татаров Дмитрий3833

--- Занятие 3/Практические задания/DragonFractal/DragonFractalTask.cs
+++ Занятие 3/Практические задания/DragonFractal/DragonFractalTask.cs
@@ -3,6 +3,9 @@
 namespace Fractals;
 
 internal static class DragonFractalTask
+namespace Fractals
+{    
+    internal static class DragonFractalTask
 {
     public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
     {
@@ -25,5 +28,33 @@ internal static class DragonFractalTask
         2. Нарисуйте текущую точку методом pixels.SetPixel(x, y)
 
         */
+        public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+        {
+            var x = 1.0;
+            var y = 0.0;
+            var angle45 = Math.PI * 45 / 180;
+            var angle135 = Math.PI * 135 / 180;
+            var random = new Random(seed);
+            for (int i = 0; i < iterationsCount; i++)
+            {
+                var nextNumber = random.Next(1, 3);
+                if (nextNumber == 1)
+                {
+                    var x1 = (x * Math.Cos(angle45) - y * Math.Sin(angle45)) / Math.Sqrt(2);
+                    var y1 = (x * Math.Sin(angle45) + y * Math.Cos(angle45)) / Math.Sqrt(2);
+                    x = x1; // !!!
+                    y = y1; // !!!
+                }
+                if (nextNumber == 2)
+                {
+                    var x1 = (x * Math.Cos(angle135) - y * Math.Sin(angle135)) / Math.Sqrt(2) + 1;
+                    var y1 = (x * Math.Sin(angle135) + y * Math.Cos(angle135)) / Math.Sqrt(2);
+                    x = x1; // !!!
+                    y = y1; // !!!
+                }
+                pixels.SetPixel(x, y);
+
+            }
+        }
     }
 }
