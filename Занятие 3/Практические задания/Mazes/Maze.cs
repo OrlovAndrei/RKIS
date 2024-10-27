a587303d7b71eb1745eb7ef89505cae79bef25b7
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-10-27 22:33:10 +0300

    Татаров Дмитрий3833

--- Занятие 3/Практические задания/Mazes/Maze.cs
+++ Занятие 3/Практические задания/Mazes/Maze.cs
@@ -2,34 +2,35 @@
 using System.Linq;
 using Avalonia;
 
-namespace Mazes;
+namespace Mazes
 
 public class Maze
 {
-    private readonly bool[][] walls;
-
-    public Maze(string[] lines)
+    public class Maze
     {
-        Robot = ParsePoint(lines[0]);
-        Exit = ParsePoint(lines[1]);
-        walls = ParseWalls(lines.Skip(2));
-        Size = new Size(walls.Max(row => row.Length), walls.Length);
+        private readonly bool[][] walls;
     }
+    public Maze(string[] lines)
+        {
+            Robot = ParsePoint(lines[0]);
+            Exit = ParsePoint(lines[1]);
+            walls = ParseWalls(lines.Skip(2));
+            Size = new Size(walls.Max(row => row.Length), walls.Length);
+        }
 
-    public Point Robot { get; }
-    public Point Exit { get; }
-    public Size Size { get; }
+        public Point Robot { get; }
+        public Point Exit { get; }
+        public Size Size { get; }
 
     private static Point ParsePoint(string s)
     {
         var coords = s.Split(' ').Select(int.Parse).ToArray();
         return new Point(coords[0], coords[1]);
     }
-
-    public bool IsWall(Point pos)
-    {
-        return walls[(int)pos.Y][(int)pos.X];
-    }
+            public bool IsWall(Point pos)
+        {
+            return walls[(int)pos.Y][(int)pos.X];
+        }
 
     private static bool[][] ParseWalls(IEnumerable<string> lines)
     {
