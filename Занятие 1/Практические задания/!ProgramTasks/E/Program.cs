0a05fc36a66ac75673165b939478be112e263552
Tigran469 <martirosian.13.tigran@gmail.com>
2024-10-28 17:22:03 +0300

    lesson 1: make E
    там должен был быть этот текст, почему изменились не знаю
    // Вася уверен, что ошибка где-то тут

--- Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
@@ -7,7 +7,7 @@ namespace E
         static void Main(string[] args)
         {
             string doubleNumber = "894376.243643";
-            double number = double.Parse(doubleNumber, CultureInfo.InvariantCulture); // ���� ������, ��� ������ ���-�� ���
+            double number = double.Parse(doubleNumber, CultureInfo.InvariantCulture); // Вася уверен, что ошибка где-то тут
             Console.WriteLine(number + 1);
         }
     }
