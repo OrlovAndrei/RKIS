66b66d671ab93a74326e4f3d91c6ed029437e8a6
Vlada-en <Edenisova73@mail.ru>
2024-10-28 23:30:10 +0300

    lesson 5: TextGenerator

--- Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
@@ -6,7 +6,24 @@ static class TextGeneratorTask
         Dictionary<string, string> nextWords,
         string phraseBeginning,
         int wordsCount)
+
     {
+        for (int i = 0; i < wordsCount; i++)
+        {
+            string[] splitWords = phraseBeginning.Split(' ');
+            if (splitWords.Length > 0)
+            {
+                if (splitWords.Length >= 2 && nextWords.ContainsKey(splitWords[splitWords.Length - 2] + " " + splitWords[splitWords.Length - 1]))
+                    phraseBeginning += " " + nextWords[splitWords[splitWords.Length - 2] + " " + splitWords[splitWords.Length - 1]];
+
+                else if (splitWords.Length >= 1 && nextWords.ContainsKey(splitWords[splitWords.Length - 1]))
+                    phraseBeginning += " " + nextWords[splitWords[splitWords.Length - 1]];
+
+                else
+                    return phraseBeginning;
+            }
+        }
+        
         return phraseBeginning;
     }
 }
