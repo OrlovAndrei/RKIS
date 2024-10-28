f0315ce5448722d12121de139fe7544bcf1b56a0
habibylin <postnikovaleks0@gmail.com>
2024-10-28 12:55:44 +0300

    lesson4 C

--- Занятие 4/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,18 +1,18 @@
 ﻿namespace C
-{
-    static void Main(string[] args)
+ {
+        static void Main(string[] args)
         {
-            double pi = Math.PI;
-            int tenThousand = 10000L;
-            float tenThousandPi = pi * tenThousand;
-            int roundedTenThousandPi = tenThousandPi;
-            int integerPartOfTenThousandPi = tenThousandPi;
-            int tenThousand =(int)10000L;
-            float tenThousandPi =(float)(pi * tenThousand);
-            int roundedTenThousandPi =(int)Math.Round(tenThousandPi);
-            int integerPartOfTenThousandPi =(int)tenThousandPi;
-            Console.WriteLine(integerPartOfTenThousandPi);
-            Console.WriteLine(roundedTenThousandPi);
+            Console.WriteLine(GetElementCount(new[] {1, 1, 2, 1}, 1));
+            Console.WriteLine(GetElementCount(new[] { 1, 1, 2, 1 }, 1));
+        }
+
+        public static int GetElementCount(int[] items, int itemToCount)
+        {
+            ...
+            int count = 0;
+            foreach (var e in items)
+                if (e == itemToFind) count++;
+            return count;
         }
     }
 }
