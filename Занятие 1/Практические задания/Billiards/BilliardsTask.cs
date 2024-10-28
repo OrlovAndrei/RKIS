4ebe73e22ae3ddfb4151cf51fd570f5b36df7527
Serafim <serafimperminov21@gmail.com>
2024-10-28 17:52:03 +0300

    lesson 1: make billiardstask

--- Занятие 1/Практические задания/Billiards/BilliardsTask.cs
+++ Занятие 1/Практические задания/Billiards/BilliardsTask.cs
@@ -1,16 +1,31 @@
-namespace Billiards;
+using System;
 
-public static class BilliardsTask
+namespace Billiards
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="directionRadians">Угол направления движения шара</param>
-    /// <param name="wallInclinationRadians">Угол</param>
-    /// <returns></returns>
-    public static double BounceWall(double directionRadians, double wallInclinationRadians)
+    public static class BilliardsTask
     {
-        //TODO
-        return 0.0;
+        /// <summary>
+        /// Вычисляет угол отражения шарика от стены.
+        /// </summary>
+        /// <param name="directionRadians">Угол направления движения шара.</param>
+        /// <param name="wallInclinationRadians">Угол наклона стены.</param>
+        /// <returns>Угол отражения в радианах.</returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            // Вычисляем угол отражения
+            double angleOfReflection = 2 * wallInclinationRadians - directionRadians;
+
+            // Приводим угол к диапазону [0, 2π) для удобства
+            while (angleOfReflection < 0)
+            {
+                angleOfReflection += 2 * Math.PI;
+            }
+            while (angleOfReflection >= 2 * Math.PI)
+            {
+                angleOfReflection -= 2 * Math.PI;
+            }
+
+            return angleOfReflection;
+        }
     }
 }
