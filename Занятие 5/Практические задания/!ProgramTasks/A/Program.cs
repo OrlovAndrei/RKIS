ba61d9ef58abc7eba37ab9d2ea1b64e93c6832fe
habibylin <postnikovaleks0@gmail.com>
2024-10-28 13:05:54 +0300

    LESSON5 A

--- Занятие 5/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 5/Практические задания/!ProgramTasks/A/Program.cs
@@ -4,21 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace A
-{
+﻿using System;
     internal class Program
     {
         static void Main(string[] args)
         {
-            string text = @"решИла нЕ Упрощать и зашифРОВАтЬ Все послаНИЕ
-                            дАже не Старайся нИЧЕГО у тЕбя нЕ получится с расшифРОВкой
-                            Сдавайся НЕ твоего ума Ты не споСОбЕн Но может быть
-                            если особенно упорно подойдешь к делу
-                            будет Трудно конечнО
-                            Код ведЬ не из простых
-                            очень ХОРОШИЙ код
-                            то у тебя все получится
-                            и я буДу Писать тЕбЕ еще
-                            чао";}
+            string message = "решИла нЕ Упрощать и зашифРОВАтЬ Все послаНИЕдАже не Старайся нИЧЕГО у тЕбя нЕ получится с расшифРОВкойСдавайся НЕ твоего ума Ты не споСОбЕн Но может бытьесли особенно упорно подойдешь к делубудет Трудно конечнОКод ведЬ не из простыхочень ХОРОШИЙ кодто у тебя все получитсяи я буДу Писать тЕбЕ ещечао";
+            List<string> capitalWords = new List<string>();
+
+            string[] words = message.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                if (char.IsUpper(words[i][0]))
+                {
+                    capitalWords.Add(words[i]);
+                }
+            }
+
+            Console.WriteLine("Результат:");
+            foreach (string word in capitalWords)
+            {
+                Console.Write(word + " ");
+            }
+        }
     }
 }
-
