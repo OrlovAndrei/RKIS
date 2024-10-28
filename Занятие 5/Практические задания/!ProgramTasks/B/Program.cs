4569be213d1017787f68a47bf5b840164d434945
habibylin <postnikovaleks0@gmail.com>
2024-10-28 11:39:04 +0300

    УРОК 5B

--- Занятие 5/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 5/Практические задания/!ProgramTasks/B/Program.cs
@@ -1,6 +1,40 @@
 ﻿namespace B
 {
+namespace B
+{
     internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var records = new List<string>
+            {
+                "Alex:alex99@mail.ru",
+                "Shurik:shurik2020@google.com",
+            };
+
+            var emailDictionary = new Dictionary<string, HashSet<string>>();
+
+            foreach (var record in records)
+            {
+                var parts = record.Split(':');
+                var name = parts[0];
+                var email = parts[1];
+
+                var key = name.Substring(0, 2).ToUpper();
+
+                if (!emailDictionary.ContainsKey(key))
+                {
+                    emailDictionary[key] = new HashSet<string>();
+                }
+                emailDictionary[key].Add(email);
+            }
+
+            foreach (var kvp in emailDictionary)
+            {
+                Console.WriteLine($"Key: {kvp.Key} -> Values: {string.Join(", ", kvp.Value)}");
+        {
+        }
+     internal class Program
     {
         static void Main(string[] args)
         {
