90bc2e5cb0ef39ce0aef1ea19228d3e542f9e4f0
Рудакова Алёна <arudakova436@gmail.com>
2024-10-28 12:27:37 +0300

    Update DiagonalMazeTask.cs

--- Занятие 3/Практические задания/Mazes/DiagonalMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/DiagonalMazeTask.cs
@@ -1,14 +1,59 @@
-﻿namespace Mazes;
+using System;
 
-public static class DiagonalMazeTask
+namespace Mazes
 {
-    public static void MoveOut(Robot robot, int width, int height)
+    public static class DiagonalMazeTask
     {
-         
-        for(int i = 0; i < width-3; i++) 
-            { robot.MoveTo(Direction.Right);}
-            for (int i = 0; i < height - 3; i++)
-            { robot.MoveTo(Direction.Down); }
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            var firstDirection = ChooseDirection(width, height);
+            var secondDirection = ChangeDirection(firstDirection);
+            var stepsOver = ChooseStepsOver(width, height);
+
+            while (true)
+            {
+                GoToDirection(robot, firstDirection, stepsOver);
+                if (robot.Finished)
+                    break;
+                GoToDirection(robot, secondDirection, 1);
+            }
+        }
+
+        private static Direction ChooseDirection(int width, int height)
+        {
+            if (width > height)
+            {
+                return Direction.Right;
+            }
+            else
+            {
+                return Direction.Down;
+            }
+        }
+
+        private static int ChooseStepsOver(int width, int height)
+        {
+            var a = width - 2;
+            var b = height - 2;
+            return Math.Max(a, b) / Math.Min(a, b);
+        }
+
+        private static void GoToDirection(Robot robot, Direction dir, int steps)
+        {
+            for (var i = 0; i < steps; i++)
+                robot.MoveTo(dir);
+        }
+
+        private static Direction ChangeDirection(Direction dir)
+        {
+            if (dir == Direction.Down)
+            {
+                return Direction.Right;
+            }
+            else
+            {
+                return Direction.Down;
+            }
         }
     }
-}  
+}
