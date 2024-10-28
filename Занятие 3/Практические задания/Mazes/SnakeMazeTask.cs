816187faecbdb07e42789d72bda675c4b82a4c24
Рудакова Алёна <arudakova436@gmail.com>
2024-10-28 12:31:47 +0300

    Update SnakeMazeTask.cs

--- Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/SnakeMazeTask.cs
@@ -1,33 +1,37 @@
 namespace Mazes
 {
-    public static class DiagonalMazeTask
+    public static class SnakeMazeTask
     {
-        public static void Move(Robot robot, int attitudeParties, Direction direction, Direction direction1)
-        {
-            if (!robot.Finished)
-                for (var robotStep = 0; robotStep < attitudeParties; robotStep++)
-                {
-                    robot.MoveTo(direction);
-                }
-            if (!robot.Finished)
-                robot.MoveTo(direction1);
-        }
- 
         public static void MoveOut(Robot robot, int width, int height)
         {
-            while (!robot.Finished) //Этот цикл стоит вынести во вспомогательный метод и там делать нужные шаги. А метод движения в любом направлении на любое число шагов взять из первой задачи
+            var stepsToHorizont = width - 3;
+            while (true)
             {
-                if (width > height) //Зачем данное условие многократно проверять в цикле, если значения не меняются
-                {
-                    var attitudeParties = (width - 2) / (height - 2);
-                    Move(robot, attitudeParties, Direction.Right, Direction.Down);
-                }
-                else
-                {
-                    var attitudeParties = (height - 2) / (width - 2);
-                    Move(robot, attitudeParties, Direction.Down, Direction.Right);
-                }
+                GoToRight(robot, stepsToHorizont);
+                GoToDown(robot);
+                GoToLeft(robot, stepsToHorizont);
+                if (robot.Finished)
+                    break;
+                GoToDown(robot);
             }
+        }
+
+        private static void GoToRight(Robot robot, int steps)
+        {
+            for (var i = 0; i < steps; i++)
+                robot.MoveTo(Direction.Right);
+        }
+
+        private static void GoToDown(Robot robot)
+        {
+            for (var i = 0; i < 2; i++)
+                robot.MoveTo(Direction.Down);
+        }
+
+        private static void GoToLeft(Robot robot, int steps)
+        {
+            for (var i = 0; i < steps; i++)
+                robot.MoveTo(Direction.Left);
         }
     }
 }
