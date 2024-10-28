6e671cb998bf31603634c1f5f2d8a9620b41d1d7
habibylin <postnikovaleks0@gmail.com>
2024-10-28 11:54:58 +0300

    Update AngryBirdsTask.cs

--- Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
+++ Занятие 1/Практические задания/Angry Birds/AngryBirdsTask.cs
@@ -1,11 +1,21 @@
-using System;
+sing System;
 
 namespace AngryBirds;
 
 public static class AngryBirdsTask
+namespace AngryBirds
 {
     public static double FindSightAngle(double v, double distance)
+    public static class AngryBirdsTask
     {
         return Math.PI / 4;
+
+        public static double FindSightAngle(double v, double distance)
+        {
+
+            double g = 9.8;
+
+            return 0.5 * Math.Asin(distance * g / Math.Pow(v, 2));
+        }
     }
 }
