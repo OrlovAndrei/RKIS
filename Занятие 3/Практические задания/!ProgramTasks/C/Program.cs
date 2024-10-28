92d18345552db300e478c96c9b633e06805c4767
habibylin <postnikovaleks0@gmail.com>
2024-10-28 12:48:40 +0300

    lesson3 C

--- Занятие 3/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,15 +1,15 @@
 ﻿namespace C
    static void Main(строка[] аргументы)
-        {
-            двойное пи = Математика.ПИ;
-            int tenThousand =  10000L;
-            float tenThousandPi =  пи * tenThousand;
-            int roundedTenThousandPi =  tenThousandPi;
-            int integerЧастьДесятиТысячПи =  десятьТысячПи;
-            int tenThousand = (int) 10000L;
-            float tenThousandPi = (float)( пи * tenThousand ) ;
-            int roundedTenThousandPi = (int)Math.Round( tenThousandPi ) ;
-            int integerЧастьДесятиТысячПи = (int) десятьТысячПи;
-            Console.WriteLine(целаяЧастьДесятиТысячПи);
-            Console.WriteLine(округленноеДесятьТысячПи);
-        } 
+      {
+            string top = null!;
+            for (int i = 0; i < text.Length + 2; i++)
+            {
+                top += "-";
+            }
+            Console.WriteLine($"+{top}+");
+            Console.WriteLine($"| {text} |");
+            Console.WriteLine($"+{top}+");
+
+        }
+    }
+}
