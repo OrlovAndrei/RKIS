20cde708324c2a7b1d2b0c1847e54742ea19fc09
habibylin <postnikovaleks0@gmail.com>
2024-10-28 12:15:54 +0300

    LESSON2 C

--- Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
@@ -4,21 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(MiddleOf(5, 0, 100)); // => 5
-            Console.WriteLine(MiddleOf(12, 12, 11)); // => 12
-            Console.WriteLine(MiddleOf(1, 1, 1)); // => 1
-            Console.WriteLine(MiddleOf(2, 3, 2));
-            Console.WriteLine(MiddleOf(8, 8, 8));
-            Console.WriteLine(MiddleOf(5, 0, 1));
-        }
-
-        public static int MiddleOf(int a, int b, int c)
-        {
             if (a > b)
                 if (b > c) return b;
                 else if (a > c) return ...
             else
                 ...
+            if ((a >= b) && (b >= c) || (c >= b) && (b >= a)) // проверяем b по всем возможным сценариям
+                return b;
+            else if ((a >= c) && (c >= b) || (b >= c) && (c >= a)) // проверяем c по всем возможным сценариям
+                return c;
+            else // a можно не проверять так как он крайний вариант
+                return a;
+
+
         }
     }
 }
