87721ab8ab14f92ef75c74d1e1d7b50dab9843eb
habibylin <postnikovaleks0@gmail.com>
2024-10-28 13:14:36 +0300

    LESSON6 B

--- Занятие 6/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 6/Практические задания/!ProgramTasks/B/Program.cs
@@ -1,25 +1,33 @@
 ﻿namespace B
-{
-    public class MenuItem
+  public class MenuItem
     {
-        public string Caption;
-        public string HotKey;
-        public MenuItem[] Items;
+        public string Caption { get; set; }
+        public string HotKey { get; set; }
+        public MenuItem[] Items { get; set; }
     }
 
     internal class Program
-    {
-        static void Main(string[] args)
-        {
-            GenerateMenu();
-        }
-
         public static MenuItem[] GenerateMenu()
         {
-            return new[] {
-            ...
-            };
-        }
-}
-    }
-}
+            return new[]
+            {
+                new MenuItem
+                {
+                    Caption = "File",
+                    HotKey = "F",
+                    Items = new[]
+                    {
+                        new MenuItem { Caption = "New", HotKey = "N", Items = null },
+                        new MenuItem { Caption = "Save", HotKey = "S", Items = null }
+                    }
+                },
+                new MenuItem
+                {
+                    Caption = "Edit",
+                    HotKey = "E",
+                    Items = new[]
+                    {
+                        new MenuItem { Caption = "Copy", HotKey = "C", Items = null },
+                        new MenuItem { Caption = "Paste", HotKey = "V", Items = null }
+                    }
+                }
