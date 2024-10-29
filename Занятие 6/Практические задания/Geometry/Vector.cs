f28211d28e6a40de1570b76408deb90ed1a8d37f
maxvlzzn <msms60837@gmail.com>
2024-10-29 17:34:23 +0300

    lesson 6: make Geometry1

---
+++ Занятие 6/Практические задания/Geometry/Vector.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Geometry
+{
+    public class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
