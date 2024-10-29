0ceea00eaa1649ede954076dc5541ca7bc6dbcc3
Arima Kurosawa <nightmoon.yt228@gmail.com>
2024-10-29 17:31:04 +0300

    Lesson 1: Make I

--- Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/I/Program.cs
@@ -4,14 +4,15 @@
     {
         static string GetLastHalf(string text)
         {
-            ...
+            int halfLength = text.Length / 2;
+            return text.Substring(halfLength).Replace(" ", "");
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(GetLastHalf("I love CSharp!"));
-            Console.WriteLine(GetLastHalf("1234567890"));
-            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+            Console.WriteLine(GetLastHalf("I love CSharp!"));       // Вывод: "CSharp!"
+            Console.WriteLine(GetLastHalf("1234567890"));           // Вывод: "67890"
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си")); // Вывод: "сольляси"
         }
     }
 }
