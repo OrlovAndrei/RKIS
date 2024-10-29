1ba1392d31f7f8649fe9da7b5aecd26493b0d8f2
maxvlzzn <msms60837@gmail.com>
2024-10-29 17:26:02 +0300

    lesson 6: make A

--- Занятие 6/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 6/Практические задания/!ProgramTasks/A/Program.cs
@@ -1,20 +1,24 @@
-﻿using System.Globalization;
-
-namespace A
+﻿namespace A
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var city = new City();
             city.Name = "Novorossiysk";
-            city.Location = new GeoLocation();
-            city.Location.Latitude = 44.43;
-            city.Location.Longitude = 37.46;
-            Console.WriteLine("I love {0} located at ({1}, {2})",
-                city.Name,
+	@@ -16,5 +14,17 @@ static void Main(string[] args)
                 city.Location.Longitude.ToString(CultureInfo.InvariantCulture),
                 city.Location.Latitude.ToString(CultureInfo.InvariantCulture));
+        }
+            class GeoLocation
+        {
+            public double Latitude;
+            public double Longitude;
+        }
+        class City
+        {
+            public GeoLocation Location;
+            public string Name;
         }
     }
 }
