ad9de71f4309460ae94edf37c92b9a7774b093d1
maxvlzzn <msms60837@gmail.com>
2024-10-29 17:19:55 +0300

    lesson 5: исправил D

--- Занятие 5/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 5/Практические задания/!ProgramTasks/D/Program.cs
@@ -1,13 +1,10 @@
-﻿using System
-
-namespace D
+﻿namespace D
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] commands = 
-            {
+            string[] commands = {
                 "push Привет! Это снова я! Пока!",
                 "pop 5",
                 "push Как твои успехи? Плохо?",
@@ -15,28 +12,32 @@ namespace D
                 "push 1234567890",
                 "pop 26"
             };
+
             Stack<string> stack = new Stack<string>();
             string text = "";
+
             foreach (var command in commands)
             {
                 if (command.StartsWith("push"))
                 {
                     string toPush = command.Substring(5);
- stack.Push(toPush);
+                    stack.Push(toPush);
                     text += toPush;
                 }
                 else if (command.StartsWith("pop"))
                 {
                     int count = int.Parse(command.Substring(4));
-                    if (text. Length >= count)
+                    if (text.Length >= count)
                     {
-                        text = text.Substring(0, text. Length - count);
+                        text = text.Substring(0, text.Length - count);
                     }
                 }
+
             }
 
- Console.WriteLine("Результат:");
- Console.WriteLine(text);
+            Console.WriteLine("Результат:");
+            Console.WriteLine(text);
+
         }
     }
 }
