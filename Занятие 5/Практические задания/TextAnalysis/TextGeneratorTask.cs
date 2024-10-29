128bb190364a68f1ae7392752dee9cb26333e207
maxvlzzn <msms60837@gmail.com>
2024-10-29 17:23:58 +0300

    lesson 5: make TextGenerator

--- Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
@@ -1,5 +1,4 @@
 ﻿namespace TextAnalysis;
-
 static class TextGeneratorTask
 {
     public static string ContinuePhrase(
@@ -7,6 +6,21 @@ static class TextGeneratorTask
         string phraseBeginning,
         int wordsCount)
     {
+        for (int i = 0; i < wordsCount; i++)
+        {
+            string[] splitWords = phraseBeginning.Split(' ');
+            if (splitWords.Length > 0)
+            {
+                if (splitWords.Length >= 2 && nextWords.ContainsKey(splitWords[splitWords.Length - 2] + " " + splitWords[splitWords.Length - 1]))
+                    phraseBeginning += " " + nextWords[splitWords[splitWords.Length - 2] + " " + splitWords[splitWords.Length - 1]];
+
+                else if (splitWords.Length >= 1 && nextWords.ContainsKey(splitWords[splitWords.Length - 1]))
+                    phraseBeginning += " " + nextWords[splitWords[splitWords.Length - 1]];
+
+                else
+                    return phraseBeginning;
+            }
+        }
         return phraseBeginning;
     }
 }
