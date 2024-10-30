1a0fdeac42cd82cb050fe9f32abc0710d1798886
Tital7 <tital20052005@icloud.com>
2024-10-30 11:17:47 +0300

    Update Program.cs

--- Занятие 1/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/A/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace A
+﻿using System;
+
+namespace A
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // пишите код тут
+            Console.WriteLine("The first step!");
         }
     }
 }
