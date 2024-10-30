f57af54146f44ac56c1c5ff30e9ff82089614845
Your Name <you@example.com>
2024-10-30 15:53:14 +0300

    lesson 4: make H (Переделанное)

--- Занятие 4/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/H/Program.cs
@@ -1,4 +1,4 @@
-﻿﻿namespace H
+﻿namespace H
 {
     internal class Program
     {
@@ -30,7 +30,12 @@
 
         public static GameResult GetGameResult(Mark[,] field)
         {
-            ...
+            bool circleWin = testWin(field, Mark.Circle);
+            bool crossWin = testWin(field, Mark.Cross);
+
+            if (circleWin == crossWin) return GameResult.Draw;
+
+            return if (circleWin) GameResult.CircleWin else GameResult.CrossWin;
         }
 
         private static void Run(string description)
@@ -48,6 +53,62 @@
                 for (var y = 0; y < field.Length; y++)
                     ans[x, y] = field[x][y] == 'X' ? Mark.Cross : (field[x][y] == 'O' ? Mark.Circle : Mark.Empty);
             return ans;
+        }
+
+
+        public static bool testLines(Mark[,] field, Mark mark)
+        {
+            bool winner = false;
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                var a = 0;
+                for (int j = 0; j < field.GetLength(1); j++)
+                    if (mark == field[i, j])
+                    {
+                        a = a + 1;
+                        if (a == 3)
+                        {
+                            winner = true;
+                            break;
+                        }
+                    }
+            }
+            return winner;
+        }
+
+        public static bool testColumn(Mark[,] field, Mark mark)
+        {
+            bool winner = false;
+            for (int nomerStroki = 0; nomerStroki < field.GetLength(0); nomerStroki++)
+            {
+                var a = 0;
+                for (int nomerKolonki = 0; nomerKolonki < field.GetLength(1); nomerKolonki++)
+                    if (mark == field[nomerKolonki, nomerStroki])
+                    {
+                        a = a + 1;
+                        if (a == 3)
+                        {
+                            winner = true;
+                            break;
+                        }
+                    }
+            }
+            return winner;
+        }
+
+        public static bool testDiagonal(Mark[,] field, Mark mark)
+        {
+            if ((mark == field[0, 0] && mark == field[1, 1] && mark == field[2, 2]) || (mark == field[2, 0] && mark == field[1, 1] && mark == field[0, 2]))
+                return true;
+            else return false;
+        }
+
+        public static bool testWin(Mark[,] field, Mark mark)
+        {
+            bool diagonal = testDiagonal(field, mark);
+            bool stroka = testLines(field, mark);
+            bool kolonka = testColon(field, mark);
+            return (diagonal || stroka || kolonka);
         }
     }
 }
