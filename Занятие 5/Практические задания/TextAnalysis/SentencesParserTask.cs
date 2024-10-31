5adbf76df38a1c35645c172d154286c40d0ffb97
Evgeniy <85281694+poltoraska@users.noreply.github.com>
2024-10-31 15:33:45 +0300

    Lesson 5: make SentencesParserTask

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,11 +1,47 @@
-﻿namespace TextAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-static class SentencesParserTask
+namespace TextAnalysis
 {
-    public static List<List<string>> ParseSentences(string text)
+    static class SentencesParserTask
     {
-        var sentencesList = new List<List<string>>();
-        //...
-        return sentencesList;
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var sentenceSeparators = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+            var sentences = text.Split(sentenceSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sentence in sentences)
+            {
+                var words = new List<string>();
+                var currentWord = new StringBuilder();
+                foreach (var ch in sentence)
+                {
+                    if (char.IsLetter(ch) || ch == '\'')
+                    {
+                        currentWord.Append(char.ToLower(ch));
+                    }
+                    else if (currentWord.Length > 0)
+                    {
+                        words.Add(currentWord.ToString());
+                        currentWord.Clear();
+                    }
+                }
+
+                if (currentWord.Length > 0)
+                {
+                    words.Add(currentWord.ToString());
+                }
+
+                if (words.Count > 0)
+                {
+                    sentencesList.Add(words);
+                }
+            }
+
+            return sentencesList;
+        }
     }
 }
