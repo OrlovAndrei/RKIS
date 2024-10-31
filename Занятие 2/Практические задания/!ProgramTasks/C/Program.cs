3101a64b24ed1222c98c539a6967bae374d874f3
Arima Kurosawa <nightmoon.yt228@gmail.com>
2024-10-31 03:16:39 +0300

    Lesson 2: Make C

--- Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/C/Program.cs
@@ -4,21 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(MiddleOf(5, 0, 100)); // => 5
-            Console.WriteLine(MiddleOf(12, 12, 11)); // => 12
-            Console.WriteLine(MiddleOf(1, 1, 1)); // => 1
-            Console.WriteLine(MiddleOf(2, 3, 2));
+            Console.WriteLine(MiddleOf(5, 0, 100));  
+            Console.WriteLine(MiddleOf(12, 12, 11)); 
+            Console.WriteLine(MiddleOf(1, 1, 1));   
+            Console.WriteLine(MiddleOf(2, 3, 2));  
             Console.WriteLine(MiddleOf(8, 8, 8));
-            Console.WriteLine(MiddleOf(5, 0, 1));
+            Console.WriteLine(MiddleOf(5, 0, 1)); 
         }
 
         public static int MiddleOf(int a, int b, int c)
         {
-            if (a > b)
-                if (b > c) return b;
-                else if (a > c) return ...
-            else
-                ...
+            int min = Math.Min(a, Math.Min(b, c));
+            int max = Math.Max(a, Math.Max(b, c));
+            return a + b + c - min - max; 
         }
     }
 }
