1365d7c1fc5653e13ef70682a792e41dd43db47e
Daria Oleinik <darya.oleynik.06@bk.ru>
2024-10-31 15:49:55 +0300

    Lesson6: Make Vector

---
+++ Занятие 6/Практические задания/Geometry/ClassLibrary1/ClassLibrary1/Vector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
