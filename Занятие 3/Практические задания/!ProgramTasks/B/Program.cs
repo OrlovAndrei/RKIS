3538023fbf13e451bb09ced0bf7aa8e4b8aef93f
Evgeniy <85281694+poltoraska@users.noreply.github.com>
2024-10-31 14:59:45 +0300

    Lesson 3: make B

--- Занятие 3/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 3/Практические задания/!ProgramTasks/B/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace B
+namespace B
 {
     internal class Program
     {
@@ -19,9 +19,18 @@
         }
 
         public static string RemoveStartSpaces(string text)
+
         {
-            if (char.IsWhiteSpace(text[0])) return text.Substring(1);
-            else return text;
+            int y = 0;
+            while (char.IsWhiteSpace(text[y]))
+            {
+                y++;
+                if (y == text.Length)
+                {
+                    return null;
+                }
+            }
+            return text.Substring(y);
         }
     }
 }
