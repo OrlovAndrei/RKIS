e54c7db42db3241f4bb7bacd81b5eec20304e8b0
Evgeniy <85281694+poltoraska@users.noreply.github.com>
2024-10-31 15:48:17 +0300

    Lesson 5: make task 1 and 2 in SentencesParserTask

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,3 +1,62 @@
+//Задание 2
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis
+{
+    public static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var sentenceSeparators = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+            var sentences = text.Split(sentenceSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sentence in sentences)
+            {
+                var words = new List<string>();
+                var currentWord = new StringBuilder();
+                foreach (var ch in sentence)
+                {
+                    if (char.IsLetter(ch) || ch == '\'')
+                    {
+                        currentWord.Append(char.ToLower(ch));
+                    }
+                    else if (currentWord.Length > 0)
+                    {
+                        words.Add(currentWord.ToString());
+                        currentWord.Clear();
+                    }
+                }
+
+                if (currentWord.Length > 0)
+                {
+                    words.Add(currentWord.ToString());
+                }
+
+                if (words.Count > 0)
+                {
+                    sentencesList.Add(words);
+                }
+            }
+
+            return sentencesList;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//Задание 1
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
