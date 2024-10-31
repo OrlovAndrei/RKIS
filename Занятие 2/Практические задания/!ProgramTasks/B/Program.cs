c2155ba0165b23c5e858f550edf56ba202224f4b
Arima Kurosawa <nightmoon.yt228@gmail.com>
2024-10-31 03:15:23 +0300

    Lesson 2: Make B

--- Занятие 2/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 2/Практические задания/!ProgramTasks/B/Program.cs
@@ -5,8 +5,8 @@
         static void Main(string[] args)
         {
             TestMove("a1", "d4");
-            TestMove("f4", "e7");
-            TestMove("a1", "a4");
+            TestMove("f4", "e7"); 
+            TestMove("a1", "a4"); 
         }
 
         public static void TestMove(string from, string to)
@@ -16,9 +16,9 @@
 
         public static bool IsCorrectMove(string from, string to)
         {
-            var dx = Math.Abs(to[0] - from[0]); //смещение фигуры по горизонтали
-            var dy = Math.Abs(to[1] - from[1]); //смещение фигуры по вертикали
-            ...
+            var dx = Math.Abs(to[0] - from[0]);
+            var dy = Math.Abs(to[1] - from[1]);
+            return dx == dy || dx == 0 || dy == 0;
         }
     }
 }
