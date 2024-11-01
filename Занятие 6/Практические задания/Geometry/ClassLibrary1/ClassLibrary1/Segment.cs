0f4c76d6b32fbba38d04d3bee08c39d6c4dde8ca
Daria Oleinik <darya.oleynik.06@bk.ru>
2024-11-01 14:47:30 +0300

    Lesson6:Segment(3)

--- Занятие 6/Практические задания/Geometry/ClassLibrary1/ClassLibrary1/Segment.cs
+++ Занятие 6/Практические задания/Geometry/ClassLibrary1/ClassLibrary1/Segment.cs
@@ -12,5 +12,19 @@ namespace Geometry
             Begin = begin;
             End = end;
         }
+        public double GetLength()
+        {
+            return Vector.Distance(Begin, End);
+        }
+
+        public static bool IsVectorInSegment(Vector point, Segment segment)
+        {
+            double segmentLength = segment.GetLength();
+
+            double length1 = Vector.Distance(segment.Begin, point);
+            double length2 = Vector.Distance(point, segment.End);
+
+            return Math.Abs(segmentLength - (length1 + length2)) < 1e-10;
+        }
     }
 }
