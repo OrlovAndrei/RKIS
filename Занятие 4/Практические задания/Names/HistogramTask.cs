f809231661b2f06b0e1ce0fb1d48679a6affea37
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2024-11-01 20:04:51 +0300

    Lesson 4: HistogramTask

--- Занятие 4/Практические задания/Names/HistogramTask.cs
+++ Занятие 4/Практические задания/Names/HistogramTask.cs
@@ -1,12 +1,25 @@
-﻿namespace Names;
+﻿namespace Names
 
 internal static class HistogramTask
 {
     public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
     {
-        return new HistogramData(
-            $"Рождаемость людей с именем '{name}'", 
-            new [] {"1"}, 
-            new[] {0d});
+        var days = new string[31];
+        int g = 1;
+        for (int i = 0; i < days.Length; i++)
+        {
+            string f = Convert.ToString(g);
+            days[i] = "" + f;
+            g++;
+        }
+        var birthsCounts = new double[31];
+        foreach (var man in names)
+        {
+            if (man.Name == name && man.BirthDate.Day != 1)
+            {
+                birthsCounts[man.BirthDate.Day - 1]++;
+            }
+        }    
+        return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name),days,birthsCounts)
     }
 }
