d107a1c02f6beb3fc6cfed177038baace2c54aa6
Daria Oleinik <darya.oleynik.06@bk.ru>
2024-11-01 14:44:21 +0300

    Lesson6: Vector(3)

--- Занятие 6/Практические задания/Geometry/ClassLibrary1/ClassLibrary1/Vector.cs
+++ Занятие 6/Практические задания/Geometry/ClassLibrary1/ClassLibrary1/Vector.cs
@@ -4,13 +4,27 @@ namespace Geometry
 {
     public class Vector
     {
-        public double X { get; set; }
-        public double Y { get; set; }
+        public double X { get; }
+        public double Y { get; }
 
         public Vector(double x, double y)
         {
             X = x;
             Y = y;
+        }
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public Vector Add(Vector other)
+        {
+            return Geometry.Add(this, other);
+        }
+
+        public bool Belongs(Segment segment)
+        {
+            return Geometry.IsVectorInSegment(this, segment);
         }
     }
 }
