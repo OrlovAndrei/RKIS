a4ed22c62ec59cfd056b66c3bf36203e0a3f888e
Daria Oleinik <darya.oleynik.06@bk.ru>
2024-11-01 14:36:21 +0300

    Lesson6:Segment

---
+++ Занятие 6/Практические задания/Geometry/ClassLibrary1/ClassLibrary1/Segment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Segment
+    {
+        public Vector Begin { get; set; }
+        public Vector End { get; set; }
+
+        public Segment(Vector begin, Vector end)
+        {
+            Begin = begin;
+            End = end;
+        }
+    }
+}
