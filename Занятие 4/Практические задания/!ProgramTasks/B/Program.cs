c3e4da8e4d7fe8890a36fd91a317b8793987c2cb
Eduard1731 <jonibongonii@gmail.com>
2024-11-01 15:28:49 +0300

    lesson 4: make B

--- Занятие 4/Практические задания/!ProgramTasks/B/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/B/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(MaxIndex(new[] {1.0, .2, 100, 2e+10}));
+            Console.WriteLine(MaxIndex(new[] { 1.0, .2, 100, 2e+10 }));
         }
 
         static double Min(double[] array)
@@ -15,9 +15,25 @@
             return min;
         }
 
+
         public static int MaxIndex(double[] array)
         {
-            ...
+            if (array.Length == 0) return -1; // Проверка на пустой массив
+
+            double max = array[0];
+            int firstIndex = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    firstIndex = i;
+                }
+            }
+
+            return firstIndex;
         }
+
     }
 }
