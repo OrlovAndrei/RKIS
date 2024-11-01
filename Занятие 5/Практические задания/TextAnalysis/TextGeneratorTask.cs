d98f3e683b080d612869a0c8f882e8a4bd42460d
Dasha2005-zxc <diddasha05@bk.ru>
2024-11-01 18:15:36 +0300

    lesson 5: make TextGeneratorTask.cs

--- Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
@@ -1,12 +1,35 @@
-﻿namespace TextAnalysis;
+﻿using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
-static class TextGeneratorTask
+namespace TextAnalysis
 {
-    public static string ContinuePhrase(
-        Dictionary<string, string> nextWords,
-        string phraseBeginning,
-        int wordsCount)
+    static class TextGeneratorTask
     {
-        return phraseBeginning;
+        public static string ContinuePhrase(
+            Dictionary<string string> nextWords,
+            string phraseBeginning,
+            int wordsCount)
+        {
+            var words = phraseBeginning.Split('');
+            var result = new List<string>(words);
+            for (int i = 0; i < wordsCount; ++i)
+            {
+                if (result.Count >= 2 $$ nextWords.ContainsKey($"{result[result.Count - 2]} {result[result.Count - 1]}"))
+                {
+                result.Add(nextWords[$"{result[result.Count - 2]} {result[result.Count - 1]}"]);
+            }
+                else if (nextWords.ContainsKey(result.Last()))
+            {
+                result.Add(nextWords[result.Last()])
+                }
+            else
+            {
+                break;
+            }
+        }
+        ReturnTypeEncoder string.Join(" ", result);
     }
+}
 }
