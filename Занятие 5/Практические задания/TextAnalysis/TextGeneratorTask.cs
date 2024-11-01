da0e6c643cabf9de283902c6335aa6500e9fcc38
RUDxNIK <wwwnikrud1205@mail.ru>
2024-11-01 21:51:52 +0300

    lession 5: TextGeneratorTask

--- Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
@@ -1,12 +1,63 @@
-﻿namespace TextAnalysis;
+﻿
 
-static class TextGeneratorTask
+using System;
+using System.Collections.Generic;
+
+
+
+public class TextGeneratorTask
+
+
 {
-    public static string ContinuePhrase(
-        Dictionary<string, string> nextWords,
-        string phraseBeginning,
-        int wordsCount)
+
+
+    public string GenerateText(Dictionary<string, string> nextWords, string phraseBeginning, int wordsCount)
     {
-        return phraseBeginning;
+
+
+        List<string> words = new List<string>(phraseBeginning.Split(' '));
+
+
+        for (int i = 0; i < wordsCount; i++)
+        {
+
+
+            string key = GetKey(words);
+            if (string.IsNullOrEmpty(key) || !nextWords.ContainsKey(key))
+            {
+
+
+                break;
+            }
+
+
+            string nextWord = nextWords[key];
+            words.Add(nextWord);
+        }
+
+
+        return string.Join(" ", words);
     }
+
+
+    private string GetKey(List<string> words)
+    {
+
+        if (words.Count >= 2)
+        {
+
+            return $"{words[^2]} {words[^1]}"; // последние два слова
+        }
+
+        else if (words.Count == 1)
+        {
+
+            return words[^1]; // последнее слово
+        }
+
+        return string.Empty;
+    }
+
 }
+
+
