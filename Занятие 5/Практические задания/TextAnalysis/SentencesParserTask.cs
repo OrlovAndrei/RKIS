37c8be55d12c7d1e6c520fe25d3a4d128d438907
Lussuru <Lussuru17@mail.ru>
2024-11-01 17:34:52 +0300

    lesson 5: TextAnalysis

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,11 +1,31 @@
-﻿namespace TextAnalysis;
+﻿using System.Text.RegularExpressions;
+
+namespace TextAnalysis;
 
 static class SentencesParserTask
 {
-    public static List<List<string>> ParseSentences(string text)
+        public static List<List<string>> ParseSentences(string text)
     {
-        var sentencesList = new List<List<string>>();
-        //...
-        return sentencesList;
+        var result = new List<List<string>>();
+        var sentences = Regex.Split(text, @"(?<=[.!?;:()])\s*");
+
+        foreach (var sentence in sentences)
+        {
+            var cleanedSentence = sentence.Trim();
+            var words = Regex.Matches(cleanedSentence, @"[a-zA-Zа-яА-ЯёЁ']+");
+            var wordList = new List<string>();
+
+            foreach (Match word in words)
+            {
+                wordList.Add(word.Value.ToLower());
+            }
+
+            if (wordList.Count > 0)
+            {
+                result.Add(wordList);
+            }
+        }
+
+        return result;
     }
 }
