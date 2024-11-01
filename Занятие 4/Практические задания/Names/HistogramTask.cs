4e74f2b5477af80c7f1c2e1e46c5462e1c4bce21
Eduard1731 <jonibongonii@gmail.com>
2024-11-01 15:52:24 +0300

    lesson 4: make HistogramTask

--- Занятие 4/Практические задания/Names/HistogramTask.cs
+++ Занятие 4/Практические задания/Names/HistogramTask.cs
@@ -1,12 +1,28 @@
-﻿namespace Names;
+﻿using System;
+using System.Linq;
 
-internal static class HistogramTask
+namespace Names
 {
-    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    internal static class HistogramTask
     {
-        return new HistogramData(
-            $"Рождаемость людей с именем '{name}'", 
-            new [] {"1"}, 
-            new[] {0d});
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            const int minDay = 1;
+            int maxDay = names.Max(nd => nd.BirthDate.Day);
+
+            var days = Enumerable.Range(minDay, maxDay - minDay + 1).Select(day => day.ToString()).ToArray();
+
+            var birthCounts = new double[maxDay - minDay + 1];
+
+            foreach (var day in names)
+            {
+                if (day.Name.Equals(name, StringComparison.OrdinalIgnoreCase) && day.BirthDate.Day > minDay)
+                {
+                    birthCounts[day.BirthDate.Day - minDay]++;
+                }
+            }
+
+            return new HistogramData(string.Format("Рождаемость людей с именем '{0}'", name), days, birthCounts);
+        }
     }
 }
