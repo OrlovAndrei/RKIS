d9aeceac8c3f309b2b744bea608c8635f1fabf2d
Dasha2005-zxc <diddasha05@bk.ru>
2024-11-01 17:49:18 +0300

    lesson 5: make FrequencyAnalisis

--- Занятие 5/Практические задания/TextAnalysis/FrequencyAnalysisTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/FrequencyAnalysisTask.cs
@@ -1,11 +1,52 @@
-﻿namespace TextAnalysis;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-static class FrequencyAnalysisTask
+namespace TextAnalysis;
+{
+    static class FrequencyAnalysisTask
 {
     public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
     {
+        var ngramsCount = new Dictionary<string, int>();
+        foreach (List<string> sentence in text)
+        {
+            for (int i = 0; i < sentence.Count - 1; i++)
+            {
+                string bigram = $"{sentence[i]} {sentence[i + 1]}";
+                if (!ngramsCount.ContainsKey(bigram))
+                    ngremsCount[bigram] = 0;
+                ngramsCount[bigram]++;
+            }
+            for (int i = 0; i < sentence.Count - 1; i++)
+            {
+                string bigram = $"{sentence[i]} {sentence[i + 1]}";
+                if (!ngramsCount.ContainsKey(bigram))
+                    ngramsCount[bigram] = 0;
+                ngramsCount[bigram]++;
+            }
+            for (int i = 0; i < sentence.Count - 2; i++)
+            {
+                string trigram = $"{sentence[i]} {sentence[i + 1]} {sentence[i + 2]}";
+                if (!ngramsCount.ContainsKey(trigram))
+                    ngramsCount[trigram] = 0;
+                ngramsCount[trigram]++;
+            }
+        }
         var result = new Dictionary<string, string>();
-        //...
+        foreach (var entry in ngramsCount)
+        {
+            string start = entry.Key.Substring(0, entry.Key.LastIndexOf(''));
+            string end = entry.Key.Substring(entry.Key.LastIndexOf('') + 1);
+            if (!result.ContainsKey(start) ||
+                (String.CompareOrdinal(result[start], end) > 0 &&
+                 ngramsCount[$"{start} {end}"] >= ngramsCount[$"{start} {result[start]}"]))
+            {
+                result[start] = end;
+            }
+        }
         return result;
     }
 }
+}
+   
--- Занятие 5/Практические задания/TextAnalysis/FrequencyAnalysis_Tests.cs
+++ Занятие 5/Практические задания/TextAnalysis/FrequencyAnalysis_Tests.cs
@@ -1,167 +1,34 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace TextAnalysis;
-
-[TestFixture]
-public class FrequencyAnalysis_Tests
+class FrequencyAnalysisTask
 {
-    [Test]
-    [Order(00)]
-    public void ReturnEmptyDictionary_OnEmptyText()
+    public static Dictionary<string, string> GetMostFrequentContinuations(string[] sentences)
     {
-        var text = "";
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>();
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    [Test]
-    [Order(01)]
-    public void ReturnEmptyDictionary_OnTextWithOneSentenceWithOneWord()
-    {
-        var text = "abc";
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>();
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    [Test]
-    [Order(02)]
-    public void ReturnCorrectResult_OnTextWithOneSentenceWithTwoWords()
-    {
-        var text = "x y";
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>
+        var nGrams = new Diictionary<string, int>();
+        foreach (string sentence in sentences)
         {
-            {"x", "y"}
-        };
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    [Test]
-    [Order(03)]
-    public void ReturnCorrectResult_OnTextWithOneSentenceWithMultipleWords()
-    {
-        var text = "x y z";
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>
-        {
-            {"x", "y"},
-            {"y", "z"},
-            {"x y", "z"}
-        };
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    [Test]
-    [Order(04)]
-    public void ReturnCorrectResult_OnTextWithTwoSentencesWithOneWord()
-    {
-        var text = "x.y";
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>();
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    [Test]
-    [Order(05)]
-    public void ReturnResult_WithMostFrequentBigrams([Values("x y. x z. x y.", "x z. x y. x y", "x y. x y.", "x y")]
-        string text)
-    {
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>
-        {
-            {"x", "y"}
-        };
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    [Test]
-    [Order(06)]
-    public void ReturnResult_WithLexicographicallyFirstBigram_IfBigramsHaveSameFrequency(
-        [Values("x y. x z.", "x z. x y.", "x y. x yy.", "x yy. x y")]
-        string text)
-    {
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>
-        {
-            {"x", "y"}
-        };
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    [Test]
-    [Order(50)]
-    public void IgnoreSentencesWithSingleWord([Values("x. ax. y. z")] string text)
-    {
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>();
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    [Test]
-    [Order(60)]
-    public void ReturnPairForEveryBigram([Values("x y. y z.", "y z. x y.")] string text)
-    {
-        var parsedText = ParseText(text);
-        var expected = new Dictionary<string, string>
-        {
-            {"x", "y"},
-            {"y", "z"}
-        };
-
-        var actual = FrequencyAnalysisTask.GetMostFrequentNextWords(parsedText);
-
-        AssertResult(expected, actual, text);
-    }
-
-    // Упрощённый парсинг текста
-    public List<List<string>> ParseText(string text)
-    {
-        return text.Split('.')
-            .Select(sentence => sentence.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList())
-            .ToList();
-    }
-
-    public static void AssertResult(
-        Dictionary<string, string> expected,
-        Dictionary<string, string> actual,
-        string text)
-    {
-        foreach (var key in expected.Keys)
-        {
-            if (!actual.ContainsKey(key))
-                Assert.Fail($"Input text: [{text}]\nMissing expected key [{key}] in dictionary");
-            Assert.AreEqual(expected[key], actual[key], $"Input text: [{text}]\nWrong value for key [{key}]");
+            string[] words = sentence.Split('')
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                string bigram = $"{words[i]} {words[i + 1]}";
+                if (!nGrams.ContainsKey(bigram))
+                    nGrams[bigram] = 0
+                nGrams[trigram]++
+            }
         }
-
-        foreach (var key in actual.Keys)
-            if (!expected.ContainsKey(key))
-                Assert.Fail($"Input text: [{text}]\nKey [{key}] should not be in dictionary");
+        var result = new Dictionary<string, string>();
+        foreach (var key in nGrams.Keys)
+        {
+            string start = key.Substring(0, key.LastIndexOf(''));
+            string end = key.Substring(key.LastIndexOf('') + 1);
+            if (!result.ContainsKey(start) ||
+                (result[start].CompareTo(end) > 0 && nGrams[$"{start} {end}"] >= nGrams[$"{start} {result[start]}"]))
+            {
+                result[start] = end;
+            }
+        }
+        return result;
     }
-
 }
