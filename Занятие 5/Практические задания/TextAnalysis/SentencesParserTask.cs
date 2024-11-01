cb80546ef5da01bc06c4ab7ead59e402b058d80e
Eduard1731 <jonibongonii@gmail.com>
2024-11-01 16:09:19 +0300

    lesson 5: make  SentencesParserTask

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,11 +1,54 @@
-﻿namespace TextAnalysis;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-static class SentencesParserTask
+namespace TextAnalysis
 {
-    public static List<List<string>> ParseSentences(string text)
+    static class SentencesParserTask
     {
-        var sentencesList = new List<List<string>>();
-        //...
-        return sentencesList;
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            var sentenceSeparators = new char[] { '.', '!', '?', ';', ':', '(', ')' };
+            var sentences = text.Split(sentenceSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var sentence in sentences)
+            {
+                var words = ExtractWordsFromSentence(sentence);
+                if (words.Count > 0)
+                {
+                    sentencesList.Add(words);
+                }
+            }
+
+            return sentencesList;
+        }
+
+        private static List<string> ExtractWordsFromSentence(string sentence)
+        {
+            var words = new List<string>();
+            var currentWord = new StringBuilder();
+
+            foreach (var ch in sentence)
+            {
+                if (char.IsLetter(ch) || ch == '\'')
+                {
+                    currentWord.Append(char.ToLower(ch));
+                }
+                else if (currentWord.Length > 0)
+                {
+                    words.Add(currentWord.ToString());
+                    currentWord.Clear();
+                }
+            }
+
+            if (currentWord.Length > 0)
+            {
+                words.Add(currentWord.ToString());
+            }
+
+            return words;
+        }
     }
 }
