62742c326ba198d3eb6d2bdb66664cefcc2889c1
Evgeniy <85281694+poltoraska@users.noreply.github.com>
2024-11-02 16:41:17 +0300

    Lesson 5: make TextGeneratorTask

--- Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
@@ -1,48 +1,17 @@
-using System;
-using System.Collections.Generic;
-
-namespace TextAnalysis
+using System.Text;
+namespace TextAnalysis;
+static class TextGeneratorTask
 {
-    static class TextGeneratorTask
+    public static string ContinuePhrase(Dictionary<string, string> frequentNextWords, string initialWord, int totalWordsCount)
     {
-        public static string ContinuePhrase(
-            Dictionary<string, string> nextWords,
-            string phraseBeginning,
-            int wordsCount)
+        StringBuilder resultPhrase = new StringBuilder(initialWord);
+        for (int index = 1; index < totalWordsCount; index++)
         {
-            var words = phraseBeginning.Split(' ');
-            var result = new List<string>(words);
-
-            for (int i = 0; i < wordsCount; i++)
-            {
-                string lastTwoWordsKey = null;
-                string lastWordKey = null;
-
-                if (result.Count >= 2)
-                {
-                    lastTwoWordsKey = result[^2] + " " + result[^1];
-                }
-
-                if (result.Count >= 1)
-                {
-                    lastWordKey = result[^1];
-                }
-
-                if (lastTwoWordsKey != null && nextWords.ContainsKey(lastTwoWordsKey))
-                {
-                    result.Add(nextWords[lastTwoWordsKey]);
-                }
-                else if (lastWordKey != null && nextWords.ContainsKey(lastWordKey))
-                {
-                    result.Add(nextWords[lastWordKey]);
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            return string.Join(" ", result);
+            if (!frequentNextWords.ContainsKey(initialWord))
+                break;
+            initialWord = frequentNextWords[initialWord];
+            resultPhrase.Append(" " + initialWord);
         }
+        return resultPhrase.ToString();
     }
 }
