fc60672f7176202291e8f26c2bec708f998cf19e
Vlada-en <Edenisova73@mail.ru>
2024-11-02 18:20:58 +0300

    lesson5: переделала, на случай если 1-ый проект работает некорректно, FrequencyAnalysisTask-остался тем же

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,45 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace TextAnalysis;
-
-static class SentencesParserTask
+namespace TextAnalysis
 {
-    public static List<List<string>> ParseSentences(string text)
+    static class SentencesParserTask
     {
-        var sentencesList = new List<List<string>>();
-
-
-if (text == null) return null;  
-       text = text.ToLower();
-       var sentences = text.Split(".:;?!()".ToCharArray(), 
-           StringSplitOptions.RemoveEmptyEntries);
-       foreach (var sent in sentences)
-       {
-            var listWords = new List<string>();  
-            var builder = new StringBuilder();
-            foreach (var ch in sent)
-            {
-                if (char.IsLetter(ch) || ch == '\'')
-                    builder.Append(ch);
-                else
-                    AddNotEmptyWord( builder, listWords);
-            }
-            AddNotEmptyWord(builder, listWords);
-            if (listWords.Count > 0)
-                sentencesList.Add(listWords);
-       }
-        return sentencesList;
-    }
-
-    public static void AddNotEmptyWord(StringBuilder builder, List<string> listWords)
-    {
-        if (builder.Length > 0)
+        static readonly char[] SplitSymbols = new char[7] { '.', '!', '?', ';', ':', '(', ')' };
+        public static List<List<string>> ParseSentences(string text)
         {
-            listWords.Add(builder.ToString());
-            builder.Clear();
+            var sentenceList = new List<List<string>>();
+
+            var sentenceIndex = 0;
+            var wordIndex = 0;
+            var nextWord = true;
+            var nextSentence = true;
+
+            foreach (var symbol in text)
+            {
+                if (char.IsLetter(symbol) || symbol.Equals('\''))
+                {
+                    nextSentence = true;
+
+                    if (sentenceList.Count == 0)
+                        sentenceList.Add(new List<string>());
+
+                    if (nextWord)
+                    {
+                        if (sentenceList[sentenceIndex].Count != 0)
+                            wordIndex++;
+                        sentenceList[sentenceIndex].Add("");
+                        nextWord = false;
+                    }
+                    sentenceList[sentenceIndex][wordIndex] += symbol.ToString().ToLower();
+                }
+                else
+                {
+                    nextWord = true;
+                    if (IsSplitSymbol(symbol)
+                        && sentenceList.Count != 0
+                        && nextSentence)
+                    {
+                        sentenceList.Add(new List<string>());
+                        sentenceIndex++;
+                        wordIndex = 0;
+                        nextSentence = false;
+                    }
+                }
+            }
+            if (sentenceList.Count != 0 && sentenceList[sentenceIndex].Count == 0)
+                sentenceList.RemoveAt(sentenceIndex);
+            return sentenceList;
+        }
+
+        private static bool IsSplitSymbol(char c)
+        {
+            foreach (var splitSymbol in SplitSymbols)
+                if (c.Equals(splitSymbol))
+                    return true;
+            return false;
         }
     }
 }
