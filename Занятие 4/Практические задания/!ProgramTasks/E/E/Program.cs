988ebb9615026df47258b45a40c1468810f02b0f
Яровой 3834 <yarik.kuskov95@gmail.com>
2024-11-02 18:55:43 +0300

    Add files via upload
    lesson 4: make E

---
+++ Занятие 4/Практические задания/!ProgramTasks/E/E/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace E
+{
+    internal class Program
+    {
+        enum Suits
+        {
+            Wands,
+            Coins,
+            Cups,
+            Swords
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSuit(Suits.Wands));
+            Console.WriteLine(GetSuit(Suits.Coins));
+            Console.WriteLine(GetSuit(Suits.Cups));
+            Console.WriteLine(GetSuit(Suits.Swords));
+        }
+        private static string GetSuit(Suits suit)
+        {
+            return new string[] { "жезлов", "монет", "кубков", "мечей" }[Convert.ToInt32(suit)];
+        }
+
+    }
+
+}
