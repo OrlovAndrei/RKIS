d9607688c54b6fdd63d810413a7017d8f1d57d51
Али <allaoa215@bk.ru>
2024-11-02 13:58:04 +0300

    lesson 4; make A

--- Занятие 4/Практические задания/!ProgramTasks/A/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/A/Program.cs
@@ -1,10 +1,19 @@
-public static int[] GetFirstEvenNumbers(int count)
+﻿namespace A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
         {
-            var array = new int[count];
-            for (int i = 0; i < array.Length; i++)
-                array[i] = 2*(i+1);
-            return array;
+            foreach (var num in GetFirstEvenNumbers(100))
+                Console.WriteLine(num);
         }
 
+        public static int[] GetFirstEvenNumbers(int count)
+        {
+            int[] evenNumbers = new int[count];
+            for (int i = 0; i < count; i++)
+                evenNumbers[i] = 2 * (i + 1);
+            return evenNumbers;
+        }
     }
-
+}
