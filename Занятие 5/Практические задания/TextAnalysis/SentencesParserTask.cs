6b987eff4dd7f7304b50c0d37d26239c6627bb48
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-11-02 20:23:10 +0300

    lesson 5, SentencesParserTask.cs

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,11 +1,46 @@
-﻿namespace TextAnalysis;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextAnalysis;
 
 static class SentencesParserTask
 {
     public static List<List<string>> ParseSentences(string text)
     {
         var sentencesList = new List<List<string>>();
-        //...
+
+
+if (text == null) return null;  
+       text = text.ToLower();
+       var sentences = text.Split(".:;?!()".ToCharArray(), 
+           StringSplitOptions.RemoveEmptyEntries);
+       foreach (var sent in sentences)
+       {
+            var listWords = new List<string>();  
+            var builder = new StringBuilder();
+            foreach (var ch in sent)
+            {
+                if (char.IsLetter(ch) || ch == '\'')
+                    builder.Append(ch);
+                else
+                    AddNotEmptyWord( builder, listWords);
+            }
+            AddNotEmptyWord(builder, listWords);
+            if (listWords.Count > 0)
+                listSentences.Add(listWords);
+       }
         return sentencesList;
     }
+
+    public static void AddNotEmptyWord(StringBuilder builder, List<string> listWords)
+    {
+        if (builder.Length > 0)
+        {
+            listWords.Add(builder.ToString());
+            builder.Clear();
+        }
+    }
+    
 }
