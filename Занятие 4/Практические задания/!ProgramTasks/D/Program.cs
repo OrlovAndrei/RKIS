dc9e2b647e97599f4014dd9bef38bbb3313d48e9
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-11-02 19:57:04 +0300

    lesson 4, D

--- Занятие 4/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/D/Program.cs
@@ -14,10 +14,23 @@ namespace D
 
         public static int FindSubarrayStartIndex(int[] array, int[] subArray)
         {
-            for (var i = 0; i < array.Length - subArray.Length + 1; i++)
+            for (var i = 0; i < array.Length - subArray.Length; i++)
+            {
                 if (ContainsAtIndex(array, subArray, i))
                     return i;
+            }
             return -1;
+        }    
+        public static bool ContainsAtIndex(int[] array, int[] subArray, int startindex)
+        {
+            if (subArray.Length == 0)
+                return true;
+            for (int j = 0; j < subArray.Length; j++)
+            {
+                    if (array[startindex + j] != subArray[j])
+                    return false;  
+            }
+            return true;
         }
     }
 }
