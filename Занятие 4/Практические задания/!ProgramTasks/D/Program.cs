bd48cad1a05b1d82fd05cf53e71cef7db693ce74
Di-Ay <92014990+Di-Ay@users.noreply.github.com>
2024-11-02 19:48:56 +0300

    lesson 4, D

--- Занятие 4/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/D/Program.cs
@@ -7,6 +7,9 @@ namespace D
         static void Main(string[] args)
         {
             Console.WriteLine(FindSubarrayStartIndex(new[] { 1, 2, 4, 1, 2 }, new[] { 1, 2 }));
+            Console.WriteLine(FindSubarrayStartIndex(new[] { 1, 2, 3, 4, 3, 4 }, new[] { 3, 4 }));  
+            Console.WriteLine(FindSubarrayStartIndex(new[] { 1, 2, 3 }, new[] { 4 }));  
+            Console.WriteLine(FindSubarrayStartIndex(new[] { 1, 2, 3 }, new int[0]));
         }
 
         public static int FindSubarrayStartIndex(int[] array, int[] subArray)
