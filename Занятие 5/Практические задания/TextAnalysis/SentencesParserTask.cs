e73e156b0dee4b1cd97db93f70819087a9a78e19
C-conscious <167548148+DeDaL97@users.noreply.github.com>
2024-11-02 22:57:51 +0300

    Lesson 5: SentencesParserTask

--- Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/SentencesParserTask.cs
@@ -1,11 +1,27 @@
-﻿namespace TextAnalysis;
-
-static class SentencesParserTask
+﻿namespace SentencesParserTask
 {
-    public static List<List<string>> ParseSentences(string text)
+    static class TextAnalysis
     {
-        var sentencesList = new List<List<string>>();
-        //...
-        return sentencesList;
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var result = new List<List<string>>();
+            var result = new List<List<string>>();
+            var sentences = Regex.Split(text, @"(?<=[.!?;:()])\s*");
+            foreach (var sentence in sentences)
+            {
+                var cleanedSentence = sentence.Trim();
+                var words = Regex.Matches(cleanedSentence, @"[a-zA-Zа-яА-ЯёЁ']+");
+                var wordList = new List<string>();
+                foreach (Match word in words)
+                {
+                    wordList.Add(word.Value.ToLower());
+                }
+                if (wordList.Count > 0)
+                {
+                    result.Add(wordList);
+                }
+            }
+            return result;
+        }
     }
-}
+ }
