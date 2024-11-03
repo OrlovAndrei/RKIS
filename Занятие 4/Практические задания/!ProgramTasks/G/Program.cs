99a7a0793ee8d6f267a2a70982adde0f6a7a28e9
Evgeny <pekarink@gmail.com>
2024-11-03 22:05:49 +0300

    4.G

--- Занятие 4/Практические задания/!ProgramTasks/G/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/G/Program.cs
@@ -18,14 +18,24 @@
             PrintArray(GetPoweredArray(new[] { 42 }, 0));
         }
 
-        public static void PrintArray(int[] arr) {
+        public static void PrintArray(int[] arr)
+        {
             foreach (var item in arr)
                 Console.WriteLine(item);
         }
 
         public static int[] GetPoweredArray(int[] arr, int power)
         {
-            ...
+            // Создаем новый массив той же длины
+            int[] poweredArray = new int[arr.Length];
+
+            // Заполняем новый массив результатами возведения в степень
+            for (int i = 0; i < arr.Length; i++)
+            {
+                poweredArray[i] = (int)Math.Pow(arr[i], power);
+            }
+
+            return poweredArray;
         }
     }
 }
