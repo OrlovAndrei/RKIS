07508835219d07dc6d09d418a10fc1495d0c390d
Evgeniy <85281694+poltoraska@users.noreply.github.com>
2024-11-03 17:30:14 +0300

    Lesson 4: make HeatmapTask

--- Занятие 4/Практические задания/Names/HeatmapTask.cs
+++ Занятие 4/Практические задания/Names/HeatmapTask.cs
@@ -4,32 +4,28 @@ namespace Names
 {
     internal static class HeatmapTask
     {
+
         public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
         {
-            const int minimumDay = 2;
-            const int maximumDay = 31;
-            const int minimumMonth = 1;
-            const int maximumMonth = 12;
-            var days = new string[maximumDay - minimumDay + 1];
-            for (var i = 0; i < days.Length; i++)
-            {
-                days[i] = (i + minimumDay).ToString();
-            }
+            var minMonth = 1;
+            var maxMonth = 12;
+            var minDay = 2;
+            var maxDay = 31;
 
-            var months = new string[maximumMonth];
+            var months = new string[maxMonth];
             for (var i = 0; i < months.Length; i++)
-            {
-                months[i] = (i + minimumMonth).ToString();
-            }
+                months[i] = (i + minMonth).ToString();
+
+            var days = new string[maxDay - minDay + 1];
+            for (var i = 0; i < days.Length; i++)
+                days[i] = (i + minDay).ToString();
+
             var birthCounts = new double[days.Length, months.Length];
             foreach (var name in names)
             {
-                if (name.BirthDate.Day >= minimumDay)
-                {
-                    birthCounts[name.BirthDate.Day - minimumDay, name.BirthDate.Month - minimumMonth]++;
-                }
+                if (name.BirthDate.Day > 1)
+                    birthCounts[(name.BirthDate.Day - minDay), (name.BirthDate.Month - minMonth)]++;
             }
-
             return new HeatmapData("Карта интенсивностей рождаемости", birthCounts, days, months);
         }
     }
