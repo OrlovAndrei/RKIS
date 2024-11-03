b1e8c22d5ab197c4b58fecbc3cb3ca624bcbcb77
Evgeny <pekarink@gmail.com>
2024-11-03 21:43:35 +0300

    1.E

--- Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/E/Program.cs
@@ -7,7 +7,7 @@ namespace E
         static void Main(string[] args)
         {
             string doubleNumber = "894376.243643";
-            double number = double.Parse(doubleNumber, CultureInfo.InvariantCulture); // ���� ������, ��� ������ ���-�� ���
+            double number = double.Parse(doubleNumber, CultureInfo.InvariantCulture); 
             Console.WriteLine(number + 1);
         }
     }
