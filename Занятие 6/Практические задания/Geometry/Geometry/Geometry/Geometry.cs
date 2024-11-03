a9983cac41187f2d79791f4870cf9220ccbb96b0
darkarat <dar.karat3011@gmail.com>
2024-11-03 03:56:17 +0300

    lesson 6: make Geometry

---
+++ Занятие 6/Практические задания/Geometry/Geometry/Geometry/Geometry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Geometry
+{
+    internal class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt(Math.Pow(vector.X, 2) + Math.Pow(vector.Y, 2));
+        }
+        public static Vector Add(Vector vector, Vector vector2)
+        {
+            Vector vector3 = new Vector
+            {
+                X = vector.X + vector2.X,
+                Y = vector.Y + vector2.Y
+            };
+            return vector3;
+        }
+    }
+}
---
+++ Занятие 6/Практические задания/Geometry/Geometry/Geometry/Vector.cs
@@ -0,0 +1,8 @@
+﻿namespace Geometry
+{
+    public class Vector
+    {
+        public double X {get;set;}
+        public double Y {get;set;}
+    }
+}
