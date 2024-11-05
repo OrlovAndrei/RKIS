167bdeb395f4021c0f330d2ab41de742e8d99241
Evgeniy <85281694+poltoraska@users.noreply.github.com>
2024-11-05 14:55:59 +0300

    Update HistogramSample.cs

--- Занятие 4/Практические задания/Names/HistogramSample.cs
+++ Занятие 4/Практические задания/Names/HistogramSample.cs
@@ -1,33 +1,32 @@
 using System;
+using System.Reflection.Emit;
 
 namespace Names;
 
-internal static class HistogramSample
+internal static class HistogramTask
 {
-	// Пример подготовки данных для построения графиков:
-	public static HistogramData GetHistogramBirthsByYear(NameData[] names)
-	{
-		/*
-		Подготовка данных для построения гистограммы 
-		— количества людей в выборке в зависимости от года их рождения.
-		*/
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var minDay = 2;
+        var maxDay = 31;
+        foreach (var day in names)
+        {
+            minDay = Math.Min(minDay, day.BirthDate.Day);
+            maxDay = Math.Max(maxDay, day.BirthDate.Day);
+        }
 
-		Console.WriteLine("Статистика рождаемости по годам");
-		var minYear = int.MaxValue;
-		var maxYear = int.MinValue;
-		foreach (var name in names)
-		{
-			minYear = Math.Min(minYear, name.BirthDate.Year);
-			maxYear = Math.Max(maxYear, name.BirthDate.Year);
-		}
+        var days = new string[maxDay - minDay + 1];
 
-		var years = new string[maxYear - minYear + 1];
-		for (var y = 0; y < years.Length; y++)
-			years[y] = (y + minYear).ToString();
-		var birthsCounts = new double[maxYear - minYear + 1];
-		foreach (var name in names)
-			birthsCounts[name.BirthDate.Year - minYear]++;
+        for (var y = 0; y < days.Length; y++)
+            days[y] = (y + minDay).ToString();
 
-		return new HistogramData("Рождаемость по годам", years, birthsCounts);
-	}
+        var birthsCounts = new double[maxDay - minDay + 1];
+
+        foreach (var day in names)
+        {
+            if (day.Name == name && day.BirthDate.Day > 1)
+                birthsCounts[day.BirthDate.Day - minDay]++;
+        }
+        return new HistogramData(String.Format("Рождаемость людей с именем '{0}'", name), days, birthsCounts);
+    }
 }
