74eb974107b99419b77b7914d12946e9153ad3a8
Programmuer <dashazagorulko06@gmail.com>
2024-11-05 20:56:00 +0300

    Update Program.cs

--- Занятие 4/Практические задания/!ProgramTasks/G/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/G/Program.cs
@@ -23,9 +23,14 @@
                 Console.WriteLine(item);
         }
 
-        public static int[] GetPoweredArray(int[] arr, int power)
+         public static int[] GetPoweredArray(int[] arr, int power)
         {
-            ...
+            int[] result = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result[i] = (int)Math.Pow(arr[i], power);
+            }
+            return result;
         }
     }
 }
