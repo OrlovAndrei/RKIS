ecda74e6c21dffa016b261251a447468ed8c0cc5
Evgeniy <85281694+poltoraska@users.noreply.github.com>
2024-11-05 14:59:11 +0300

    Update TextGeneratorTask.cs

--- Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
+++ Занятие 5/Практические задания/TextAnalysis/TextGeneratorTask.cs
@@ -2,15 +2,15 @@ using System.Text;
 namespace TextAnalysis;
 static class TextGeneratorTask
 {
-    public static string ContinuePhrase(Dictionary<string, string> frequentNextWords, string initialWord, int totalWordsCount)
+    public static string ContinuePhrase(Dictionary<string, string> freqNextWords, string initWord, int totalWordsCount)
     {
-        StringBuilder resultPhrase = new StringBuilder(initialWord);
+        StringBuilder resultPhrase = new StringBuilder(initWord);
         for (int index = 1; index < totalWordsCount; index++)
         {
-            if (!frequentNextWords.ContainsKey(initialWord))
+            if (!freqNextWords.ContainsKey(initWord))
                 break;
-            initialWord = frequentNextWords[initialWord];
-            resultPhrase.Append(" " + initialWord);
+            initWord = freqNextWords[initWord];
+            resultPhrase.Append(" " + initWord);
         }
         return resultPhrase.ToString();
     }
