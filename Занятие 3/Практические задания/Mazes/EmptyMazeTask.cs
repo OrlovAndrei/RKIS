55dbcffecfa730f5a390f95aec974b89f0ac3312
Git-art-dark <vladimirrumancev27544@gmail.com>
2024-11-06 14:55:38 +0300

    rebase

--- Занятие 3/Практические задания/Mazes/EmptyMazeTask.cs
+++ Занятие 3/Практические задания/Mazes/EmptyMazeTask.cs
@@ -1,9 +1,27 @@
-﻿namespace Mazes;
-
-public static class EmptyMazeTask
+﻿namespace Mazes
 {
-	public static void MoveOut(Robot robot, int width, int height)
-	{
-		
-	}
+    public static class EmptyMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            MoveRight(robot, width);
+            MoveDown(robot, height);
+        }
+
+        private static void MoveRight(Robot robot, int count)
+        {
+            for (int i = 0; i < count - 3; i++)
+            {
+                robot.MoveTo(Direction.Right);
+            }
+        }
+
+        private static void MoveDown(Robot robot, int count)
+        {
+            for (int i = 0; i < count - 3; i++)
+            {
+                robot.MoveTo(Direction.Down);
+            }
+        }
+    }
 }
