81728303a69645715be42c1244db130335354d56
Git-art-dark <vladimirrumancev27544@gmail.com>
2024-11-06 16:43:27 +0300

    Lesson 6: make GeometryPainting

--- Занятие 6/Практические задания/GeometryPainting/SegmentExtensions.cs
+++ Занятие 6/Практические задания/GeometryPainting/SegmentExtensions.cs
@@ -2,6 +2,56 @@
 using Avalonia.Media;
 using GeometryTasks;
 
-namespace GeometryPainting;
+namespace GeometryPainting
+{
+    public static class SegmentExtensions
+    {
+        private static readonly Dictionary<Segment, Color> segmentColors = new Dictionary<Segment, Color>();
 
-//Напишите здесь код, который заставит работать методы segment.GetColor и segment.SetColor
+        public static void SetColor(this Segment segment, Color newColor)
+        {
+            if (newColor == null)
+            {
+                return;
+            }
+
+            if (segmentColors.ContainsKey(segment))
+            {
+                segmentColors[segment] = newColor;
+            }
+            else
+            {
+                segmentColors.Add(segment, newColor);
+            }
+        }
+        
+        public static bool Contains(Segment segment, Color newColor)
+        {
+            if (segment == null)
+            {
+                return false;
+            }
+
+            return segmentColors.TryGetValue(segment, out Color currentColor) && currentColor != newColor;
+        }
+
+        public static Color GetColor(this Segment segment)
+        {
+            if (segment != null && segmentColors.TryGetValue(segment, out Color retrievedColor))
+            {
+                return retrievedColor;
+            }
+            return Color.FromRgb(0, 0, 0);
+        }
+
+        public static bool Equal(Segment firstSegment, Segment secondSegment)
+        {
+            if (firstSegment == null || secondSegment == null)
+            {
+                return false;
+            }
+
+            return firstSegment.Begin.X == secondSegment.Begin.X && firstSegment.Begin.Y == secondSegment.Begin.Y && firstSegment.End.X == secondSegment.End.X && firstSegment.End.Y == secondSegment.End.Y;
+        }
+    }
+}
