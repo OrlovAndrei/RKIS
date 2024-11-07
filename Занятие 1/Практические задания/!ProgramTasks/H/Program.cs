72bf76bba2957f29d5be012f198989aea6e5d6ce
Bogdan Metelkin <simpl.top48786@gmai.com>
2024-11-07 23:31:21 +0300

    lesson 1 : fix H

--- Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/H/Program.cs
@@ -2,14 +2,6 @@
 {
     internal class Program
     {
-        static int GetSquere(int number)
-        {
-            return Math.Pow(number, 2);
-        }
-        static void Print(int number)
-        {
-            Console.WriteLine(number.ToString());
-        }
         static void Main(string[] args)
         {
             Print(GetSquare(42));
