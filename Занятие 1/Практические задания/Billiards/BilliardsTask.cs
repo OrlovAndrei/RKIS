ff3bf9f8bf4b33e85d681f8bc55cfbb7e0e957ed
Bogdan Metelkin <simpl.top48786@gmai.com>
2024-11-07 22:15:55 +0300

    lesson 1: fix BilliardsTask

--- Занятие 1/Практические задания/Billiards/BilliardsTask.cs
+++ Занятие 1/Практические задания/Billiards/BilliardsTask.cs
@@ -4,7 +4,6 @@ public static class BilliardsTask
 {
     public static double BounceWall(double directionRadians, double wallInclinationRadians)
     {
-        return 0.0;
         double result = (2 * wallInclinationRadians) - directionRadians;
         
             return result;
