1abbb39fa4cdbd135db7287c64067b6fa7ffe630
Bogdan Metelkin <simpl.top48786@gmai.com>
2024-11-07 21:47:06 +0300

    lesson 1: fix C

--- Занятие 1/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 1/Практические задания/!ProgramTasks/C/Program.cs
@@ -6,7 +6,7 @@
         {
             double pi = Math.PI;
             int tenThousand =(int)10000L;
-            float tenThousandPi =(float)pi * tenThousand);
+            float tenThousandPi =(float) (pi * tenThousand);
             int roundedTenThousandPi =(int)Math.Round(tenThousandPi);
             int integerPartOfTenThousandPi =(int)tenThousandPi;
             Console.WriteLine(integerPartOfTenThousandPi);
