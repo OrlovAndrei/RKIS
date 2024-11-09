7b28170670d40ceb05bc8eb68373c7f712747e1d
Evgeniy <85281694+poltoraska@users.noreply.github.com>
2024-11-09 17:39:28 +0300

    Update Vector.cs

--- Занятие 6/Практические задания/Geometry/Vector.cs
+++ Занятие 6/Практические задания/Geometry/Vector.cs
@@ -5,18 +5,21 @@
         public double X;
         public double Y;
         public double GetLength()
+            
         {
             return Geometry.GetLength(this);
         }
 
 
         public Vector Add(Vector vector)
+            
         {
             return Geometry.Add(vector, this);
         }
 
 
         public bool Belongs(Segment segment)
+            
         {
             return Geometry.IsVectorInSegment(this, segment);
         }
