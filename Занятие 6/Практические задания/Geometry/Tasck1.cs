1fd5578958f4503c236adfa72661fa3c40a67168
Ardashev Sasha <ardashev004@gmail.com>
2024-11-09 17:15:49 +0300

    lesson6: Task1

---
+++ Занятие 6/Практические задания/Geometry/Tasck1.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Geometry
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+    }
+
+    public static class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+        }
+
+        public static Vector Add(Vector v1, Vector v2)
+        {
+            return new Vector { X = v1.X + v2.X, Y = v1.Y + v2.Y };
+        }
+    }
+}
