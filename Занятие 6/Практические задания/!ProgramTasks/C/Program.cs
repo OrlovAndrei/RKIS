bac5bf9e3caf1343a07e96733d1e02b87bc7a13d
GOL <164672521+accum104@users.noreply.github.com>
2024-11-09 22:19:27 +0300

    lesson 6 make C

--- Занятие 6/Практические задания/!ProgramTasks/C/Program.cs
+++ Занятие 6/Практические задания/!ProgramTasks/C/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace C
+namespace C
 {
+    public static class StringExtensions
+    {
+        public static int ToInt(this string str)
+        {
+            return int.TryParse(str, out var result) ? result : 0;
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
-        {
-            var arg1 = "100500";
-            Console.Write(arg1.ToInt() + "42".ToInt()); // 100542
-        }
-    }
-}
