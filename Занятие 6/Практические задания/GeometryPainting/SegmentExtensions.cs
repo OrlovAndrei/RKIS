033309e64e95527045bba197e8f6ccff6eb763f8
Alex <Alexeychik-uss@mail.ru>
2024-11-10 11:19:56 +0300

    Lesson 6: SegmentExtensions

--- Занятие 6/Практические задания/GeometryPainting/SegmentExtensions.cs
+++ Занятие 6/Практические задания/GeometryPainting/SegmentExtensions.cs
@@ -4,4 +4,25 @@ using GeometryTasks;
 
 namespace GeometryPainting;
 
-//Напишите здесь код, который заставит работать методы segment.GetColor и segment.SetColor
+public static class SegmentExtensions
+{
+    private static readonly Dictionary<Segment, Color> segmentColorMap = new Dictionary<Segment, Color>();
+
+    public static Color GetColor(this Segment segment)
+    {
+        if (segmentColorMap.ContainsKey(segment)) return segmentColorMap[segment];
+        else return Colors.Black;
+    }
+
+    public static void SetColor(this Segment segment, Color color)
+    {
+        if (segmentColorMap.ContainsKey(segment))
+        {
+            segmentColorMap[segment] = color;
+        }
+        else
+        {
+            segmentColorMap.Add(segment, color);
+        }
+    }
+}
