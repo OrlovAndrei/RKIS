54a1c7926fb958a74620c87d76e7bf106c73dd73
Agni <63151335+crash36663@users.noreply.github.com>
2024-11-10 17:17:46 +0300

    Create Geometry3.cs

---
+++ Занятие 6/Практические задания/Geometry/Geometry3.cs
@@ -0,0 +1,33 @@
+namespace Geometry
+{
+    public class Vector
+    {
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public Vector Add(Vector other)
+        {
+            return Geometry.Add(this, other);
+        }
+
+        public bool Belongs(Segment segment)
+        {
+            return Geometry.IsVectorInSegment(this, segment);
+        }
+    }
+
+    public class Segment
+    {
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+
+        public bool Contains(Vector vector)
+        {
+            return Geometry.IsVectorInSegment(vector, this);
+        }
+    }
+}
