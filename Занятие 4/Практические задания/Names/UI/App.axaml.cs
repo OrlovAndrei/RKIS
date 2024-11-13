f41dd31f5d47d46b6857be69acda8b873e105b8c
42808 <42808@SRV-13>
2024-11-13 14:44:28 +0300

    Lesson 4: Names

--- Занятие 4/Практические задания/Names/UI/App.axaml.cs
+++ Занятие 4/Практические задания/Names/UI/App.axaml.cs
@@ -2,23 +2,25 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace Names.UI;
-
-public partial class App : Application
+namespace Names.UI
 {
-	public override void Initialize()
-	{
-		AvaloniaXamlLoader.Load(this);
-	}
 
-	public override void OnFrameworkInitializationCompleted()
+	public partial class App : Application
 	{
-		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+		public override void Initialize()
 		{
-			desktop.MainWindow = new MainWindow();
+			AvaloniaXamlLoader.Load(this);
 		}
 
-		base.OnFrameworkInitializationCompleted();
-		Program.ShowPlots();
+		public override void OnFrameworkInitializationCompleted()
+		{
+			if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+			{
+				desktop.MainWindow = new MainWindow();
+			}
+
+			base.OnFrameworkInitializationCompleted();
+			Program.ShowPlots();
+		}
 	}
 }
