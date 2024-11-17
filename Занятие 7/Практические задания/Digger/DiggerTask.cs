1c33fcaa7051cb06cda5d374810bbfffc255b359
Alex <Alexeychik-uss@mail.ru>
2024-11-17 11:38:06 +0300

    Lesson 7. DiggerTask1

--- Занятие 7/Практические задания/Digger/DiggerTask.cs
+++ Занятие 7/Практические задания/Digger/DiggerTask.cs
@@ -1,6 +1,42 @@
 ﻿using System;
 using Avalonia.Input;
+using Digger.Architecture;
 
 namespace Digger;
 
-//Напишите здесь классы Player, Terrain и другие.
+class Player : ICreature
+{
+    public CreatureCommand Act(int x, int y)
+    {
+        var command = new CreatureCommand();
+        var direction = Game.KeyPressed;
+
+        if (direction == Key.Right && x + 1 < Game.MapWidth)
+            command.DeltaX++;
+        else if (direction == Key.Left && x - 1 >= 0)
+            command.DeltaX--;
+        else if (direction == Key.Down && y + 1 < Game.MapHeight)
+            command.DeltaY++;
+        else if (direction == Key.Up && y - 1 >= 0)
+            command.DeltaY--;
+
+        return command;
+    }
+
+    public bool DeadInConflict(ICreature conflictedObject) => false;
+
+    public int GetDrawingPriority() => 0;
+
+    public string GetImageFileName() => "Digger.png";
+}
+
+class Terrain : ICreature
+{
+    public CreatureCommand Act(int x, int y) => new();
+
+    public bool DeadInConflict(ICreature conflictedObject) => true;
+
+    public int GetDrawingPriority() => 1;
+
+    public string GetImageFileName() => "Terrain.png";
+}
