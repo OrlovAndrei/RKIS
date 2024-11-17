891bf3fba4e3febfc7f4e2db7c8bf19eefdc46d6
Alex <Alexeychik-uss@mail.ru>
2024-11-17 14:43:14 +0300

    Lesson 7. DiggerTask3

--- Занятие 7/Практические задания/Digger/DiggerTask.cs
+++ Занятие 7/Практические задания/Digger/DiggerTask.cs
@@ -31,7 +31,7 @@ class Player : ICreature
                Game.Map[targetX, targetY] is not Sack;
     }
 
-    public bool DeadInConflict(ICreature conflictedObject) => conflictedObject is Sack;
+    public bool DeadInConflict(ICreature conflictedObject) => conflictedObject is Sack || conflictedObject is Monster;
 
     public int GetDrawingPriority() => 0;
 
@@ -43,12 +43,13 @@ class Terrain : ICreature
     public CreatureCommand Act(int x, int y) => new()
     {
         DeltaX = 0,
-        DeltaY = 0
+        DeltaY = 0,
+        TransformTo = this
     };
 
-    public bool DeadInConflict(ICreature conflictedObject) => true;
+    public bool DeadInConflict(ICreature conflictedObject) => conflictedObject is Player;
 
-    public int GetDrawingPriority() => 5;
+    public int GetDrawingPriority() => 100;
 
     public string GetImageFileName() => "Terrain.png";
 }
@@ -57,12 +58,18 @@ class Sack : ICreature
 {
     private int fallCount;
     private bool isFalling;
+    public bool IsFalling => isFalling;
 
     public CreatureCommand Act(int x, int y)
     {
-        bool canFall = y + 1 < Game.MapHeight &&
-                       (Game.Map[x, y + 1] == null ||
-                        (Game.Map[x, y + 1] is Player && isFalling));
+        var command = new CreatureCommand
+        {
+            DeltaX = 0,
+            DeltaY = 1,
+            TransformTo = this
+        };
+
+        bool canFall = CanFallTo(x + command.DeltaX, y + command.DeltaY);
 
         if (canFall)
         {
@@ -105,9 +112,19 @@ class Sack : ICreature
         isFalling = false;
     }
 
+    private bool CanFallTo(int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= Game.MapWidth || y >= Game.MapHeight)
+            return false;
+
+        var cell = Game.Map.GetValue(x, y);
+
+        return cell == null || (cell is Monster || cell is Player) && fallCount > 0;
+    }
+
     public bool DeadInConflict(ICreature conflictedObject) => false;
 
-    public int GetDrawingPriority() => 3;
+    public int GetDrawingPriority() => 10;
 
     public string GetImageFileName() => "Sack.png";
 }
@@ -123,7 +140,99 @@ class Gold : ICreature
         return true;
     }
 
-    public int GetDrawingPriority() => 3;
+    public int GetDrawingPriority() => 10;
 
     public string GetImageFileName() => "Gold.png";
+}
+
+public class Monster : ICreature
+{
+    public string GetImageFileName() => "Monster.png";
+
+    public int GetDrawingPriority() => 20;
+
+    public CreatureCommand Act(int x, int y)
+    {
+        var command = new CreatureCommand
+        {
+            DeltaX = 0,
+            DeltaY = 0,
+            TransformTo = this
+        };
+
+        if (TryMoveTowardsPlayer(x, y, out var deltaX, out var deltaY))
+        {
+            command.DeltaX = deltaX;
+            command.DeltaY = deltaY;
+        }
+
+        return command;
+    }
+
+    private bool TryMoveTowardsPlayer(int x, int y, out int deltaX, out int deltaY)
+    {
+        deltaX = 0;
+        deltaY = 0;
+
+        if (IsPlayerInSection(0, 0, x, Game.MapHeight) && CanMoveTo(x - 1, y))
+        {
+            deltaX = -1;
+            return true;
+        }
+
+        if (IsPlayerInSection(x + 1, 0, Game.MapWidth, Game.MapHeight) && CanMoveTo(x + 1, y))
+        {
+            deltaX = 1;
+            return true;
+        }
+
+        if (IsPlayerInSection(0, 0, Game.MapWidth, y) && CanMoveTo(x, y - 1))
+        {
+            deltaY = -1;
+            return true;
+        }
+
+        if (IsPlayerInSection(0, y + 1, Game.MapWidth, Game.MapHeight) && CanMoveTo(x, y + 1))
+        {
+            deltaY = 1;
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool IsPlayerInSection(int x0, int y0, int x1, int y1)
+    {
+        for (var x = x0; x < x1; x++)
+        {
+            for (var y = y0; y < y1; y++)
+            {
+                if (Game.Map.GetValue(x, y) is ICreature creature && creature is Player)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private bool CanMoveTo(int x, int y)
+    {
+        if (IsOutOfBounds(x, y)) return false;
+        var cell = Game.Map.GetValue(x, y) as ICreature;
+        return cell == null || IsMoveableCell(cell);
+    }
+
+    private bool IsOutOfBounds(int x, int y)
+    {
+        return x < 0 || y < 0 || x >= Game.MapWidth || y >= Game.MapHeight;
+    }
+
+    private bool IsMoveableCell(ICreature cell)
+    {
+        return !(cell is Sack || cell is Monster || cell is Terrain);
+    }
+
+    public bool DeadInConflict(ICreature conflictedObject)
+    {
+        return conflictedObject is Monster || (conflictedObject is Sack sack && sack.IsFalling);
+    }
 }
