456f47c06d352765f9a3f44a68de45bf365f0c88
SXDIST <nightmoon.yt228@gmail.com>
2024-11-18 21:18:02 +0300

    Lesson 4: Make HeatmapTask

--- Занятие 4/Практические задания/Names/HeatmapTask.cs
+++ Занятие 4/Практические задания/Names/HeatmapTask.cs
@@ -1,13 +1,32 @@
-﻿namespace Names;
+﻿using System;
+using System.Linq;
+
+namespace Names;
 
 internal static class HeatmapTask
 {
     public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
     {
+        var xLabels = Enumerable.Range(2, 30).Select(day => day.ToString()).ToArray();
+        var yLabels = Enumerable.Range(1, 12).Select(month => month.ToString()).ToArray();
+
+        var birthsCounts = new double[30, 12];
+
+        foreach (var person in names)
+        {
+            if (person.BirthDate.Day == 1)
+                continue;
+
+            int dayIndex = person.BirthDate.Day - 2;
+            int monthIndex = person.BirthDate.Month - 1;
+
+            birthsCounts[dayIndex, monthIndex]++;
+        }
+
         return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+            "Карта интенсивностей рождаемости",
+            birthsCounts,
+            xLabels,
+            yLabels);
     }
 }
