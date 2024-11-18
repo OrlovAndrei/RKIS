98fd842254673998803f3bd6994b30b57da5d07f
SXDIST <nightmoon.yt228@gmail.com>
2024-11-18 13:15:15 +0300

    Lesson 4: Make D

--- Занятие 4/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 4/Практические задания/!ProgramTasks/D/Program.cs
@@ -7,14 +7,30 @@ namespace D
         static void Main(string[] args)
         {
             Console.WriteLine(FindSubarrayStartIndex(new[] { 1, 2, 4, 1, 2 }, new[] { 1, 2 }));
+            Console.WriteLine(FindSubarrayStartIndex(new[] { 1, 2, 3, 4, 3, 4 }, new[] { 3, 4 }));
+            Console.WriteLine(FindSubarrayStartIndex(new[] { 1, 2, 3 }, new int[] { }));
+            Console.WriteLine(FindSubarrayStartIndex(new[] { 1, 2, 3 }, new[] { 4 }));
         }
 
         public static int FindSubarrayStartIndex(int[] array, int[] subArray)
         {
-            for (var i = 0; i < array.Length - subArray.Length + 1; i++)
+            if (subArray.Length == 0)
+                return 0;
+
+            for (var i = 0; i <= array.Length - subArray.Length; i++)
                 if (ContainsAtIndex(array, subArray, i))
                     return i;
             return -1;
+        }
+
+        public static bool ContainsAtIndex(int[] array, int[] subArray, int startIndex)
+        {
+            for (int i = 0; i < subArray.Length; i++)
+            {
+                if (array[startIndex + i] != subArray[i])
+                    return false;
+            }
+            return true;
         }
     }
 }
