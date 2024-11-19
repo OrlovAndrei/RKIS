873475f08956cbd67173088ce4cf4cb2e5435bd4
SXDIST <nightmoon.yt228@gmail.com>
2024-11-19 08:22:12 +0300

    Lesson 5: Make D

--- Занятие 5/Практические задания/!ProgramTasks/D/Program.cs
+++ Занятие 5/Практические задания/!ProgramTasks/D/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace D
+﻿namespace D;
+
+internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var filePath = "operations.txt";
+
+        if (!File.Exists(filePath))
         {
+            Console.WriteLine($"Файл {filePath} не найден.");
+            return;
         }
+
+        var commands = File.ReadAllLines(filePath);
+
+        var result = DecodeOperations(commands);
+
+        Console.WriteLine("Результат работы стека:");
+        Console.WriteLine(result);
+    }
+
+    private static string DecodeOperations(string[] commands)
+    {
+        var stack = string.Empty;
+
+        foreach (var command in commands)
+            if (command.StartsWith("push "))
+            {
+                var textToPush = command.Substring(5);
+                stack += textToPush;
+            }
+            else if (command.StartsWith("pop "))
+            {
+                if (int.TryParse(command.Substring(4), out var charsToPop) && charsToPop > 0)
+                    stack = stack.Length > charsToPop
+                        ? stack.Substring(0, stack.Length - charsToPop)
+                        : string.Empty;
+            }
+
+        return stack;
     }
 }
