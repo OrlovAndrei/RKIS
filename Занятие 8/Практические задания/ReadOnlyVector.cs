9f440c0bc7f148290d6c53fb366cf9690694ba90
Vlada-en <Edenisova73@mail.ru>
2024-11-21 16:42:45 +0300

    lesson 8: make ReadOnlyVector(задание 3)- первый readonlyvector , что я прислала работает неправильно, исправила

---
+++ Занятие 8/Практические задания/ReadOnlyVector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyVector
+{
+    public class ReadOnlyVector
+    {
+        public double X { get; }
+        public double Y { get; }
+
+        // Конструктор
+        public ReadOnlyVector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // Метод для сложения векторов
+        public ReadOnlyVector Add(ReadOnlyVector other)
+        {
+            if (other == null)
+                throw new ArgumentNullException(nameof(other));
+
+            return new ReadOnlyVector(X + other.X, Y + other.Y);
+        }
+
+        // Метод для создания нового вектора с другим значением X
+        public ReadOnlyVector WithX(double newX)
+        {
+            return new ReadOnlyVector(newX, Y);
+        }
+
+        // Метод для создания нового вектора с другим значением Y
+        public ReadOnlyVector WithY(double newY)
+        {
+            return new ReadOnlyVector(X, newY);
+        }
+    }
+}
+
