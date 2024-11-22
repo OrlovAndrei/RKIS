4edd0c4dddd4fbd215226eb39fb36bba8c73a4d6
nikkh2 <harlamova1@icloud.com>
2024-11-22 14:05:44 +0300

    lesson 8: Make HotelAccounting

--- Занятие 8/Практические задания/HotelAccounting/AccountingModel.cs
+++ Занятие 8/Практические задания/HotelAccounting/AccountingModel.cs
@@ -6,4 +6,64 @@ using System.Threading.Tasks;
 
 namespace HotelAccounting;
 
-//создайте класс AccountingModel здесь
+{
+  public class AccountingModel : ModelBase
+  {
+    private double _price;
+    private int _nightsCount;
+    private double _discount;
+    private double _total;
+    
+    public double Price {
+      get { return _price; }
+      set {
+        if (value < 0)
+        {
+          throw new ArgumentException("Price должна быть неотрицательной");
+        }
+        _price = value;
+        UpdateTotal();
+        Notify(nameof(Price));
+      }
+    }
+
+    public int NightsCount {
+      get { return _nightsCount; }
+      set {
+        if (value <= 0)
+        {
+          throw new ArgumentException("NightsCount должен быть положительным");
+        }
+        _nightsCount = value;
+        UpdateTotal();
+        Notify(nameof(NightsCount));
+      }
+    }
+
+    public double Discount {
+      get { return _discount; }
+      set {
+        _discount = value;
+        UpdateTotal();
+        Notify(nameof(Discount));
+      }
+    }
+
+    public double Total {
+      get { return _total; }
+      set {
+        if (value < 0)
+        {
+          throw new ArgumentException("Total должен быть неотрицательным");
+        }
+        _total = value;
+        UpdateDiscount();
+        Notify(nameof(Total));
+      }
+    }
+    
+    private void UpdateTotal() => _total = _price * _nightsCount * (1 - _discount / 100);
+
+    private void UpdateDiscount() => _discount = 100 - 100 * _total / (_price * _nightsCount);
+  }
+}
