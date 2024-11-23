e3b423bcff9ff733a158b5188637ae08798dfa6f
Dasha2005-zxc <diddasha05@bk.ru>
2024-11-23 21:30:50 +0300

    lesson 8: make AccountingModel.cs

---
+++ Занятие 8/Практические задания/HotelAccounting/AccountingModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAccounting
+{
+    class AccountingModel : ModelBase
+    {
+        private double price;
+        private int nightsCount;
+        private double discount;
+        private double total;
+
+        public double Price
+        {
+            get { return price: }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Price must be non-negative.");
+                price = value;
+                SetNewTotal();
+                Notify(nameof(Price));
+            }
+        }
+
+        public int NightsCount
+        {
+            get { return nightsCount; }
+            set
+            {
+                discount = value;
+                SetNewTotal();
+                Notyfy(nameof(Discount));
+            }
+        }
+
+        public double Total
+        {
+            get { return total; }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Totall must be non-negative.");
+                total = value;
+
+                if (total != Price * NightsCount * (1 - discount / 100))
+                    SetNewDiscount();
+                Notifi(nameof(Total));
+            }
+        }
+        public AccountingModel()
+        {
+            price = 0;
+            nightsCount = 1;
+            discount = 0;
+            total = 0;
+        }
+
+        private void SetNewTotal()
+        {
+            Total = Price * NightsCount * (1 - discount / 100);
+        }
+
+        private void SetNewDiscount()
+        {
+            if (Price > 0 && NightsCount > 0)
+            {
+                Discount = (1 - Total / (Price * NightsCount)) * 100;
+            }
+            else
+            {
+                Discount = 0;
+            }
+        }
+    }
+}
+
+
+        
+
+
+
---
+++ Занятие 8/Практические задания/HotelAccounting/ModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace HotelAccounting;
+
+public class ModelBase : INotifyPropertyChanged
+{
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	protected void Notify(string propertyName)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
