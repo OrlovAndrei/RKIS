ed6d62890b30c56614807fc65c8619f81263b6b1
maxvlzzn <msms60837@gmail.com>
2024-11-24 13:50:16 +0300

    lesson 8: make ReadOnlyVector

---
+++ Занятие 8/Практические задания/ReadOnlyVector/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ReadOnlyVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadOnlyVector vector1 = new ReadOnlyVector(3.0, 4.0);
+            ReadOnlyVector vector2 = new ReadOnlyVector(1.0, 2.0);
+
+            ReadOnlyVector sum = vector1.Add(vector2);
+            Console.WriteLine($"Sum: X = {sum.X}, Y = {sum.Y}"); // Сумма: X = 4.0, Y = 6.0
+
+            ReadOnlyVector modifiedX = vector1.WithX(10.0);
+            Console.WriteLine($"Modified X: X = {modifiedX.X}, Y = {modifiedX.Y}"); // X = 10.0, Y = 4.0
+
+            ReadOnlyVector modifiedY = vector1.WithY(20.0);
+            Console.WriteLine($"Modified Y: X = {modifiedY.X}, Y = {modifiedY.Y}"); // X = 3.0, Y = 20.0
+        }
+    }
+}
---
+++ Занятие 8/Практические задания/ReadOnlyVector/ReadOnlyVector.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyVector
+{
+    public class ReadOnlyVector
+    {
+        public double X { get; }
+        public double Y { get; }
+        public ReadOnlyVector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public ReadOnlyVector Add(ReadOnlyVector other)
+        {
+            if (other == null)
+                throw new ArgumentNullException(nameof(other));
+
+            return new ReadOnlyVector(X + other.X, Y + other.Y);
+        }
+        public ReadOnlyVector WithX(double newX)
+        {
+            return new ReadOnlyVector(newX, Y);
+        }
+        public ReadOnlyVector WithY(double newY)
+        {
+            return new ReadOnlyVector(X, newY);
+        }
+    }
+}
