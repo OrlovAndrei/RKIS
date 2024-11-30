72f0d7026be07ee46cc719b755ce962cbc1763b2
Sofa <sofa.linder@gmail.com>
2024-11-30 12:25:07 +0300

    Update App.axaml.cs1

--- Занятие 8/Практические задания/HotelAccounting/UI/App.axaml.cs
+++ Занятие 8/Практические задания/HotelAccounting/UI/App.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace PocketGoogle.UI;
+namespace HotelAccounting.UI;
 
 public partial class App : Application
 {
