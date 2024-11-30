26b4a041eaffef2b1ccb4f3b6c8b2805fd28473c
Dasha2005-zxc <diddasha05@bk.ru>
2024-11-30 12:31:58 +0300

    lesson 9: make FieldsParserTask.cs

--- Занятие 9/Практические задания/TableParser/FieldsParserTask.cs
+++ Занятие 9/Практические задания/TableParser/FieldsParserTask.cs
@@ -1,14 +1,72 @@
-public static void RunTests()
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TableParser;
 {
-    Test("hello world", new[] { "hello", "world" });
-    Test("hello  world", new[] { "hello", "world" });
-    Test(String.Empty, new string[0]);
-    Test("'x y'", new[] { "x y" });
-    Test(""a 'b' 'c' d"", new[] { "a 'b' 'c' d" });
-    Test("'"1"", new[] { ""1"" });
-    Test("a"b c d e"", new[] { "a", "b c d e" });
-    Test(""b c d e"f", new[] { "b c d e", "f" });
-    Test(" 1 ", new[] { "1" });
-    Test(""a \\"c\\""", new[] { "a "c"" });
-    Test(""\\\\"", new[]{"\"});        
+    public class FieldsParserTask
+{
+    private static readonly char[] StopChars = { ' ', '\"', '\'' };
+
+    public static List<string> ParseLine(string line)
+    {
+        var tokenizedString = new List<string>();
+
+        for (var startIndex = 0; startIndex < line.Length;)
+        {
+            var nextToken = ReadField(line, startIndex);
+
+            if (nextToken.Value != " " || (nextToken.Value == " " && nextToken.Length > 1))
+                tokenizedString.Add(nextToken.Value);
+
+            startIndex += nextToken.Length;
+        }
+
+        return tokenizedString;
+    }
+
+    private static Token ReadField(string line, int startIndex)
+    {
+        switch (line[startIndex])
+        {
+            case ' ':
+                return new Token(" ", startIndex, 1);
+            case '"':
+                return ParseQuotedField(line, startIndex, "\"");
+            case '\'':
+                return ParseQuotedField(line, startIndex, "'");
+            default:
+                return ParseFieldWithoutQuotes(line, startIndex);
+        }
+    }
+
+    private static Token ParseQuotedField(string line, int startIndex, string quote)
+    {
+        return ParseField(line, startIndex + 1, quote.ToCharArray(), 2);
+    }
+
+    private static Token ParseFieldWithoutQuotes(string line, int startIndex)
+    {
+        return ParseField(line, startIndex, StopChars, 0);
+    }
+
+    private static Token ParseField(string line, int startIndex, char[] stopChars, int quotesNumber)
+    {
+        var tokenValue = new StringBuilder();
+        var slashesCounter = 0;
+
+        for (var i = startIndex; i < line.Length && !stopChars.Contains(line[i]); i++)
+        {
+            if (line[i] == '\\')
+            {
+                slashesCounter++;
+                i++;
+            }
+
+            tokenValue.Append(line[i]);
+        }
+
+        return new Token(tokenValue.ToString(), startIndex,
+            tokenValue.Length + quotesNumber + slashesCounter);
+    }
+}
 }
