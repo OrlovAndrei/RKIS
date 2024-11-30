022059f1f790ed740e6a47597b1c73d925cdf0f6
Sofa <sofa.linder@gmail.com>
2024-11-30 12:15:50 +0300

    Update SegmentExtensions.cs3

--- Занятие 6/Практические задания/GeometryPainting/SegmentExtensions.cs
+++ Занятие 6/Практические задания/GeometryPainting/SegmentExtensions.cs
@@ -1,26 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.Concurrent;
 using Avalonia.Media;
 using GeometryTasks;
 
-namespace GeometryPainting;
-
-public class Data
-    {
-        public Color Col;
-    }
+namespace GeometryPainting
+{
     public static class SegmentExtensions
     {
-        public static ConditionalWeakTable<Segment, Data> Table = new ConditionalWeakTable<Segment, Data>();
-        public static void SetColor(this Segment segment, Color color)
-        {
-            Table.Add(segment, new Data { Col = color });
-        }
+        private static readonly ConcurrentDictionary<Segment, Color> SegmentColors = new();
+
         public static Color GetColor(this Segment segment)
         {
-            Data color;
-            if (Table.TryGetValue(segment, out color))
-                return color.Col;
-            return Color.Black;
+            return SegmentColors.TryGetValue(segment, out var color) ? color : Colors.Black;
+        }
+
+        public static void SetColor(this Segment segment, Color color)
+        {
+            SegmentColors[segment] = color;
+        }
+
+        public static void RemoveColor(this Segment segment)
+        {
+            SegmentColors.TryRemove(segment, out _);
         }
     }
 }
