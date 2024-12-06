abe6a6dc7aa11a48b76e1cc460da1da7f92a2470
Ardashev Sasha <ardashev004@gmail.com>
2024-12-06 14:51:50 +0300

    Lesson10: Task 1, 2

--- Занятие 10/Практические задания/StructBenchmarking/BenchmarkTask.cs
+++ Занятие 10/Практические задания/StructBenchmarking/BenchmarkTask.cs
@@ -1,27 +1,59 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using NUnit.Framework;
 
-namespace StructBenchmarking;
-public class Benchmark : IBenchmark
+namespace StructBenchmarking
 {
-    public double MeasureDurationInMs(ITask task, int repetitionCount)
+    public class Benchmark : IBenchmark
     {
-        GC.Collect();                   // Эти две строчки нужны, чтобы уменьшить вероятность того,
-        GC.WaitForPendingFinalizers();  // что Garbadge Collector вызовется в середине измерений
-                                        // и как-то повлияет на них.
-           
-		throw new NotImplementedException();
-	}
-}
+        public double MeasureDurationInMs(ITask task, int repetitionCount)
+        {
+            GC.Collect();                   
+            GC.WaitForPendingFinalizers();  
+            task.Run();
+            var time = new Stopwatch();
+            time.Start();
+            for (int i = 0; i < repetitionCount; i++)
+            {
+                task.Run();
+            }
+            time.Stop();
+            return time.Elapsed.TotalMilliseconds / repetitionCount;
+        }
+    }
 
-[TestFixture]
-public class RealBenchmarkUsageSample
-{
-    [Test]
-    public void StringConstructorFasterThanStringBuilder()
+    [TestFixture]
+    public class RealBenchmarkUsageSample
     {
-        throw new NotImplementedException();
+        [Test]
+        public void StringConstructorFasterThanStringBuilder()
+        {
+            var benchmark = new Benchmark();
+
+            Assert.Less(benchmark.MeasureDurationInMs(new StrCreateConstructor(), 10000), benchmark.MeasureDurationInMs(new StrCreateStrigBuilder(), 10000));
+        }
+    }
+
+    public class StrCreateStrigBuilder : ITask
+    {
+        public void Run()
+        {
+            var strBuilder = new StringBuilder();
+            for (int size = 0; size < 10000; size++)
+            {
+                strBuilder.Append("a");
+            }
+            var str = strBuilder.ToString();
+        }
+    }
+
+    public class StrCreateConstructor : ITask
+    {
+        public void Run()
+        {
+            var str = new string('a', 10000);
+        }
     }
 }
