0c3d4346034ed7e416dbaabec924303a55e29a61
Терминальный пользователь 40625 <40625@avt.local>
2024-12-07 15:18:55 +0300

    lesson 9:FieldsParserTask

--- Занятие 9/Практические задания/TableParser/QuotedFieldTask.cs
+++ Занятие 9/Практические задания/TableParser/QuotedFieldTask.cs
@@ -1,25 +1,27 @@
 using NUnit.Framework;
 
-namespace TableParser;
-
-[TestFixture]
-public class QuotedFieldTaskTests
+namespace TableParser
 {
-	[TestCase("''", 0, "", 2)]
-	[TestCase("'a'", 0, "a", 3)]
-	public void Test(string line, int startIndex, string expectedValue, int expectedLength)
-	{
-		var actualToken = QuotedFieldTask.ReadQuotedField(line, startIndex);
-		Assert.AreEqual(new Token(expectedValue, startIndex, expectedLength), actualToken);
-	}
+    [TestFixture]
+    public class QuotedFieldTaskTests
+    {
+        [TestCase("''", 0, "", 2)]
+        [TestCase("'a'", 0, "a", 3)]
+        public void Test(string line, int startIndex, string expectedValue, int expectedLength)
+        {
+            var actualToken = QuotedFieldTask.ReadQuotedField(line, startIndex);
+            Assert.AreEqual(new Token(expectedValue, startIndex, expectedLength), actualToken);
+        }
 
-	// Добавьте свои тесты
+        // Добавьте свои тесты
+    }
+
+    class QuotedFieldTask
+    {
+        public static Token ReadQuotedField(string line, int startIndex)
+        {
+            return new Token(line, startIndex, line.Length - startIndex);
+        }
+    }
 }
 
-class QuotedFieldTask
-{
-	public static Token ReadQuotedField(string line, int startIndex)
-	{
-		return new Token(line, startIndex, line.Length - startIndex);
-	}
-}
