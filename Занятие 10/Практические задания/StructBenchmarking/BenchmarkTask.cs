e31258c2bc35b2e88e7df88b58957b09e326c53c
Vrublevskaya1 <vrublevskaya07@list.ru>
2024-12-07 22:34:53 +0300

    lesson 9: Task ArrayCreation, Benchmark, Experiments, MethodCall

--- Занятие 10/Практические задания/StructBenchmarking/BenchmarkTask.cs
+++ Занятие 10/Практические задания/StructBenchmarking/BenchmarkTask.cs
@@ -4,24 +4,72 @@ using System.Diagnostics;
 using NUnit.Framework;
 
 namespace StructBenchmarking;
-public class Benchmark : IBenchmark
-{
-    public double MeasureDurationInMs(ITask task, int repetitionCount)
-    {
-        GC.Collect();                   // Эти две строчки нужны, чтобы уменьшить вероятность того,
-        GC.WaitForPendingFinalizers();  // что Garbadge Collector вызовется в середине измерений
-                                        // и как-то повлияет на них.
-           
-		throw new NotImplementedException();
-	}
-}
 
-[TestFixture]
-public class RealBenchmarkUsageSample
-{
-    [Test]
-    public void StringConstructorFasterThanStringBuilder()
+    public class Benchmark : IBenchmark
     {
-        throw new NotImplementedException();
+        public void WarmUpRun(ITask task)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            task.Run();
+            Thread.Sleep(1000);
+            stopwatch.Stop();
+        }
+
+        public double MeasureDurationInMs(ITask task, int repetitionCount)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Stopwatch stopwatch = new Stopwatch();
+            WarmUpRun(task);
+            stopwatch.Start();
+
+            for (var i = 0; i < repetitionCount; i++)
+            {
+                task.Run();
+            }
+
+            return (double)stopwatch.ElapsedMilliseconds / repetitionCount;
+        }
     }
-}
+
+    [TestFixture]
+
+    public class StringBuilderTest : ITask
+    {
+        public void Run()
+        {
+            var stringBuilder = new StringBuilder();
+
+            for (var i = 0; i < 10000; i++)
+            {
+                stringBuilder.Append('a');
+            }
+
+            stringBuilder.ToString();
+        }
+    }
+
+    public class StringTest : ITask
+    {
+        public void Run()
+        {
+            new string('a', 10000);
+        }
+    }
+
+    public class RealBenchmarkUsageSample
+    {
+        [Test]
+        public void StringConstructorFasterThanStringBuilder()
+        {
+            var stringBuilderTest = new StringBuilderTest();
+            var stringTest = new StringTest();
+
+            var benchMark = new Benchmark();
+            var result1 = benchMark.MeasureDurationInMs(stringBuilderTest, 10000);
+            var result2 = benchMark.MeasureDurationInMs(stringTest, 10000);
+            Assert.Less(result2, result1);
+        }
+    }
