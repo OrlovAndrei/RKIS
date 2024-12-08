cd81660cf06df0553ec0f2104666224eb546b608
GOL <164672521+accum104@users.noreply.github.com>
2024-12-08 21:00:56 +0300

    lesson 10 make BenchmarkTask

--- Занятие 10/Практические задания/StructBenchmarking/BenchmarkTask.cs
+++ Занятие 10/Практические задания/StructBenchmarking/BenchmarkTask.cs
@@ -3,25 +3,91 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using NUnit.Framework;
 
-namespace StructBenchmarking;
-public class Benchmark : IBenchmark
+namespace StructBenchmarking
 {
-    public double MeasureDurationInMs(ITask task, int repetitionCount)
+    // Интерфейс для замеров времени выполнения задач
+    public interface IPerformanceBenchmark
     {
-        GC.Collect();                   // Эти две строчки нужны, чтобы уменьшить вероятность того,
-        GC.WaitForPendingFinalizers();  // что Garbadge Collector вызовется в середине измерений
-                                        // и как-то повлияет на них.
-           
-		throw new NotImplementedException();
-	}
-}
+        double CalculateExecutionTimeInMs(ITask task, int executionCount);
+    }
 
-[TestFixture]
-public class RealBenchmarkUsageSample
-{
-    [Test]
-    public void StringConstructorFasterThanStringBuilder()
+    // Интерфейс для задачи
+    public interface ITask
     {
-        throw new NotImplementedException();
+        void Execute();
+    }
+
+    // Класс PerformanceBenchmark, реализующий IPerformanceBenchmark
+    public class PerformanceBenchmark : IPerformanceBenchmark
+    {
+        public double CalculateExecutionTimeInMs(ITask task, int executionCount)
+        {
+            // Принудительная сборка мусора перед измерением
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            // Прогревочный вызов для JIT-компиляции
+            task.Execute();
+
+            // Замер времени выполнения
+            var timer = Stopwatch.StartNew();
+            for (int i = 0; i < executionCount; i++)
+            {
+                task.Execute();
+            }
+            timer.Stop();
+
+            // Возвращаем среднее время выполнения в миллисекундах
+            return timer.Elapsed.TotalMilliseconds / executionCount;
+        }
+    }
+
+    // Реализация задачи через StringBuilder
+    public class StringBuilderExecution : ITask
+    {
+        public void Execute()
+        {
+            var stringBuilder = new System.Text.StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                stringBuilder.Append('a');
+            }
+            var output = stringBuilder.ToString();
+        }
+    }
+
+    // Реализация задачи через конструктор строки
+    public class StringConstructorExecution : ITask
+    {
+        public void Execute()
+        {
+            var output = new string('a', 10000);
+        }
+    }
+
+    // Класс с тестами
+    [TestFixture]
+    public class BenchmarkTests
+    {
+        [Test]
+        public void StringConstructorIsFasterThanStringBuilder()
+        {
+            var performanceBenchmark = new PerformanceBenchmark();
+
+            var stringBuilderTask = new StringBuilderExecution();
+            var stringConstructorTask = new StringConstructorExecution();
+
+            int executionCount = 1000; // Количество повторений для измерения
+
+            // Измеряем время выполнения каждой задачи
+            double stringBuilderTime = performanceBenchmark.CalculateExecutionTimeInMs(stringBuilderTask, executionCount);
+            double stringConstructorTime = performanceBenchmark.CalculateExecutionTimeInMs(stringConstructorTask, executionCount);
+
+            Console.WriteLine($"StringBuilder time: {stringBuilderTime} ms");
+            Console.WriteLine($"String constructor time: {stringConstructorTime} ms");
+
+            // Проверяем, что конструктор строки работает быстрее
+            Assert.Less(stringConstructorTime, stringBuilderTime);
+        }
     }
 }
