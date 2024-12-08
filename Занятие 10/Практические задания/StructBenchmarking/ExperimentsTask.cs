da02498a73e8b7df1dfa9c3f9fe4b5629ae26fb4
GOL <164672521+accum104@users.noreply.github.com>
2024-12-08 21:04:34 +0300

    lesson 10 make ExperimentsTask

--- Занятие 10/Практические задания/StructBenchmarking/ExperimentsTask.cs
+++ Занятие 10/Практические задания/StructBenchmarking/ExperimentsTask.cs
@@ -2,37 +2,66 @@ using System.Collections.Generic;
 
 namespace StructBenchmarking;
 
-public class Experiments
+public class PerformanceExperiments
 {
-	public static ChartData BuildChartDataForArrayCreation(
-		IBenchmark benchmark, int repetitionsCount)
-	{
-		var classesTimes = new List<ExperimentResult>();
-		var structuresTimes = new List<ExperimentResult>();
-            
-		//...
+    // Общий метод для измерений
+    private static ChartData MeasurePerformanceMetrics(
+        IBenchmark benchmark,
+        int numberOfRepetitions,
+        ITaskFactory taskFactory,
+        string chartTitle)
+    {
+        var classMeasurementPoints = new List<ExperimentResult>();
+        var structMeasurementPoints = new List<ExperimentResult>();
 
-		return new ChartData
-		{
-			Title = "Create array",
-			ClassPoints = classesTimes,
-			StructPoints = structuresTimes,
-		};
-	}
+        // Перебираем все значения размеров массива
+        foreach (var fieldSize in Constants.FieldCounts)
+        {
+            var classTask = taskFactory.CreateClassTask();
+            var structTask = taskFactory.CreateStructTask();
 
-	public static ChartData BuildChartDataForMethodCall(
-		IBenchmark benchmark, int repetitionsCount)
-	{
-		var classesTimes = new List<ExperimentResult>();
-		var structuresTimes = new List<ExperimentResult>();
-            
-		//...
+            classMeasurementPoints.Add(new ExperimentResult
+            {
+                FieldCount = fieldSize,
+                Time = benchmark.MeasureDuration(classTask, numberOfRepetitions)
+            });
 
-		return new ChartData
-		{
-			Title = "Call method with argument",
-			ClassPoints = classesTimes,
-			StructPoints = structuresTimes,
-		};
-	}
+            structMeasurementPoints.Add(new ExperimentResult
+            {
+                FieldCount = fieldSize,
+                Time = benchmark.MeasureDuration(structTask, numberOfRepetitions)
+            });
+        }
+
+        return new ChartData
+        {
+            Title = chartTitle,
+            ClassPoints = classMeasurementPoints,
+            StructPoints = structMeasurementPoints
+        };
+    }
+
+    // Метод для эксперимента 1 (создание массива)
+    public static ChartData GenerateChartDataForArrayCreation(
+        IBenchmark benchmark, int numberOfRepetitions)
+    {
+        return MeasurePerformanceMetrics(
+            benchmark,
+            numberOfRepetitions,
+            new ArrayCreationTaskFactory(),
+            "Create array"
+        );
+    }
+
+    // Метод для эксперимента 2 (передача в метод).
+    public static ChartData GenerateChartDataForMethodCall(
+        IBenchmark benchmark, int numberOfRepetitions)
+    {
+        return MeasurePerformanceMetrics(
+            benchmark,
+            numberOfRepetitions,
+            new MethodCallTaskFactory(),
+            "Call method with argument"
+        );
+    }
 }
