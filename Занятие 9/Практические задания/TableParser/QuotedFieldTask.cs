78b263462cf5727506fb152c7f0e9a2c924e7174
Agni <63151335+crash36663@users.noreply.github.com>
2024-12-12 10:04:30 +0300

    Update QuotedFieldTask.cs

--- Занятие 9/Практические задания/TableParser/QuotedFieldTask.cs
+++ Занятие 9/Практические задания/TableParser/QuotedFieldTask.cs
@@ -3,97 +3,90 @@ using NUnit.Framework;
 namespace TableParser;
 
 [TestFixture]
-public class FieldParserTaskTests
+public class QuotedFieldTaskTests
 {
-    public static void Test(string input, string[] expectedResult)
-    {
-        var actualResult = FieldsParserTask.ParseLine(input);
-        Assert.AreEqual(expectedResult.Length, actualResult.Count);
-        for (int i = 0; i < expectedResult.Length; ++i)
-        {
-            Assert.AreEqual(expectedResult[i], actualResult[i].Value);
-        }
-    }
+    [TestCase("''", 0, "", 2)]
+    [TestCase("'a'", 0, "a", 3)]
+    [TestCase("'ab'", 0, "ab", 4)]
+    [TestCase("'a''b'", 0, "a'b", 6)] // Обработка экранированных кавычек
+    [TestCase("'test string'", 0, "test string", 13)]
+    [TestCase("'string with spaces'", 0, "string with spaces", 19)]
+    [TestCase("'string with ''quotes'''", 0, "string with 'quotes'", 25)] // Обработка экранированных кавычек
+    [TestCase("not a quoted string", 0, "", 0)] // Строка не в кавычках
+    [TestCase("'unclosed quote", 0, "unclosed quote", 15)] // Незакрытая кавычка
+    [TestCase("", 0, "", 0)] // Пустая строка
+    [TestCase(null, 0, null, 0)] // Null строка
 
-    [TestCase("text", new[] { "text" })]
-    [TestCase("hello world", new[] { "hello", "world" })]
-    [TestCase("123 456", new[] { "123", "456" })]
-    [TestCase("'quoted text'", new[] { "quoted text" })]
-    [TestCase("text 'quoted'", new[] { "text", "quoted" })]
-    [TestCase("'quoted' text", new[] { "quoted", "text" })]
-    [TestCase("'quoted text' with more text", new[] { "quoted text", "with", "more", "text" })]
-    [TestCase("text with 'quoted' text", new[] { "text", "with", "quoted", "text" })]
-    [TestCase("'q1' 'q2' 'q3'", new[] { "q1", "q2", "q3" })]
-    [TestCase("1 'q1' 2 'q2'", new[] { "1", "q1", "2", "q2" })]
-    [TestCase("", new string[0])]
-    [TestCase(null, new string[0])]
-    [TestCase(" 'quoted' ", new[] { "quoted" })] // Пробелы вокруг
-    [TestCase("'a''b'", new[] { "a'b" })]          // Экранированная кавычка
-    [TestCase("'a\\'b'", new[] { "a'b" })]         // Экранированная кавычка с escape символом
-    public static void RunTests(string input, string[] expectedOutput)
+    public void Test(string line, int startIndex, string expectedValue, int expectedLength)
     {
-        Test(input, expectedOutput);
+        var actualToken = QuotedFieldTask.ReadQuotedField(line, startIndex);
+        Assert.AreEqual(new Token(expectedValue, startIndex, expectedLength), actualToken);
     }
 }
 
-public class FieldsParserTask
+public class Token
 {
-    public static List<Token> ParseLine(string line)
-    {
-        List<Token> tokens = new List<Token>();
-        int index = 0;
-        while (index < line?.Length ?? 0)
-        {
-            index = SkipSpaces(line, index);
-            if (index >= line.Length) break;
+    public string Value { get; }
+    public int StartIndex { get; }
+    public int Length { get; }
 
-            if (line[index] == '\'')
+    public Token(string value, int startIndex, int length)
+    {
+        Value = value;
+        StartIndex = startIndex;
+        Length = length;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Token other)
+        {
+            return Value == other.Value && StartIndex == other.StartIndex && Length == other.Length;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Value, StartIndex, Length);
+    }
+}
+
+
+public class QuotedFieldTask
+{
+    public static Token ReadQuotedField(string line, int startIndex)
+    {
+        if (string.IsNullOrEmpty(line) || startIndex >= line.Length || line[startIndex] != '\'') return new Token("", startIndex, 0);
+
+        int endIndex = startIndex + 1;
+        StringBuilder sb = new StringBuilder();
+        bool escape = false;
+
+        while (endIndex < line.Length)
+        {
+            char c = line[endIndex];
+            if (escape)
             {
-                Token token = ReadQuotedField(line, index);
-                tokens.Add(token);
-                index = token.GetIndexNextToToken();
+                sb.Append(c);
+                escape = false;
+            }
+            else if (c == '\'')
+            {
+                endIndex++;
+                break;
+            }
+            else if (c == '\\')
+            {
+                escape = true;
             }
             else
             {
-                Token token = ReadField(line, index);
-                tokens.Add(token);
-                index = token.GetIndexNextToToken();
+                sb.Append(c);
             }
-        }
-        return tokens;
-    }
-
-    private static int SkipSpaces(string line, int startIndex)
-    {
-        while (startIndex < line.Length && char.IsWhiteSpace(line[startIndex]))
-        {
-            startIndex++;
-        }
-        return startIndex;
-    }
-
-    private static Token ReadField(string line, int startIndex)
-    {
-        int endIndex = startIndex;
-        while (endIndex < line.Length && !char.IsWhiteSpace(line[endIndex]) && line[endIndex] != '\'')
-        {
             endIndex++;
         }
-        return new Token(line.Substring(startIndex, endIndex - startIndex), startIndex, endIndex - startIndex);
-    }
 
-    private static Token ReadQuotedField(string line, int startIndex)
-    {
-        return QuotedFieldTask.ReadQuotedField(line, startIndex);
-    }
-}
-
-// ... (Token class and QuotedFieldTask class from previous answer) ...
-
-public static class TokenExtensions
-{
-    public static int GetIndexNextToToken(this Token token)
-    {
-        return token.StartIndex + token.Length;
+        return new Token(sb.ToString(), startIndex, endIndex - startIndex);
     }
 }
