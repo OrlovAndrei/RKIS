dd7d15f8a9bd0c1dafcee10c787791e9909236eb
Dasha2005-zxc <diddasha05@bk.ru>
2024-12-13 19:14:13 +0300

    lesson 12: make LeftBorderTask.cs

--- Занятие 12/Практические задания/Autocomplete/LeftBorderTask.cs
+++ Занятие 12/Практические задания/Autocomplete/LeftBorderTask.cs
@@ -1,35 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace Autocomplete;
-
-// Внимание!
-// Есть одна распространенная ловушка при сравнении строк: строки можно сравнивать по-разному:
-// с учетом регистра, без учета, зависеть от кодировки и т.п.
-// В файле словаря все слова отсортированы методом StringComparison.InvariantCultureIgnoreCase.
-// Во всех функциях сравнения строк в C# можно передать способ сравнения.
-public class LeftBorderTask
+namespace Autocomplete
 {
-	/// <returns>
-	/// Возвращает индекс левой границы.
-	/// То есть индекс максимальной фразы, которая не начинается с prefix и меньшая prefix.
-	/// Если такой нет, то возвращает -1
-	/// </returns>
-	/// <remarks>
-	/// Функция должна быть рекурсивной
-	/// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
-	/// </remarks>
-	public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
-	{
-		// IReadOnlyList похож на List, но у него нет методов модификации списка.
-		// Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-		for (int i = 0; i < phrases.Count; i++)
-		{
-			if (string.Compare(prefix, phrases[i], StringComparison.InvariantCultureIgnoreCase) < 0
-			    || phrases[i].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
-				return i - 1;
-		}
-		return phrases.Count-1;
-	}
+    public class LeftBorderTask
+    {
+        public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+        {
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                bool isPrefixMatch = phrases[mid].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase);
+
+                if (!isPrefixMatch && string.Compare(prefix, phrases[mid], StringComparison.InvariantCultureIgnoreCase) > 0)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return left == phrases.Count ? -1 : left - 1;
+        }
+    }
 }
