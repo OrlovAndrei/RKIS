5cc785276c658f53326d495fdf5676ebabd25d8f
wrzla <wrzla@ya.ru>
2024-12-14 12:42:14 +0300

    Update RectanglesTask.cs

--- Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
+++ Занятие 2/Практические задания/Rectangles/RectanglesTask.cs
@@ -1,41 +1,52 @@
 using System;
- 
-namespace Rectangles
+
+namespace Rectangles;
+
+public static class RectanglesTask
 {
-    public static class RectanglesTask
+    public static bool AreIntersected(Rectangle r1, Rectangle r2)
     {
-        public static bool AreIntersected(Rectangle r1, Rectangle r2)
+        // так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
+        var left = Math.Max(Math.Min(r1!.Left, r1!.Right), Math.Min(r2.Left, r2.Right));
+        var right = Math.Min(Math.Max(r1!.Left, r1!.Right), Math.Max(r2.Left, r2.Right));
+        var bottom = Math.Min(Math.Max(r1!.Top, r1!.Bottom), Math.Max(r2.Top, r2.Bottom));
+        var top = Math.Max(Math.Min(r1.Top, r1.Bottom), Math.Min(r2.Top, r2.Bottom));
+        if (right < left || bottom < top)
         {
-            return !(r1.Left > r2.Right || r1.Top > r2.Bottom || r1.Right < r2.Left || r1.Bottom < r2.Top);
+            return false;
         }
- 
-        public static int IntersectionSquare(Rectangle r1, Rectangle r2)
+        return true;
+    }
+    // Площадь пересечения прямоугольников
+    public static int IntersectionSquare(Rectangle r1, Rectangle r2)
+    {
+        if (AreIntersected(r1, r2))
         {
-            if (AreIntersected(r1, r2))
-            {
-                if (r1.Left == r2.Right || r1.Top == r2.Bottom || r1.Right == r2.Left || r1.Bottom == r2.Top)
-                    return 0;
-                else
-                {
-                    int maximumLeft = Math.Max(r1.Left, r2.Left);
-                    int minimumLeftWeight = Math.Min(r1.Right, r2.Right);
-                    int maximumTop = Math.Max(r1.Top, r2.Top);
-                    int minimumTopHeight = Math.Min(r1.Bottom, r2.Bottom);
-                    return (minimumLeftWeight - maximumLeft) * (minimumTopHeight - maximumTop);
-                }
-            }
+            var left = Math.Max(Math.Min(r1.Left, r1.Right), Math.Min(r2.Left, r2.Right));
+            var right = Math.Min(Math.Max(r1.Left, r1.Right), Math.Max(r2.Left, r2.Right));
+            var bottom = Math.Min(Math.Max(r1.Top, r1.Bottom), Math.Max(r2.Top, r2.Bottom));
+            var top = Math.Max(Math.Min(r1.Top, r1.Bottom), Math.Min(r2.Top, r2.Bottom));
+            var height = Math.Max(right, left) - Math.Min(left, right);
+            var width = Math.Max(bottom, top) - Math.Min(top, bottom);
+            return height * width;
+        }
+        return 0;
+    }
+
+        // Если прямоугольники совпадают, можно вернуть номер любого из них.
+        public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
+    {
+        if (r1.Top >= r2.Top && r1.Left >= r2.Left && r1.Right <= r2.Right && r1.Bottom <= r2.Bottom)
+        {
             return 0;
         }
- 
-        public static int IndexOfInnerRectangle(Rectangle r1, Rectangle r2)
+        else if (r2.Top >= r1.Top && r2.Left >= r1.Left && r2.Right <= r1.Right && r2.Bottom <= r1.Bottom)
         {
-            if (r1.Left <= r2.Left && r1.Right >= r2.Right && r1.Bottom >= r2.Bottom && r1.Top <= r2.Top)
-                return 1;
-            else if
-                (r1.Left >= r2.Left && r1.Right <= r2.Right && r1.Bottom <= r2.Bottom && r1.Top >= r2.Top)
-                return 0;
-            else
-                return -1;              
+            return 1;
+        }
+        else
+        {
+            return -1;
         }
     }
 }
