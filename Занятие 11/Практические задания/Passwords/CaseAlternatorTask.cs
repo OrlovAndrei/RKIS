659cf7166db32752ebb8a8492e8fc1b8932a48d9
Ardashev Sasha <ardashev004@gmail.com>
2024-12-14 21:17:41 +0300

    lesson 11: CaseAlternatorTask

--- Занятие 11/Практические задания/Passwords/CaseAlternatorTask.cs
+++ Занятие 11/Практические задания/Passwords/CaseAlternatorTask.cs
@@ -1,18 +1,33 @@
 namespace Passwords;
 
 public class CaseAlternatorTask
-{
-	//Тесты будут вызывать этот метод
-	public static List<string> AlternateCharCases(string lowercaseWord)
-	{
-		var result = new List<string>();
-		AlternateCharCases(lowercaseWord.ToCharArray(), 0, result);
-		return result;
-	}
+{public static List<string> AlternateCharCases(string lowercaseWord)
+    {
+        var result = new List<string>();
+        AlternateCharCases(lowercaseWord.ToCharArray(), 0, result);
+        return result.OrderBy(s => s).Distinct().ToList();
+    }
 
-	static void AlternateCharCases(char[] word, int startIndex, List<string> result)
-	{
-		// TODO
-		result.Add(new string (word));
-	}
+    static void AlternateCharCases(char[] word, int startIndex, List<string> result)
+    {
+        if (startIndex == word.Length)
+        {
+            result.Add(new string(word));
+            return;
+        }
+
+        if (char.IsLetter(word[startIndex]))
+        {
+            AlternateCharCases(word, startIndex + 1, result);
+
+            char originalChar = word[startIndex];
+            word[startIndex] = char.ToUpper(originalChar);
+            AlternateCharCases(word, startIndex + 1, result);
+            word[startIndex] = originalChar;
+        }
+        else
+        {
+            AlternateCharCases(word, startIndex + 1, result);
+        }
+    }
 }
