8503717fd379d07b6d2b026c8203308669a1f9c2
Kurt337 <tema.sitnikov05@gmail.com>
2024-12-14 23:06:56 +0300

    lesson 11  CaseAlternatorTask.cs

--- Занятие 11/Практические задания/Passwords/CaseAlternatorTask.cs
+++ Занятие 11/Практические задания/Passwords/CaseAlternatorTask.cs
@@ -1,7 +1,7 @@
-namespace Passwords;
+using System;
+using System.Collections.Generic;
 
-public class CaseAlternatorTask
-{
+namespace Passwords
 	//Тесты будут вызывать этот метод
 	public static List<string> AlternateCharCases(string lowercaseWord)
 	{
@@ -12,7 +12,31 @@ public class CaseAlternatorTask
 
 	static void AlternateCharCases(char[] word, int startIndex, List<string> result)
 	{
-		// TODO
-		result.Add(new string (word));
+			// Если дошли до конца слова, добавляем текущую комбинацию в результат
+            if (startIndex == word.Length)
+            {
+                result.Add(new string(word));
+                return;
+            }
+
+            // Рекурсивно обходим оставшиеся символы
+            AlternateCharCases(word, startIndex + 1, result);
+
+            // Если текущий символ — буква, меняем регистр и продолжаем
+            if (char.IsLetter(word[startIndex]))
+            {
+                // Меняем регистр текущей буквы
+                word[startIndex] = char.IsLower(word[startIndex])
+                    ? char.ToUpper(word[startIndex])
+                    : char.ToLower(word[startIndex]);
+
+                // Рекурсивно обходим оставшиеся символы с измененным регистром
+                AlternateCharCases(word, startIndex + 1, result);
+
+                // Возвращаем символ в исходный регистр
+                word[startIndex] = char.IsLower(word[startIndex])
+                    ? char.ToUpper(word[startIndex])
+                    : char.ToLower(word[startIndex]);
+			}
 	}
 }
