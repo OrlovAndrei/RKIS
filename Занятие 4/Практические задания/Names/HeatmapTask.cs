d775b30e245e317cb3d3bd125114a5b10a48d09d
Valera-Mkoyan25 <vale.mkoyan@gmail.com>
2024-12-14 12:34:46 +0300

    Update HeatmapTask.cs

--- Занятие 4/Практические задания/Names/HeatmapTask.cs
+++ Занятие 4/Практические задания/Names/HeatmapTask.cs
@@ -1,13 +1,41 @@
-﻿namespace Names;
-
-internal static class HeatmapTask
+using System;
+namespace Names
 {
-    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+
+    internal static class HeatmapTask
     {
-        return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            double[,] intensity = new double[30, 12];
+
+            foreach (var name in names)
+            {
+                int month = name.BirthDate.Month - 1; 
+                int day = name.BirthDate.Day;
+
+                if (day > 1)
+                {
+                    intensity[day - 2, month]++;
+                }
+            }
+
+            string[] xLabels = new string[30];
+            for (int i = 0; i < xLabels.Length; i++)
+            {
+                xLabels[i] = (i + 2).ToString(); 
+            }
+
+            string[] yLabels = new string[12];
+            for (int i = 0; i < yLabels.Length; i++)
+            {
+                yLabels[i] = (i + 1).ToString(); 
+            }
+
+            return new HeatmapData(
+                "Тепловая карта рождаемости",
+                intensity,
+                xLabels,
+                yLabels);
+        }
     }
 }
