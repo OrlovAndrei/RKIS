290d0754dc8c9c9cfba33448394bdccb7b1fa8e0
wrzla <wrzla@ya.ru>
2024-12-14 12:41:42 +0300

    Update PluralizeTask.cs

--- Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
+++ Занятие 2/Практические задания/Pluralize/PluralizeTask.cs
@@ -1,28 +1,23 @@
-﻿using System;
 namespace Pluralize
 {
-
     public static class PluralizeTask
     {
         public static string PluralizeRubles(int count)
         {
             int lastDigit = count % 10;
             int lastTwoDigits = count % 100;
-            if (lastTwoDigits >= 11 && lastTwoDigits <= 14)
+
+            if (lastDigit == 1 && lastTwoDigits != 11)
             {
-                return $"рублей"; 
+                return "рубль";
             }
-            else if (lastDigit == 1)
+            else if (lastDigit >= 2 && lastDigit <= 4 && (lastTwoDigits < 12 || lastTwoDigits > 14))
             {
-                return $"рубль"; 
-            }
-            else if (lastDigit >= 2 && lastDigit <= 4)
-            {
-                return $"рубля"; 
+                return "рубля";
             }
             else
             {
-                return $"рублей";
+                return "рублей";
             }
         }
     }
