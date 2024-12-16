126a33177cae796bdb4367de59857329c5eb5236
Lunassan <Dino.zavrik2705@gmail.com>
2024-12-16 23:06:40 +0300

    Lesson 9: make TableParser

--- Занятие 9/Практические задания/TableParser/QuotedFieldTask.cs
+++ Занятие 9/Практические задания/TableParser/QuotedFieldTask.cs
@@ -1,25 +1,71 @@
+using System.Text;
 using NUnit.Framework;
 
-namespace TableParser;
-
-[TestFixture]
-public class QuotedFieldTaskTests
+namespace TableParser
 {
-	[TestCase("''", 0, "", 2)]
-	[TestCase("'a'", 0, "a", 3)]
-	public void Test(string line, int startIndex, string expectedValue, int expectedLength)
-	{
-		var actualToken = QuotedFieldTask.ReadQuotedField(line, startIndex);
-		Assert.AreEqual(new Token(expectedValue, startIndex, expectedLength), actualToken);
-	}
+    [TestFixture]
+    public class QuotedFieldTaskTests
+    {
+        [TestCase("''", 0, "", 2)]
+        [TestCase("'a'", 0, "a", 3)]
+        [TestCase("'a\\\' b'", 0, "a' b", 7)]
+        public void Test(string Line, int startIndex, string expectedValue, int expectedLength)
+        {
+            var parsedToken = QuotedFieldTask.ReadQuotedField(Line, startIndex);
+            Assert.AreEqual(parsedToken, new Token(expectedValue, startIndex, expectedLength));
+        }
 
-	// Добавьте свои тесты
-}
+        public void MyTests(string Line, int startIndex, string expectedValue, int expectedLength)
+        {
+            var parsedToken = QuotedFieldTask.ReadQuotedField(Line, startIndex);
+            Assert.AreEqual(parsedToken, new Token(expectedValue, startIndex, expectedLength));
+        }
+    }
 
-class QuotedFieldTask
-{
-	public static Token ReadQuotedField(string line, int startIndex)
-	{
-		return new Token(line, startIndex, line.Length - startIndex);
-	}
+    class QuotedFieldTask
+    {
+        private static bool HandleEscapeCharacter(StringBuilder stringBuilder, char currentCharacter, bool isEscaped)
+        {
+            if (isEscaped)
+            {
+                stringBuilder.Append(currentCharacter);
+                return false;
+            }
+            return true;
+        }
+
+        public static Token ReadQuotedField(string Line, int startIndex)
+        {
+            var stringBuilder = new StringBuilder();
+            var isEscaped = false;
+            var currentIndex = startIndex;
+            var firstQuoteChar = Line[currentIndex++];
+
+            while (currentIndex < Line.Length)
+            {
+                var currentChar = Line[currentIndex++];
+
+                if (currentChar == '\\')
+                {
+                    isEscaped = HandleEscapeCharacter(stringBuilder, currentChar, isEscaped);
+                }
+                else if (currentChar == firstQuoteChar)
+                {
+                    if (!isEscaped)
+                    {
+                        break;
+                    }
+
+                    isEscaped = HandleEscapeCharacter(stringBuilder, currentChar, isEscaped);
+                }
+                else
+                {
+                    stringBuilder.Append(currentChar);
+                }
+            }
+
+            return new Token(stringBuilder.ToString(), startIndex, currentIndex - startIndex);
+        }
+
+    }
 }
