c492799f6edbb779b15f10060b7b141185116a8d
Lunassan <Dino.zavrik2705@gmail.com>
2024-12-16 22:57:49 +0300

    Lesson 8: make Hotel and ReadOnlyVector

--- Занятие 8/Практические задания/PocketGoogle/Indexer.cs
+++ Занятие 8/Практические задания/PocketGoogle/Indexer.cs
@@ -8,23 +8,96 @@ namespace PocketGoogle;
 
 public class Indexer : IIndexer
 {
-	public void Add(int id, string documentText)
-	{
-		throw new NotImplementedException();
-	}
+    private readonly Dictionary<string, Dictionary<int, HashSet<int>>> indexes = new();
+    private static readonly HashSet<char> Separators = new() { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' };
 
-	public List<int> GetIds(string word)
-	{
-		throw new NotImplementedException();
-	}
+    public void Add(int id, string documentText)
+    {
+        int wordStartIndex = 0;
 
-	public List<int> GetPositions(int id, string word)
-	{
-		throw new NotImplementedException();
-	}
+        for (int i = 0; i < documentText.Length; i++)
+        {
+            if (Separators.Contains(documentText[i]))
+            {
+                if (i > wordStartIndex)
+                {
+                    AddWordToIndex(id, documentText.Substring(wordStartIndex, i - wordStartIndex), wordStartIndex);
+                }
+                wordStartIndex = i + 1;
+            }
+        }
 
-	public void Remove(int id)
-	{
-		throw new NotImplementedException();
-	}
+        if (wordStartIndex < documentText.Length)
+        {
+            AddWordToIndex(id, documentText.Substring(wordStartIndex), wordStartIndex);
+        }
+    }
+
+    private void AddWordToIndex(int id, string word, int wordStartIndex)
+    {
+        if (string.IsNullOrWhiteSpace(word)) return;
+
+        if (!indexes.ContainsKey(word))
+        {
+            indexes[word] = new Dictionary<int, HashSet<int>>();
+        }
+
+        var idDictionary = indexes[word];
+
+        if (!idDictionary.ContainsKey(id))
+        {
+            idDictionary[id] = new HashSet<int>();
+        }
+
+        idDictionary[id].Add(wordStartIndex);
+    }
+
+    public List<int> GetIds(string word)
+    {
+        if (indexes.ContainsKey(word))
+        {
+            var idDictionary = indexes[word];
+            var ids = new List<int>();
+            foreach (var id in idDictionary.Keys)
+            {
+                ids.Add(id);
+            }
+            return ids;
+        }
+        return new List<int>();
+    }
+
+    public List<int> GetPositions(int id, string word)
+    {
+        if (indexes.ContainsKey(word) && indexes[word].ContainsKey(id))
+        {
+            var positions = new List<int>(indexes[word][id]);
+            return positions;
+        }
+        return new List<int>();
+    }
+
+    public void Remove(int id)
+    {
+        var wordsToRemove = new List<string>();
+
+        foreach (var entry in indexes)
+        {
+            if (entry.Value.ContainsKey(id))
+            {
+                wordsToRemove.Add(entry.Key);
+            }
+        }
+
+        foreach (var word in wordsToRemove)
+        {
+            var idDictionary = indexes[word];
+            idDictionary.Remove(id);
+
+            if (idDictionary.Count == 0)
+            {
+                indexes.Remove(word);
+            }
+        }
+    }
 }
